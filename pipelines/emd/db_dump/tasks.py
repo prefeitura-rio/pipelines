544d55db03eb15cfc88dd54820af24443b937d17
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-01-11 13:24:48 -0300

    fix scopes

--- pipelines/emd/db_dump/tasks.py
+++ pipelines/emd/db_dump/tasks.py
@@ -3,14 +3,19 @@ General purpose tasks for dumping database data.
 """
 
 from pathlib import Path
-from typing import List, Tuple, Union
+from typing import Union
 
-import pandas as pd
 from prefect import task
 import pymssql
 import basedosdados as bd
 
 from pipelines.utils import log
+from pipelines.emd.db_dump.utils import (
+    batch_to_dataframe,
+    dataframe_to_csv,
+    sql_server_fetch_batch,
+    sql_server_get_columns,
+)
 
 ###############
 #
@@ -50,60 +55,11 @@ def sql_server_execute(cursor, query):
     return cursor
 
 
-@task
-def sql_server_fetch_batch(cursor, batch_size):
-    """
-    Fetches a batch of rows from the SQL Server.
-    """
-    log(f"Fetching batch of {batch_size} rows")
-    return cursor.fetchmany(batch_size)
-
-
-@task
-def sql_server_get_columns(cursor):
-    """
-    Returns the column names of the SQL Server.
-    """
-    log("Getting column names")
-    return [column[0] for column in cursor.description]
-
-
-###############
-#
-# Dataframe
-#
-###############
-
-
-@task
-def batch_to_dataframe(batch: Tuple[Tuple], columns: List[str]) -> pd.DataFrame:
-    """
-    Converts a batch of rows to a dataframe.
-    """
-    log(f"Converting batch of size {len(batch)} to dataframe")
-    return pd.DataFrame(batch, columns=columns)
-
-
 ###############
 #
 # File
 #
 ###############
-
-
-@task
-def dataframe_to_csv(dataframe: pd.DataFrame, path: Union[str, Path]) -> None:
-    """
-    Writes a dataframe to a CSV file.
-    """
-    # Remove filename from path
-    path = Path(path).parent
-    # Create directory if it doesn't exist
-    path.mkdir(parents=True, exist_ok=True)
-    # Write dataframe to CSV
-    log(f"Writing dataframe to CSV: {path}")
-    dataframe.to_csv(path, index=False)
-    log(f"Wrote dataframe to CSV: {path}")
 
 
 @task
