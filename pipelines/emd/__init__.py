b31b713d98d2edb63fad9ecbc1b68b74509953f3
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-01-27 13:29:04 -0300

    example flows cleanup

--- pipelines/emd/__init__.py
+++ pipelines/emd/__init__.py
@@ -4,8 +4,6 @@ Prefect flows for emd project
 ###############################################################################
 # Automatically managed, please do not touch
 ###############################################################################
-from .my_flow.flows import *
-from .test_flow.flows import *
 from .tweets_flamengo.flows import *
 from .db_dump.flows import *
 from .dbt.flows import *
