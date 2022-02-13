dff3934f59d449fab7e95104a3831ef43e9ec416
d116626 <d116626@gmail.com>
2022-02-13 00:59:53 -0300

    fix(infra): change bd and jinja version

--- pipelines/emd/db_dump/utils.py
+++ pipelines/emd/db_dump/utils.py
@@ -23,6 +23,7 @@ def batch_to_dataframe(batch: Tuple[Tuple], columns: List[str]) -> pd.DataFrame:
     log(f"Converting batch of size {len(batch)} to dataframe")
     return pd.DataFrame(batch, columns=columns)
 
+
 ###############
 #
 # File
@@ -42,3 +43,63 @@ def dataframe_to_csv(dataframe: pd.DataFrame, path: Union[str, Path]) -> None:
     log(f"Writing dataframe to CSV: {path}")
     dataframe.to_csv(path, index=False)
     log(f"Wrote dataframe to CSV: {path}")
+
+
+import os
+from pathlib import Path
+import pandas as pd
+
+
+def to_partitions(data, partition_columns, savepath):
+    """Save data in to hive patitions schema, given a dataframe and a list of partition columns.
+    Args:
+        data (pandas.core.frame.DataFrame): Dataframe to be partitioned.
+        partition_columns (list): List of columns to be used as partitions.
+        savepath (str, pathlib.PosixPath): folder path to save the partitions
+
+    Exemple:
+
+        data = {
+            "ano": [2020, 2021, 2020, 2021, 2020, 2021, 2021,2025],
+            "mes": [1, 2, 3, 4, 5, 6, 6,9],
+            "sigla_uf": ["SP", "SP", "RJ", "RJ", "PR", "PR", "PR","PR"],
+            "dado": ["a", "b", "c", "d", "e", "f", "g",'h'],
+        }
+
+        to_partitions(
+            data=pd.DataFrame(data),
+            partition_columns=['ano','mes','sigla_uf'],
+            savepath='partitions/'
+        )
+    """
+
+    if isinstance(data, (pd.core.frame.DataFrame)):
+
+        savepath = Path(savepath)
+
+        unique_combinations = (
+            data[partition_columns]
+            .drop_duplicates(subset=partition_columns)
+            .to_dict(orient="records")
+        )
+
+        for filter_combination in unique_combinations:
+            patitions_values = [
+                f"{partition}={value}"
+                for partition, value in filter_combination.items()
+            ]
+            filter_save_path = Path(savepath / "/".join(patitions_values))
+            filter_save_path.mkdir(parents=True, exist_ok=True)
+
+            df_filter = data.loc[
+                data[filter_combination.keys()]
+                .isin(filter_combination.values())
+                .all(axis=1),
+                :,
+            ]
+            df_filter = df_filter.drop(columns=partition_columns)
+
+            df_filter.to_csv(filter_save_path / "data.csv", index=False)
+
+    else:
+        raise (BaseException("Data need to be a pandas DataFrame"))
