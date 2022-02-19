939778fac1b91fdce203bda73e4e6216f4cb1fe8
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-02-19 15:46:11 -0300

    add smart split function

--- pipelines/utils.py
+++ pipelines/utils.py
@@ -136,6 +136,34 @@ def send_telegram_message(
     )
 
 
+def smart_split(
+    text: str,
+    max_length: int,
+    separator: str = " ",
+) -> List[str]:
+    """
+    Splits a string into a list of strings.
+    """
+    if len(text) <= max_length:
+        return [text]
+
+    separator_index = text.rfind(separator, 0, max_length)
+    if (separator_index >= max_length) or (separator_index == -1):
+        raise ValueError(
+            f"Cannot split text \"{text}\" into {max_length}"
+            f"characters using separator \"{separator}\""
+        )
+
+    return [
+        text[:separator_index],
+        *smart_split(
+            text[separator_index + len(separator):],
+            max_length,
+            separator,
+        ),
+    ]
+
+
 def untuple_clocks(clocks):
     """
     Converts a list of tuples to a list of clocks.
