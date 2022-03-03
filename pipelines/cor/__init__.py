e09364387bea5b32a395f642273454d02feeff78
patriciacatandi <patriciabcatandi@gmail.com>
2022-03-03 09:34:21 -0300

    adiçao do pipeline de meteorologia do inmet

--- pipelines/cor/__init__.py
+++ pipelines/cor/__init__.py
@@ -1,5 +1,6 @@
 """
 Prefect flows for cor project
 """
+from pipelines.cor.alagamentos.meteorologia_inmet.flows import *
 from pipelines.cor.alagamentos.satelite.flows import *
 from pipelines.cor.cet_bot.flows import *
