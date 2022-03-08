cf9e754f56f451b6b419ecbc9f951cdb1a6a2771
patriciacatandi <patriciabcatandi@gmail.com>
2022-03-08 11:14:13 -0300

    alteraçoes referentes a nova estrutura de pastas

---
+++ pipelines/rj_cor/alagamentos/meteorologia_inmet/__init__.py
@@ -0,0 +1,7 @@
+"""
+Prefect flows for meteorologia_inmet project
+"""
+###############################################################################
+# Automatically managed, please do not touch
+###############################################################################
+from .flows import *
