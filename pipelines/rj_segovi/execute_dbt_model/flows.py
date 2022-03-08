160e2cdbdaad55f94026b329d3850f028fd61726
d116626 <d116626@gmail.com>
2022-03-08 12:16:55 -0300

    feat(dados): dump more sme tables

--- pipelines/rj_segovi/execute_dbt_model/flows.py
+++ pipelines/rj_segovi/execute_dbt_model/flows.py
@@ -14,7 +14,7 @@ from pipelines.rj_segovi.execute_dbt_model.schedules import (
 )
 
 run_dbt_1746_flow = deepcopy(run_dbt_model_flow)
-run_dbt_1746_flow.name = "SEOP: 1746 - Materializar tabelas"
+run_dbt_1746_flow.name = "SEGOVI: 1746 - Materializar tabelas"
 run_dbt_1746_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 run_dbt_1746_flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 run_dbt_1746_flow.schedule = _1746_monthly_update_schedule
