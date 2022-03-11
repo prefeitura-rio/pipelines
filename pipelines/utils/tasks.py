71026dfddc66bbd5848d30af604599228298167d
d116626 <d116626@gmail.com>
2022-03-11 18:29:21 -0300

    feat(infra): fix partitioned header path

--- pipelines/utils/tasks.py
+++ pipelines/utils/tasks.py
@@ -57,59 +57,30 @@ def dump_header_to_csv(
         for fname in filenames:
             if fname.endswith(".csv"):
                 file = join(subdir, fname)
-                print(file)
+                log(f"Found CSV file: {file}")
                 found = True
                 break
         if found:
             break
 
-    # Read just first row
-    dataframe = pd.read_csv(file, nrows=1)
+    # discover if it's a partitioned table
+    if partition_folders := [folder for folder in file.split("/") if "=" in folder]:
+        partition_path = "/".join(partition_folders)
+        save_header_path = Path(f"data/{uuid4()}/{partition_path}/header.csv")
+    else:
+        save_header_path = Path(f"data/{uuid4()}/header.csv")
 
     # Create directory if it doesn't exist
-    save_header_path = Path(f"data/{uuid4()}/header.csv")
     save_header_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Read just first row
+    dataframe = pd.read_csv(file, nrows=1)
 
     # Write dataframe to CSV
     dataframe.to_csv(save_header_path, index=False, encoding="utf-8")
     log(f"Wrote header CSV: {path}")
 
-    return save_header_path
-
-
-@task(
-    max_retries=constants.TASK_MAX_RETRIES.value,
-    retry_delay=timedelta(seconds=constants.TASK_RETRY_DELAY.value),
-)
-def upload_to_gcs(
-    path: Union[str, Path],
-    dataset_id: str,
-    table_id: str,
-    wait=None,  # pylint: disable=unused-argument
-) -> None:
-    """
-    Uploads a bunch of CSVs using BD+
-    """
-    # pylint: disable=C0103
-    tb = bd.Table(dataset_id=dataset_id, table_id=table_id)
-    # st = bd.Storage(dataset_id=dataset_id, table_id=table_id)
-
-    if tb.table_exists(mode="staging"):
-        # the name of the files need to be the same or the data doesn't get overwritten
-        tb.append(
-            filepath=path,
-            if_exists="replace",
-        )
-
-        log(
-            f"Successfully uploaded {path} to {tb.bucket_name}.staging.{dataset_id}.{table_id}"
-        )
-
-    else:
-        # pylint: disable=C0301
-        log(
-            "Table does not exist in STAGING, need to create it in local first.\nCreate and publish the table in BigQuery first."
-        )
+    return save_header_path.parent
 
 
 @task(
@@ -175,8 +146,42 @@ def create_bd_table(
         log(
             f"Mode overwrite: Sucessfully created table {st.bucket_name}.{dataset_id}.{table_id}"
         )
-        st.delete_table(
-            mode="staging", bucket_name=st.bucket_name, not_found_ok=True)
+        st.delete_table(mode="staging", bucket_name=st.bucket_name, not_found_ok=True)
         log(
             f"Mode overwrite: Sucessfully remove header data from {st.bucket_name}.{dataset_id}.{table_id}"
         )  # pylint: disable=C0301
+
+
+@task(
+    max_retries=constants.TASK_MAX_RETRIES.value,
+    retry_delay=timedelta(seconds=constants.TASK_RETRY_DELAY.value),
+)
+def upload_to_gcs(
+    path: Union[str, Path],
+    dataset_id: str,
+    table_id: str,
+    wait=None,  # pylint: disable=unused-argument
+) -> None:
+    """
+    Uploads a bunch of CSVs using BD+
+    """
+    # pylint: disable=C0103
+    tb = bd.Table(dataset_id=dataset_id, table_id=table_id)
+    # st = bd.Storage(dataset_id=dataset_id, table_id=table_id)
+
+    if tb.table_exists(mode="staging"):
+        # the name of the files need to be the same or the data doesn't get overwritten
+        tb.append(
+            filepath=path,
+            if_exists="replace",
+        )
+
+        log(
+            f"Successfully uploaded {path} to {tb.bucket_name}.staging.{dataset_id}.{table_id}"
+        )
+
+    else:
+        # pylint: disable=C0301
+        log(
+            "Table does not exist in STAGING, need to create it in local first.\nCreate and publish the table in BigQuery first."
+        )
