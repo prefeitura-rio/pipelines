222c866e7a4b5a2df62ddb1a6aa4c2f3166c0d70
d116626 <d116626@gmail.com>
2022-03-11 13:57:22 -0300

    feat(infra): extract last partition date

--- pipelines/utils/dump_db/tasks.py
+++ pipelines/utils/dump_db/tasks.py
@@ -7,6 +7,7 @@ from typing import Dict, Union
 from uuid import uuid4
 
 import basedosdados as bd
+import pandas as pd
 from prefect import task
 
 from pipelines.utils.dump_db.db import (
@@ -148,6 +149,68 @@ def extract_last_partition_date(partitions_dict: dict):
     return last_partition_date
 
 
+def to_partitions(data, partition_columns, savepath):
+    """Save data in to hive patitions schema, given a dataframe and a list of partition columns.
+    Args:
+        data (pandas.core.frame.DataFrame): Dataframe to be partitioned.
+        partition_columns (list): List of columns to be used as partitions.
+        savepath (str, pathlib.PosixPath): folder path to save the partitions
+    Exemple:
+        data = {
+            "ano": [2020, 2021, 2020, 2021, 2020, 2021, 2021,2025],
+            "mes": [1, 2, 3, 4, 5, 6, 6,9],
+            "sigla_uf": ["SP", "SP", "RJ", "RJ", "PR", "PR", "PR","PR"],
+            "dado": ["a", "b", "c", "d", "e", "f", "g",'h'],
+        }
+        to_partitions(
+            data=pd.DataFrame(data),
+            partition_columns=['ano','mes','sigla_uf'],
+            savepath='partitions/'
+        )
+    """
+
+    if isinstance(data, (pd.core.frame.DataFrame)):
+
+        savepath = Path(savepath)
+
+        unique_combinations = (
+            data[partition_columns]
+            .drop_duplicates(subset=partition_columns)
+            .to_dict(orient="records")
+        )
+
+        for filter_combination in unique_combinations:
+            patitions_values = [
+                f"{partition}={value}"
+                for partition, value in filter_combination.items()
+            ]
+            filter_save_path = Path(savepath / "/".join(patitions_values))
+            filter_save_path.mkdir(parents=True, exist_ok=True)
+
+            df_filter = data.loc[
+                data[filter_combination.keys()]
+                .isin(filter_combination.values())
+                .all(axis=1),
+                :,
+            ]
+            df_filter = df_filter.drop(columns=partition_columns)
+
+            file_path = Path(filter_save_path) / "data.csv"
+            df_filter.to_csv(
+                file_path, index=False, mode="a", header=not file_path.exists()
+            )
+    else:
+        raise BaseException("Data need to be a pandas DataFrame")
+
+
+def parse_date_columns(dataframe, partition_date_column):
+    dataframe["data_particao"] = pd.to_datetime(dataframe[partition_date_column])
+    dataframe["ano_particao"] = dataframe["data_particao"].dt.year
+    dataframe["mes_particao"] = dataframe["data_particao"].dt.month
+    dataframe["data_particao"] = dataframe["data_particao"].dt.date
+    return dataframe
+
+
 @task(
     checkpoint=False,
     max_retries=constants.TASK_MAX_RETRIES.value,
@@ -182,8 +245,7 @@ def format_partitioned_query(
     )
     # extract only partitioned folders
     storage_partitions_dict = parser_blobs_to_partition_dict(blobs)
-
-    # TODO: get last partition using blobs list
+    # get last partition date
     last_partition_date = extract_last_partition_date(storage_partitions_dict)
 
     # Using the last partition date, get the partitioned query.
