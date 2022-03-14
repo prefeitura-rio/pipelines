fec9926d623ac8190bd141d60d9666f68e5095f3
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-03-14 18:04:38 -0300

    update utils with master

--- pipelines/utils/dump_db/utils.py
+++ pipelines/utils/dump_db/utils.py
@@ -3,29 +3,11 @@ Utilities for the Database Dump flows.
 """
 
 from datetime import datetime
-from pathlib import Path
 from typing import List, Tuple
 
 import pandas as pd
 
-from pipelines.utils.utils import log
-
-
-def parser_blobs_to_partition_dict(blobs):
-    """
-    Extracts the partition information from the blobs.
-    """
-    partitions_dict = {}
-    for blob in blobs:
-        for folder in blob.name.split("/"):
-            if "=" in folder:
-                key = folder.split("=")[0]
-                value = folder.split("=")[1]
-                try:
-                    partitions_dict[key].append(value)
-                except KeyError:
-                    partitions_dict[key] = [value]
-    return partitions_dict
+from pipelines.utils.utils import log, remove_columns_accents
 
 
 def extract_last_partition_date(partitions_dict: dict):
@@ -44,68 +26,6 @@ def extract_last_partition_date(partitions_dict: dict):
     return last_partition_date
 
 
-def to_partitions(data: pd.DataFrame, partition_columns: List[str], savepath: str):
-    """Save data in to hive patitions schema, given a dataframe and a list of partition columns.
-    Args:
-        data (pandas.core.frame.DataFrame): Dataframe to be partitioned.
-        partition_columns (list): List of columns to be used as partitions.
-        savepath (str, pathlib.PosixPath): folder path to save the partitions
-    Exemple:
-        data = {
-            "ano": [2020, 2021, 2020, 2021, 2020, 2021, 2021,2025],
-            "mes": [1, 2, 3, 4, 5, 6, 6,9],
-            "sigla_uf": ["SP", "SP", "RJ", "RJ", "PR", "PR", "PR","PR"],
-            "dado": ["a", "b", "c", "d", "e", "f", "g",'h'],
-        }
-        to_partitions(
-            data=pd.DataFrame(data),
-            partition_columns=['ano','mes','sigla_uf'],
-            savepath='partitions/'
-        )
-    """
-
-    if isinstance(data, (pd.core.frame.DataFrame)):
-
-        savepath = Path(savepath)
-
-        # create unique combinations between partition columns
-        unique_combinations = (
-            data[partition_columns]
-            .drop_duplicates(subset=partition_columns)
-            .to_dict(orient="records")
-        )
-
-        for filter_combination in unique_combinations:
-            patitions_values = [
-                f"{partition}={value}"
-                for partition, value in filter_combination.items()
-            ]
-
-            # get filtered data
-            df_filter = data.loc[
-                data[filter_combination.keys()]
-                .isin(filter_combination.values())
-                .all(axis=1),
-                :,
-            ]
-            df_filter = df_filter.drop(columns=partition_columns)
-
-            # create folder tree
-            filter_save_path = Path(savepath / "/".join(patitions_values))
-            filter_save_path.mkdir(parents=True, exist_ok=True)
-            file_filter_save_path = Path(filter_save_path) / "data.csv"
-
-            # append data to csv
-            df_filter.to_csv(
-                file_filter_save_path,
-                index=False,
-                mode="a",
-                header=not file_filter_save_path.exists(),
-            )
-    else:
-        raise BaseException("Data need to be a pandas DataFrame")
-
-
 def parse_date_columns(
     dataframe: pd.DataFrame, partition_date_column: str
 ) -> Tuple[pd.DataFrame, List[str]]:
@@ -118,7 +38,8 @@ def parse_date_columns(
     cols = [ano_col, mes_col, data_col]
     for col in cols:
         if col in dataframe.columns:
-            raise ValueError(f"Column {col} already exists, please review your model.")
+            raise ValueError(
+                f"Column {col} already exists, please review your model.")
 
     dataframe[data_col] = pd.to_datetime(dataframe[partition_date_column])
     dataframe[ano_col] = dataframe[data_col].dt.year
@@ -126,3 +47,16 @@ def parse_date_columns(
     dataframe[data_col] = dataframe[data_col].dt.date
 
     return dataframe, [ano_col, mes_col, data_col]
+
+
+def build_query_new_columns(table_columns: List[str]) -> List[str]:
+    """ "
+    Creates the query without accents.
+    """
+    new_cols = remove_columns_accents(pd.DataFrame(columns=table_columns))
+    return "\n".join(
+        [
+            f"{old_col} AS {new_col},"
+            for old_col, new_col in zip(table_columns, new_cols)
+        ]
+    )
