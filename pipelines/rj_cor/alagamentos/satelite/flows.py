a0c87a06e4c285c57446632e2f9c49b6c06b3634
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-18 09:00:00 -0300

    minor fix

--- pipelines/rj_cor/alagamentos/satelite/flows.py
+++ pipelines/rj_cor/alagamentos/satelite/flows.py
@@ -8,16 +8,12 @@ from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 from pipelines.constants import constants
 from pipelines.rj_cor.alagamentos.satelite.tasks import (slice_data, download,
-                                                      tratar_dados, salvar_parquet)
+                                                         tratar_dados, salvar_parquet)
 from pipelines.rj_cor.alagamentos.satelite.schedules import hour_schedule
 from pipelines.utils.tasks import upload_to_gcs
 
 with Flow('goes_16') as flow:
-    CURRENT_TIME = Parameter('CURRENT_TIME', default=None) or pendulum.now("utc")
-
-
-    if CURRENT_TIME is None:
-        print('Problema no current_time')
+    CURRENT_TIME = Parameter('CURRENT_TIME', default=pendulum.now("utc"))
 
     ano, mes, dia, hora, dia_juliano = slice_data(current_time=CURRENT_TIME)
 
@@ -33,7 +29,6 @@ with Flow('goes_16') as flow:
     info = tratar_dados(filename=filename)
     path = salvar_parquet(info=info)
     upload_to_gcs(path=path, dataset_id=DATASET_ID, table_id=TABLE_ID)
-
 
     # Para taxa de precipitação
     VARIAVEL = 'RRQPEF'
