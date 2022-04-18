be087381f9abcb7df4fa9a96b5879e7e75bb8c08
patriciacatandi <patriciabcatandi@gmail.com>
2022-04-18 09:00:00 -0300

    removendo log

--- pipelines/rj_cor/meteorologia/satelite/tasks.py
+++ pipelines/rj_cor/meteorologia/satelite/tasks.py
@@ -50,11 +50,10 @@ def download(variavel: str, ano: str, dia_juliano: str, hora: str) -> Union[str,
     s3_fs = s3fs.S3FileSystem(anon=True)
 
     # Get first file of GOES-16 data (multiband format) at this time
-    s3_file = f"noaa-goes16/ABI-L2-{variavel}/{ano}/{dia_juliano}/{hora}/"
-    log(">>> s3 file ", s3_file)
+
     file = np.sort(
         np.array(
-            s3_fs.find(s3_file)
+            s3_fs.find(f"noaa-goes16/ABI-L2-{variavel}/{ano}/{dia_juliano}/{hora}/")
         )
     )[0]
 
