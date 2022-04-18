202a5e80ce270a8a8537e3642e6397dbaf295596
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-18 09:00:00 -0300

    add docstring for `get_dbt_client`

--- pipelines/emd/dbt/utils.py
+++ pipelines/emd/dbt/utils.py
@@ -10,6 +10,17 @@ def get_dbt_client(
     port: int = 8580,
     jsonrpc_version: str = "2.0",
 ) -> DbtClient:
+    """
+    Returns a DBT RPC client.
+
+    Args:
+        host: The hostname of the DBT RPC server.
+        port: The port of the DBT RPC server.
+        jsonrpc_version: The JSON-RPC version to use.
+
+    Returns:
+        A DBT RPC client.
+    """
     return DbtClient(
         host=host,
         port=port,
