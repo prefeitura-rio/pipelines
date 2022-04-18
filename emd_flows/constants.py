b1604b5b0070e58ca237d3527f9eae585ea7c05f
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-18 09:00:00 -0300

    initial stuff

---
+++ emd_flows/constants.py
@@ -0,0 +1,21 @@
+"""
+Constants for all flows
+"""
+from enum import Enum
+
+
+class constants(Enum):
+    """
+    Constants used in the EMD flows.
+    """
+    ######################################
+    # Automatically managed,
+    # please do not change these values
+    ######################################
+    DOCKER_TAG = "AUTO_REPLACE_DOCKER_TAG"
+    DOCKER_IMAGE_NAME = "AUTO_REPLACE_DOCKER_IMAGE"
+
+    ######################################
+    # Other constants
+    ######################################
+    DOCKER_IMAGE = f"{DOCKER_IMAGE_NAME}:{DOCKER_TAG}"
