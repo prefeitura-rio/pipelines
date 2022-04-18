a68c5f2b077ffc3e151dd33019c863cff0321efe
patriciacatandi <patriciabcatandi@gmail.com>
2022-04-18 09:00:00 -0300

    arrumando import no flows.py

--- pipelines/satelite/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Prefect flows for satelite project
-"""
-###############################################################################
-# Automatically managed, please do not touch
-###############################################################################
-from .flows import *
