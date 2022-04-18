299c85635d2f815b7c2d840092e798a48fd0b309
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-18 09:00:00 -0300

    fix linting issues

--- pipelines/utils/execute_dbt_model/flows.py
+++ pipelines/utils/execute_dbt_model/flows.py
@@ -47,14 +47,14 @@ with Flow(
     dbt_client = get_k8s_dbt_client(mode=mode, wait=rename_flow_run)
 
     # Run DBT model
-    materialize_this = run_dbt_model(
+    materialize_this = run_dbt_model(  # pylint: disable=invalid-name
         dbt_client=dbt_client,
         dataset_id=dataset_id,
         table_id=table_id,
         sync=True,
     )
 
-    with case(materialize_to_datario):
+    with case(materialize_to_datario, True):
         datario_materialization_flow = create_flow_run(
             flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
             project_name=constants.PREFECT_DEFAULT_PROJECT.value,
