2274cc878f5490f122bbe6ef74cdcb023f0231d0
patriciacatandi <patriciabcatandi@gmail.com>
2022-04-18 09:00:00 -0300

    adiçao de download via ftp

--- pipelines/rj_cor/meteorologia/precipitacao_alertario/flows.py
+++ pipelines/rj_cor/meteorologia/precipitacao_alertario/flows.py
@@ -1,28 +1,24 @@
 """
 Flows for precipitacao_alertario
 """
-import pendulum
 from prefect import Flow
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 from pipelines.constants import constants
-from pipelines.rj_cor.meteorologia.precipitacao_alertario.tasks import (slice_data, download,
+from pipelines.rj_cor.meteorologia.precipitacao_alertario.tasks import (download,
                                                                         tratar_dados, salvar_dados)
 from pipelines.rj_cor.meteorologia.precipitacao_alertario.schedules import minute_schedule
 from pipelines.utils.tasks import upload_to_gcs
 
 with Flow('COR: Meteorologia - Pluviometria ALERTARIO') as flow:
-    CURRENT_TIME = pendulum.now('America/Sao_Paulo')
 
     DATASET_ID = 'meio_ambiente_clima'
-    TABLE_ID = 'meteorologia_inmet'
+    TABLE_ID = 'precipitacao_alertario'
 
-    data, hora, minuto = slice_data(current_time=CURRENT_TIME)
-
-    dados = download(data=data)
-    dados = tratar_dados(dados=dados, hora=hora)
-    path = salvar_dados(dados=dados)
-    upload_to_gcs(path=path, dataset_id=DATASET_ID, table_id=TABLE_ID)
+    filename, current_time = download()
+    dados = tratar_dados(filename=filename)
+    path, partitions = salvar_dados(dados=dados, current_time=current_time)
+    upload_to_gcs(path=path, dataset_id=DATASET_ID, table_id=TABLE_ID, partitions=partitions)
 
 # para rodar na cloud
 flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
--- pipelines/rj_cor/meteorologia/precipitacao_alertario/tasks.py
+++ pipelines/rj_cor/meteorologia/precipitacao_alertario/tasks.py
@@ -1,103 +1,114 @@
 """
 Tasks for precipitacao_alertario
 """
-
-from datetime import datetime
-import json
-import math
+from datetime import timedelta
+import ftplib
 import os
 from pathlib import Path
-from typing import Tuple, Union
+import socket
+from typing import Union, Tuple
 
 import numpy as np
 import pandas as pd
 import pendulum
 from prefect import task
-import requests
 
-from pipelines.utils import log
+from pipelines.constants import constants
 
 ###################################################################################
 ######## Ver de trocar o nome da coluna data_medição e deixar padronizado #########
 ###################################################################################
 
-
-def nearest_floor_multiple(base:int, number: int) -> int:
-    """
-    Round `a_number` to a multiple of `base` considering the
-    floor on base
-
-    base: int
-    just multiples of this base are considered
-    number: int
-    number to be approximated
-
-    Returns:
-        _type_: _description_
-    """
-    return base * math.floor(number/base)
-
-
-@task(nout=2)
-def slice_data(current_time: str) -> Tuple[str, str]:
-    '''
-    Retorna a data e hora do timestamp de execução
-    '''
-    if not isinstance(current_time,str):
-        current_time = current_time.to_datetime_string()
-
-    current_time = current_time or pendulum.now("utc").to_datetime_string()
-
-    data = current_time[:10]
-    hora = current_time[11:13]
-    minuto = current_time[14:16]
-
-    # Não importa que minuto rodar (se atrasou alguns minutos
-    # por exemplo) quero que traga o dado imediatamente anterior
-    # consierando que a informação é atualizada a cada 5 min
-    minuto = nearest_floor_multiple(5, minuto)
-    return data, hora, minuto
-
-
-@task
-def download(data: str) -> pd.DataFrame:
+@task(nout=2,
+      max_retries=constants.TASK_MAX_RETRIES.value,
+      retry_delay=timedelta(seconds=constants.TASK_RETRY_DELAY.value),
+    )
+def download() -> Tuple[pd.DataFrame, str]:
     '''
     Faz o request na data especificado e retorna dados
     Tentando conseguir com o sardinha o ftp que permite
     termos os dados atualizados a cada 5 minutos
     '''
-    dados=0
-    pass
+    # Acessar FTP Riomidia
+    #### colocar no .env
+    host = '187.111.97.195'
+    dirname = '/alertario/'
+    filename = 'EstacoesMapa.txt'
+    username = 'escdados'
+    password = ''
 
-    return dados
+    base_path = os.path.join(os.getcwd(),'data', 'precipitacao_alertario', 'input')
+
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+
+    save_on = os.path.join(base_path, filename)
+
+    try:
+        ftp = ftplib.FTP(host)
+    except (socket.error, socket.gaierror):
+        print(f'ERROR: cannot reach {host}')
+        raise
+    print(f'*** Connected to host {host}')
+
+    try:
+        ftp.login(username, password)
+    except ftplib.error_perm:
+        print('ERROR: cannot login')
+        ftp.quit()
+        raise
+    print('*** Logged in successfully')
+
+    try:
+        ftp.cwd(dirname)
+    except ftplib.error_perm:
+        print(f'ERROR: cannot CD to {dirname}')
+        ftp.quit()
+        raise
+    print(f'*** Changed to folder: {dirname}')
+
+    try:
+        with open(save_on, 'wb') as file:
+            print('Getting ' + filename)
+            ftp.retrbinary('RETR ' + filename, file.write)
+        print(f'File downloaded to {save_on}')
+    except ftplib.error_perm :
+        print(f'ERROR: cannot read file {filename}')
+        raise
+
+    ftp.quit()
+
+    # Hora atual no formato YYYYMMDDHHmm
+    current_time = pendulum.now('America/Sao_Paulo').strftime("%Y%m%d%H%M")
+
+    return save_on, current_time
 
 
 @task
-def tratar_dados(dados: pd.DataFrame, hora: str) -> pd.DataFrame:
+def tratar_dados(filename: Union[str, Path]) -> pd.DataFrame:
     '''
     Renomeia colunas e filtra dados com a hora e minuto do timestamp
-    de execução mais próximo à este 
+    de execução mais próximo à este
     '''
-    # Adequando nome das variáveis
-    rename_cols = {'ts':'data_medicao',
-                   'acumulado_15_min':'acumulado_chuva_15_min',
-                   'acumulado_1_h':'acumulado_chuva_1_h',
-                   'acumulado_4_h':'acumulado_chuva_4_h',
-                   'acumulado_24_h':'acumulado_chuva_24_h',
-                   'acumulado_96_h':'acumulado_chuva_96_h'
-                    }
+    colunas = ['id', 'estacao', 'localizacao', 'data_medicao', 'latitude', 'longitude',
+                'acumulado_chuva_15_min', 'acumulado_chuva_1_h',
+                'acumulado_chuva_4_h', 'acumulado_chuva_24_h',
+                'acumulado_chuva_96_h', 'acumulado_chuva_mes']
 
-    dados = dados.rename(columns=rename_cols)
+    dados = pd.read_csv(filename, skiprows=1, sep=';', names=colunas, decimal=',')
+
+    # Adequando formato de data
+    dados['data_medicao'] = pd.to_datetime(dados['data_medicao'], format='%H:%M - %d/%m/%Y')
+
 
     # Ordenação de variáveis
-    cols_order = ['ano', 
-                  'mes', 
+    cols_order = [
                   'data_medicao',
-                  'estacao', 
+                  'estacao',
                   'acumulado_chuva_15_min',
-                  'acumulado_chuva_1_h', 
-                  'acumulado_chuva_4_h', 
-                  'acumulado_chuva_24_h', 
+                  'acumulado_chuva_1_h',
+                  'acumulado_chuva_4_h',
+                  'acumulado_chuva_24_h',
                   'acumulado_chuva_96_h']
 
     dados = dados[cols_order]
@@ -110,27 +121,37 @@ def tratar_dados(dados: pd.DataFrame, hora: str) -> pd.DataFrame:
 
     # Converte variáveis que deveriam ser float para float
     float_cols = ['acumulado_chuva_15_min',
-                  'acumulado_chuva_1_h', 
-                  'acumulado_chuva_4_h', 
-                  'acumulado_chuva_24_h', 
+                  'acumulado_chuva_1_h',
+                  'acumulado_chuva_4_h',
+                  'acumulado_chuva_24_h',
                   'acumulado_chuva_96_h']
     dados[float_cols] = dados[float_cols].apply(pd.to_numeric, errors='coerce')
 
     return dados
 
 
-@task
-def salvar_dados(dados: pd.DataFrame) -> Union[str, Path]:
+@task(nout=2)
+def salvar_dados(dados: pd.DataFrame, current_time: str) -> Tuple[Union[str, Path], str]:
     '''
     Salvar dados em csv
     '''
-    base_path = os.path.join(os.getcwd(),'data', 'precipitacao_alertario', 'output')
 
+    ano = current_time[:4]
+    mes = str(int(current_time[4:6]))
+    dia = str(int(current_time[6:8]))
+    partitions = os.path.join(f'ano={ano}', f'mes={mes}', f'dia={dia}')
+
+    base_path = os.path.join(os.getcwd(),
+                             'data',
+                             'precipitacao_alertario',
+                             'output',
+                             partitions)
+    
     if not os.path.exists(base_path):
         os.makedirs(base_path)
 
-    filename = os.path.join(base_path, 'dados.csv')
+    filename = os.path.join(base_path, f'dados_{current_time}.csv')
 
     print(f'Saving {filename}')
     dados.to_csv(filename, index=False)
-    return filename
+    return filename, partitions
