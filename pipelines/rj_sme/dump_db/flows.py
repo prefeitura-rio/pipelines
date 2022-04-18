eb0f21e0247ab22cf4dfaf6c810d7ae7c2e1ae0e
d116626 <d116626@gmail.com>
2022-04-18 09:00:00 -0300

    feat: try some default parameters for pipelines

--- pipelines/rj_sme/dump_db/flows.py
+++ pipelines/rj_sme/dump_db/flows.py
@@ -4,6 +4,7 @@ Database dumping flows for sme project
 
 from copy import deepcopy
 
+from prefect import Parameter
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
@@ -13,7 +14,14 @@ from pipelines.utils.dump_db.flows import dump_sql_flow
 
 
 dump_sme_flow = deepcopy(dump_sql_flow)
+with dump_sql_flow as dump_sql_flow:
+    hostname = Parameter("db_host", default='"10.70.6.103"')
+    port = Parameter("db_port", default="1433")
+    database = Parameter("db_database", default="GestaoEscolar")
+    secret_path = Parameter("vault_secret_path", default="clustersqlsme")
+
 dump_sme_flow.name = "SME: educacao_basica - Ingerir tabelas de banco SQL"
 dump_sme_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 dump_sme_flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 dump_sme_flow.schedule = sme_educacao_basica_daily_update_schedule
+dump_sme_flow.p
