79f073484600eb2b3fabd1c03cf1ea399929d23e
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-18 09:00:00 -0300

    fix schedule for dbt materialization

--- pipelines/rj_segovi/execute_dbt_model/schedules.py
+++ pipelines/rj_segovi/execute_dbt_model/schedules.py
@@ -4,63 +4,27 @@ Schedules for the database dump pipeline
 """
 
 from datetime import timedelta, datetime
-
 from prefect.schedules import Schedule
-from prefect.schedules.clocks import IntervalClock
 import pytz
+
 
 from pipelines.constants import constants
 from pipelines.utils.utils import untuple_clocks as untuple
+from pipelines.utils.execute_dbt_model.utils import generate_execute_dbt_model_schedules
 
-
-def get_clock(dataset_id, table_id, count):
-    """
-    Returns a clock for the given dataset and table with an offset of
-    `count` * 3 minutes.
-    """
-    return IntervalClock(
-        interval=timedelta(days=30),
-        start_date=datetime(
-            2022, 2, 15, 12, 5, tzinfo=pytz.timezone("America/Sao_Paulo")
-        )
-        + timedelta(minutes=3 * count),
-        labels=[
-            constants.EMD_AGENT_LABEL.value,
-        ],
-        parameter_defaults={
-            "dataset_id": dataset_id,
-            "table_id": table_id,
-            "mode": "prod",
-        },
-    )
-
-
-#####################################
-#
-# 1746 Schedules
-#
-#####################################
 
 _1746_views = {
-    "chamado": "chamado",
+    "chamado": {"dataset_id": "administracao_servicos_publicos_1746", "mode": "prod"},
 }
 
-_1746_clocks = [
-    IntervalClock(
-        interval=timedelta(days=30),
-        start_date=datetime(
-            2022, 2, 15, 12, 5, tzinfo=pytz.timezone("America/Sao_Paulo")
-        )
-        + timedelta(minutes=3 * count),
-        labels=[
-            constants.EMD_AGENT_LABEL.value,
-        ],
-        parameter_defaults={
-            "dataset_id": "administracao_servicos_publicos_1746",
-            "table_id": table_id,
-            "mode": "prod",
-        },
-    )
-    for count, (_, table_id) in enumerate(_1746_views.items())
-]
+_1746_clocks = generate_execute_dbt_model_schedules(
+    interval=timedelta(days=30),
+    start_date=datetime(2022, 3, 21, 4, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[
+        constants.RJ_SEGOVI_AGENT_LABEL.value,
+    ],
+    table_parameters=_1746_views,
+    runs_interval_minutes=5,
+)
+
 _1746_monthly_update_schedule = Schedule(clocks=untuple(_1746_clocks))
