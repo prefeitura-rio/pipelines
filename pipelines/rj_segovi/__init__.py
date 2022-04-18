ea4bdf3624a85f86709c91b336590ab9cd35dc8f
d116626 <d116626@gmail.com>
2022-04-18 09:00:00 -0300

    feat(infra): refactor pipelines to new structure

--- pipelines/rj_segovi/__init__.py
+++ pipelines/rj_segovi/__init__.py
@@ -3,3 +3,4 @@ Prefect flows for segovi project
 """
 
 from pipelines.rj_segovi.dump_db.flows import *
+from pipelines.rj_segovi.execute_dbt_model.flows import *
--- pipelines/rj_sme/__init__.py
+++ pipelines/rj_sme/__init__.py
@@ -3,3 +3,4 @@ Prefect flows for sme project
 """
 
 from pipelines.rj_sme.dump_db.flows import *
+from pipelines.rj_sme.execute_dbt_model.flows import *
