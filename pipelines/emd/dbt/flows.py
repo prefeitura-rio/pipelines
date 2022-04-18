801b72aa84577408d140125caf03164212eca99f
d116626 <d116626@gmail.com>
2022-04-18 09:00:00 -0300

    fix(infra): rename ergon workflow

--- pipelines/emd/dbt/flows.py
+++ pipelines/emd/dbt/flows.py
@@ -38,7 +38,7 @@ run_dbt_model_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 run_dbt_model_flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 
 run_dbt_ergon_flow = deepcopy(run_dbt_model_flow)
-run_dbt_ergon_flow.name = "EMD: ergon - Materializar tabelas Ergon"
+run_dbt_ergon_flow.name = "EMD: ergon - Materializar tabelas"
 run_dbt_ergon_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 run_dbt_ergon_flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 run_dbt_ergon_flow.schedule = ergon_monthly_update_schedule
