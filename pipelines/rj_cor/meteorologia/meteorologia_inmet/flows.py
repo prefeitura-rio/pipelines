1c091bc7ca08326667fbcb99956f5ebd33de2c29
patriciacatandi <patriciabcatandi@gmail.com>
2022-04-18 09:00:00 -0300

    adição de notify on discord

--- pipelines/rj_cor/meteorologia/meteorologia_inmet/flows.py
+++ pipelines/rj_cor/meteorologia/meteorologia_inmet/flows.py
@@ -2,6 +2,8 @@
 """
 Flows for meteorologia_inmet
 """
+from functools import partial
+
 from prefect import Flow
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
@@ -15,9 +17,14 @@ from pipelines.rj_cor.meteorologia.meteorologia_inmet.tasks import (
 )
 from pipelines.rj_cor.meteorologia.meteorologia_inmet.schedules import hour_schedule
 from pipelines.utils.tasks import create_table_and_upload_to_gcs
+from pipelines.utils.utils import notify_discord_on_failure
 
 with Flow(
-    "COR: Meteorologia - Meteorologia INMET"
+    name="COR: Meteorologia - Meteorologia INMET",
+    on_failure=partial(
+        notify_discord_on_failure,
+        secret_path=constants.EMD_DISCORD_WEBHOOK_SECRET_PATH.value,
+    ),
 ) as cor_meteorologia_meteorologia_inmet:
 
     DATASET_ID = "meio_ambiente_clima"
