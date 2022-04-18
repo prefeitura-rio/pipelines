f99015f75b2d077d832568ffd2958de43f477109
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-18 09:00:00 -0300

    fix minor linting issues

--- pipelines/rj_escritorio/seconserva_buracos_refresh_data/flows.py
+++ pipelines/rj_escritorio/seconserva_buracos_refresh_data/flows.py
@@ -5,13 +5,14 @@ DBT-related flows.
 
 from copy import deepcopy
 
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+
 from pipelines.constants import constants
 from pipelines.rj_escritorio.seconserva_buracos_refresh_data.schedules import (
     seconserva_buracos_daily_update_schedule,
 )
 from pipelines.utils.execute_dbt_model.flows import run_dbt_model_flow
-from prefect.run_configs import KubernetesRun
-from prefect.storage import GCS
 
 run_dbt_seconserva_buracos_flow = deepcopy(run_dbt_model_flow)
 run_dbt_seconserva_buracos_flow.name = "EMD: seconserva_buracos - Materializar tabelas"
