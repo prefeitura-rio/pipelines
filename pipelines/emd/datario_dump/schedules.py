f3b48064f71175ed525bea24576687a19d232057
d116626 <d116626@gmail.com>
2022-04-18 09:00:00 -0300

    feat(infra): add datario dump flow

---
+++ pipelines/emd/datario_dump/schedules.py
@@ -0,0 +1,44 @@
+"""
+Schedules for the database dump pipeline
+"""
+
+from datetime import timedelta, datetime
+
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
+import pytz
+
+from pipelines.constants import constants
+from pipelines.utils import query_to_line, untuple_clocks as untuple
+
+
+#####################################
+#
+# datario Schedules
+#
+#####################################
+
+diretorio_tables = {
+    "bairro": "https://opendata.arcgis.com/datasets/dc94b29fc3594a5bb4d297bee0c9a3f2_3.geojson",
+    "logradouro": "https://opendata.arcgis.com/datasets/899168c8feab4230a9f795ed07cdde7b_0.geojson",
+}
+
+diretorio_clocks = [
+    IntervalClock(
+        interval=timedelta(days=30),
+        start_date=datetime(2022, 3, 5, 1, 0, tzinfo=pytz.timezone("America/Sao_Paulo"))
+        + timedelta(minutes=3 * count),
+        labels=[
+            constants.EMD_AGENT_LABEL.value,
+        ],
+        parameter_defaults={
+            "url": url,
+            "dataset_id": "diretorio",
+            "dump_type": "overwrite",
+            "table_id": table_id,
+        },
+    )
+    for count, (table_id, url) in enumerate(diretorio_tables.items())
+]
+
+diretorio_monthly_update_schedule = Schedule(clocks=untuple(diretorio_clocks))
