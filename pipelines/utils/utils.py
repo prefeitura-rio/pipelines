3af9d0a9ddd808613a966ff60351aeaa62e6f503
d116626 <d116626@gmail.com>
2022-04-18 09:00:00 -0300

    fix: minor refactor

--- pipelines/utils/utils.py
+++ pipelines/utils/utils.py
@@ -7,6 +7,7 @@ from os import getenv
 from pathlib import Path
 from typing import Any, Dict, List, Tuple, Union
 
+import basedosdados as bd
 import hvac
 import numpy as np
 import pandas as pd
@@ -268,8 +269,101 @@ def remove_columns_accents(dataframe: pd.DataFrame) -> list:
     )
 
 
+def to_partitions(data: pd.DataFrame, partition_columns: List[str], savepath: str):
+    """Save data in to hive patitions schema, given a dataframe and a list of partition columns.
+    Args:
+        data (pandas.core.frame.DataFrame): Dataframe to be partitioned.
+        partition_columns (list): List of columns to be used as partitions.
+        savepath (str, pathlib.PosixPath): folder path to save the partitions
+    Exemple:
+        data = {
+            "ano": [2020, 2021, 2020, 2021, 2020, 2021, 2021,2025],
+            "mes": [1, 2, 3, 4, 5, 6, 6,9],
+            "sigla_uf": ["SP", "SP", "RJ", "RJ", "PR", "PR", "PR","PR"],
+            "dado": ["a", "b", "c", "d", "e", "f", "g",'h'],
+        }
+        to_partitions(
+            data=pd.DataFrame(data),
+            partition_columns=['ano','mes','sigla_uf'],
+            savepath='partitions/'
+        )
+    """
+
+    if isinstance(data, (pd.core.frame.DataFrame)):
+
+        savepath = Path(savepath)
+
+        # create unique combinations between partition columns
+        unique_combinations = (
+            data[partition_columns]
+            .drop_duplicates(subset=partition_columns)
+            .to_dict(orient="records")
+        )
+
+        for filter_combination in unique_combinations:
+            patitions_values = [
+                f"{partition}={value}"
+                for partition, value in filter_combination.items()
+            ]
+
+            # get filtered data
+            df_filter = data.loc[
+                data[filter_combination.keys()]
+                .isin(filter_combination.values())
+                .all(axis=1),
+                :,
+            ]
+            df_filter = df_filter.drop(columns=partition_columns)
+
+            # create folder tree
+            filter_save_path = Path(savepath / "/".join(patitions_values))
+            filter_save_path.mkdir(parents=True, exist_ok=True)
+            file_filter_save_path = Path(filter_save_path) / "data.csv"
+
+            # append data to csv
+            df_filter.to_csv(
+                file_filter_save_path,
+                index=False,
+                mode="a",
+                header=not file_filter_save_path.exists(),
+            )
+    else:
+        raise BaseException("Data need to be a pandas DataFrame")
+
+
 ###############
 #
-# File
+# Storage utils
 #
 ###############
+
+
+def get_storage_blobs(dataset_id: str, table_id: str) -> list:
+    """
+    Get all blobs from a table in a dataset.
+    """
+
+    storage = bd.Storage(dataset_id=dataset_id, table_id=table_id)
+    return list(
+        storage.client["storage_staging"]
+        .bucket(storage.bucket_name)
+        .list_blobs(prefix=f"staging/{storage.dataset_id}/{storage.table_id}/")
+    )
+
+
+def parser_blobs_to_partition_dict(blobs: list) -> dict:
+    """
+    Extracts the partition information from the blobs.
+    """
+
+    partitions_dict = {}
+    for blob in blobs:
+        for folder in blob.name.split("/"):
+            if "=" in folder:
+                key = folder.split("=")[0]
+                value = folder.split("=")[1]
+                try:
+                    partitions_dict[key].append(value)
+                except KeyError:
+                    partitions_dict[key] = [value]
+    return partitions_dict
