1aedb50db7ae01cfe214c6e2209fa9b86173b4bb
Bruno Almeida <brunodepauloalmeida@gmail.com>
2022-04-18 09:00:00 -0300

    Changing name of variable not to duplicate flows already running

--- pipelines/rj_escritorio/seconserva_buracos_refresh_data/flows.py
+++ pipelines/rj_escritorio/seconserva_buracos_refresh_data/flows.py
@@ -13,8 +13,10 @@ from pipelines.utils.execute_dbt_model.flows import run_dbt_model_flow
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
-run_dbt_sme_flow = deepcopy(run_dbt_model_flow)
-run_dbt_sme_flow.name = "EMD: seconserva_buracos - Materializar tabelas"
-run_dbt_sme_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
-run_dbt_sme_flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
-run_dbt_sme_flow.schedule = seconserva_buracos_daily_update_schedule
+run_dbt_seconserva_buracos_flow = deepcopy(run_dbt_model_flow)
+run_dbt_seconserva_buracos_flow.name = "EMD: seconserva_buracos - Materializar tabelas"
+run_dbt_seconserva_buracos_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+run_dbt_seconserva_buracos_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value
+)
+run_dbt_seconserva_buracos_flow.schedule = seconserva_buracos_daily_update_schedule
