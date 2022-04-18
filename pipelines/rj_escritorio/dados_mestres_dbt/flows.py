edba10ff1d3b83f03e701fdb3622d230f6f0f26c
Bruno Almeida <brunodepauloalmeida@gmail.com>
2022-04-18 09:00:00 -0300

    Adicionando pipeline que materializa enderecos_geolocalizados

--- pipelines/rj_escritorio/dados_mestres_dbt/flows.py
+++ pipelines/rj_escritorio/dados_mestres_dbt/flows.py
@@ -5,17 +5,32 @@ DBT-related flows.
 
 from copy import deepcopy
 
-from prefect.run_configs import KubernetesRun
-from prefect.storage import GCS
-
 from pipelines.constants import constants
-from pipelines.utils.execute_dbt_model.flows import run_dbt_model_flow
 from pipelines.rj_escritorio.dados_mestres_dbt.schedules import (
+    dados_mestresdaily_update_schedule,
     dados_mestresweekly_update_schedule,
 )
+from pipelines.utils.execute_dbt_model.flows import run_dbt_model_flow
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
 
 run_dbt_dados_mestresflow = deepcopy(run_dbt_model_flow)
 run_dbt_dados_mestresflow.name = "EMD: dados_mestres - Materializar tabelas DBT"
 run_dbt_dados_mestresflow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 run_dbt_dados_mestresflow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 run_dbt_dados_mestresflow.schedule = dados_mestresweekly_update_schedule
+
+# Pipeline da tabela enderecos_geolocalizados
+run_dbt_dados_mestres_enderecos_geolocalizados_flow = deepcopy(run_dbt_model_flow)
+run_dbt_dados_mestres_enderecos_geolocalizados_flow.name = (
+    "EMD: dados_mestres.enderecos_geolocalizados - Materializar tabelas DBT"
+)
+run_dbt_dados_mestres_enderecos_geolocalizados_flow.storage = GCS(
+    constants.GCS_FLOWS_BUCKET.value
+)
+run_dbt_dados_mestres_enderecos_geolocalizados_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value
+)
+run_dbt_dados_mestres_enderecos_geolocalizados_flow.schedule = (
+    dados_mestresdaily_update_schedule
+)
--- pipelines/rj_escritorio/dados_mestres_dbt/schedules.py
+++ pipelines/rj_escritorio/dados_mestres_dbt/schedules.py
@@ -3,15 +3,13 @@
 Schedules for the database dump pipeline
 """
 
-from datetime import timedelta, datetime
-from prefect.schedules import Schedule
+from datetime import datetime, timedelta
+
 import pytz
-
-
 from pipelines.constants import constants
-from pipelines.utils.utils import untuple_clocks as untuple
 from pipelines.utils.execute_dbt_model.utils import generate_execute_dbt_model_schedules
-
+from pipelines.utils.utils import untuple_clocks as untuple
+from prefect.schedules import Schedule
 
 dados_mestresviews = {
     "bairro": {"dataset_id": "dados_mestres", "mode": "dev"},
@@ -29,3 +27,19 @@ dados_mestresclocks = generate_execute_dbt_model_schedules(
 )
 
 dados_mestresweekly_update_schedule = Schedule(clocks=untuple(dados_mestresclocks))
+
+dados_mestres_enderecos_geolocalizados = {
+    "enderecos_geolocalizados": {"dataset_id": "dados_mestres", "mode": "dev"},
+}
+
+dados_mestresclocks = generate_execute_dbt_model_schedules(
+    interval=timedelta(days=1),
+    start_date=datetime(2022, 3, 21, 3, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[
+        constants.RJ_ESCRITORIO_DEV_AGENT_LABEL.value,
+    ],
+    table_parameters=dados_mestres_enderecos_geolocalizados,
+    runs_interval_minutes=5,
+)
+
+dados_mestresdaily_update_schedule = Schedule(clocks=untuple(dados_mestresclocks))
