5c94aea8c9952f2513098f28da8f9d20be341f49
d116626 <d116626@gmail.com>
2022-04-18 09:00:00 -0300

    feat(infra): add how to create a pipeline in readme

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 Esse repositório contém flows desenvolvidos com Prefect relacionados ao Escritório Municipal de Dados da Prefeitura do Rio de Janeiro.
 
+----
+
+
+## Criando uma nova pipeline
+
+1. Criar uma pasta com o nome do organização e o arquivo `__init__.py`
+
+2. Criar a pipeline na pasta da organização `pipelines/<organizacao>/<nova_pipeline>`, caso seja a captura de uma base a pasta deve ser o nome do `dataset_id`. 
+
+3. Na pasta da nova pipeline, devem ser criados os arquivos `flows.py`, `tasks.py`, `schedules.py` e `__init__.py`, como no exemplo da pipeline [template_pipeline](/pipelines/emd/template_pipeline/).
+
+4. Os `flows` da nova pipeline deve ser importado no arquivo `pipelines/<organizacao>/__init__.py`, como no exemplo [\_\_init\_\_.py](/pipelines/emd/__init__.py)
+
 
 ## Como rodar uma pipeline localmente
 
@@ -14,6 +27,35 @@ from pipelines.emd.test_flow.flows import flow
 run_local(flow, parameters = {"param": "val"})
 ```
 
+----
+
+## Como acessar a UI do prefect
+
+Requisitos
+Conta no GitHub
+Ser membro da organização prefeitura-rio no GitHub
+Preparo inicial
+Primeiramente, deve-se ingressar no Tailscale usando sua conta do GitHub. Para isso, acesse https://login.tailscale.com/ e clique em “Sign in with GitHub”
+
+Em seguida, autorize o acesso solicitado.
+
+Depois, você deve escolher qual “Tailnet” utilizar. Nesse caso, escolha a “basedosdados”:
+
+Dessa forma, você terá permissão para se conectar à VPN.
+
+Nessa página você encontrará instruções de instalação do Tailscale para as diversas plataformas suportadas (macOS, iOS, Windows, Linux e Android). Por serem muitas, as instruções individuais para elas não estão compreendidas nesse documento.
+Conectando-se
+Como dito anteriormente, não serão explicitados os métodos para todas as plataformas suportadas. Caso haja dúvida, é possível recorrer ao site do Tailscale ou solicitar ajuda à equipe do Escritório Municipal de Dados. Assim, então, os próximos passos considerarão um sistema Linux para uso.
+
+O comando que deve ser executado para acesso pleno à VPN é
+
+```
+sudo tailscale up --accept-routes --accept-dns
+```
+
+Caso seja solicitado o acesso a um link para autenticação, favor fazê-lo. Assim que o acesso for autorizado, será possível acessar a interface web do Prefect no endereço http://prefect-ui.prefect.svc.cluster.local:8080/.
+
+----
 
 ## Como desenvolver
 
@@ -39,3 +81,12 @@ python manage.py list-projects
 
 Em seguida, leia com anteção os comentários em cada um dos arquivos do seu projeto, de modo a evitar conflitos e erros.
 Links para a documentação do Prefect também encontram-se nos comentários.
+
+----
+
+### Requisitos
+
+- Requisitos Python em tempo de execução devem ser adicionados ao `pyproject.toml` na raiz desse repositório.
+- Requisitos do `manage.py` estão em `requirements-cli.txt`
+- Requisitos para a Action de deployment estão em `requirements-deploy.txt`
+- Requisitos para testes estão em `requirements-tests.txt`
---
+++ pipelines/emd/template_pipeline/schedules.py
@@ -0,0 +1,87 @@
+"""
+Schedules for basedosdados
+"""
+
+###############################################################################
+#
+# Aqui é onde devem ser definidos os schedules para os flows do projeto.
+# Cada schedule indica o intervalo de tempo entre as execuções.
+# Um schedule pode ser definido para um ou mais flows.
+# Mais informações sobre schedules podem ser encontradas na documentação do
+# Prefect: https://docs.prefect.io/core/concepts/schedules.html
+#
+# De modo a manter consistência na codebase, todo o código escrito passará
+# pelo pylint. Todos os warnings e erros devem ser corrigidos.
+#
+# Os schedules devem ser definidos de acordo com a sintaxe do Prefect, como,
+# por exemplo, o seguinte (para executar a cada 1 minuto):
+#
+# -----------------------------------------------------------------------------
+# from datetime import timedelta, datetime
+# from prefect.schedules import Schedule
+# from prefect.schedules.clocks import IntervalClock
+# from pipelines.constants import constants
+#
+# minute_schedule = Schedule(
+#     clocks=[
+#         IntervalClock(
+#             interval=timedelta(minutes=1),
+#             start_date=datetime(2021, 1, 1),
+#             labels=[
+#                 constants.BASEDOSDADOS_AGENT_LABEL.value,
+#             ]
+#         ),
+#     ]
+# )
+# -----------------------------------------------------------------------------
+#
+# Vale notar que o parâmetro `labels` é obrigatório e deve ser uma lista com
+# apenas um elemento, correspondendo ao label do agente que será executado.
+# O label do agente é definido em `constants.py` e deve ter o formato
+# `BASEDOSDADOS_AGENT_LABEL`.
+#
+# Outro exemplo, para executar todos os dias à meia noite, segue abaixo:
+#
+# -----------------------------------------------------------------------------
+# from prefect import task
+# from datetime import timedelta
+# import pendulum
+# from prefect.schedules import Schedule
+# from prefect.schedules.clocks import IntervalClock
+# from pipelines.constants import constants
+#
+# every_day_at_midnight = Schedule(
+#     clocks=[
+#         IntervalClock(
+#             interval=timedelta(days=1),
+#             start_date=pendulum.datetime(
+#                 2021, 1, 1, 0, 0, 0, tz="America/Sao_Paulo"),
+#             labels=[
+#                 constants.BASEDOSDADOS_AGENT_LABEL.value,
+#             ]
+#         )
+#     ]
+# )
+# -----------------------------------------------------------------------------
+#
+# Abaixo segue um código para exemplificação, que pode ser removido.
+#
+###############################################################################
+
+
+from datetime import timedelta, datetime
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
+from pipelines.constants import constants
+
+every_two_weeks = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(weeks=2),
+            start_date=datetime(2021, 1, 1),
+            labels=[
+                constants.BASEDOSDADOS_AGENT_LABEL.value,
+            ]
+        ),
+    ]
+)
