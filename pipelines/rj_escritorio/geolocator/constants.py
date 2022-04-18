3a7aa29b8a5835e94341150934a0e6a4b03c86d1
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-18 09:00:00 -0300

    start geolocator project

---
+++ pipelines/rj_escritorio/geolocator/constants.py
@@ -0,0 +1,40 @@
+"""
+Constant values for the rj_escritorio projects
+"""
+
+
+###############################################################################
+#
+# Esse é um arquivo onde podem ser declaratas constantes que serão usadas
+# pelo projeto geolocator.
+#
+# Por ser um arquivo opcional, pode ser removido sem prejuízo ao funcionamento
+# do projeto, caos não esteja em uso.
+#
+# Para declarar constantes, basta fazer conforme o exemplo abaixo:
+#
+# ```
+# class constants(Enum):
+#     """
+#     Constant values for the geolocator project
+#     """
+#     FOO = "bar"
+# ```
+#
+# Para usá-las, basta fazer conforme o exemplo abaixo:
+#
+# ```py
+# from pipelines.rj_escritorio.geolocator.constants import constants
+# print(constants.FOO.value)
+# ```
+#
+###############################################################################
+
+from enum import Enum
+
+
+class constants(Enum):  # pylint: disable=c0103
+    """
+    Constant values for the geolocator project
+    """
+    FOO = "bar"
---
+++ pipelines/rj_escritorio/geolocator/utils.py
@@ -0,0 +1,30 @@
+"""
+General purpose functions for the geolocator project
+"""
+
+###############################################################################
+#
+# Esse é um arquivo onde podem ser declaratas funções que serão usadas
+# pelo projeto geolocator.
+#
+# Por ser um arquivo opcional, pode ser removido sem prejuízo ao funcionamento
+# do projeto, caos não esteja em uso.
+#
+# Para declarar funções, basta fazer em código Python comum, como abaixo:
+#
+# ```
+# def foo():
+#     """
+#     Function foo
+#     """
+#     print("foo")
+# ```
+#
+# Para usá-las, basta fazer conforme o exemplo abaixo:
+#
+# ```py
+# from pipelines.rj_escritorio.geolocator.utils import foo
+# foo()
+# ```
+#
+###############################################################################
