da3b50aab6be00dd853fcf79d0646447bab669c9
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-28 11:19:37 -0300

    accept multiple partition columns

--- pipelines/utils/dump_db/flows.py
+++ pipelines/utils/dump_db/flows.py
@@ -30,6 +30,7 @@ from pipelines.utils.dump_db.tasks import (
     database_get,
     dump_batches_to_csv,
     format_partitioned_query,
+    parse_comma_separated_string_to_list,
 )
 
 with Flow(
@@ -52,7 +53,7 @@ with Flow(
     database = Parameter("db_database")
     database_type = Parameter("db_type")
     query = Parameter("execute_query")
-    partition_column = Parameter("partition_column", required=False)
+    partition_columns = Parameter("partition_columns", required=False, default="")
     lower_bound_date = Parameter("lower_bound_date", required=False)
 
     # Materialization parameters
@@ -102,6 +103,9 @@ with Flow(
     #
     #####################################
 
+    # Parse partition columns
+    partition_columns = parse_comma_separated_string_to_list(text=partition_columns)
+
     # Execute query on SQL Server
     db_object: Database = database_get(
         database_type=database_type,
@@ -118,7 +122,7 @@ with Flow(
         query=query,
         dataset_id=dataset_id,
         table_id=table_id,
-        partition_column=partition_column,
+        partition_column=partition_columns,
         lower_bound_date=lower_bound_date,
         wait=db_object,
     )
@@ -134,7 +138,7 @@ with Flow(
         database=db_object,
         batch_size=batch_size,
         prepath=f"data/{uuid4()}/",
-        partition_column=partition_column,
+        partition_column=partition_columns,
         wait=db_execute,
     )
 
--- pipelines/utils/dump_db/tasks.py
+++ pipelines/utils/dump_db/tasks.py
@@ -4,7 +4,7 @@ General purpose tasks for dumping database data.
 """
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Dict, Union
+from typing import Dict, List, Union
 from uuid import uuid4
 
 import basedosdados as bd
@@ -136,7 +136,7 @@ def format_partitioned_query(
     query: str,
     dataset_id: str,
     table_id: str,
-    partition_column: str = None,
+    partition_columns: List[str] = None,
     lower_bound_date: str = None,
     wait=None,  # pylint: disable=unused-argument
 ):
@@ -144,9 +144,11 @@ def format_partitioned_query(
     Formats a query for fetching partitioned data.
     """
     # If no partition column is specified, return the query as is.
-    if partition_column is None:
+    if len(partition_columns) == 0 or partition_columns[0] == "":
         log("NO partition column specified. Returning query as is")
         return query
+
+    partition_column = partition_columns[0]
 
     # Check if the table already exists in BigQuery.
     table = bd.Table(dataset_id, table_id)
@@ -192,6 +194,32 @@ def format_partitioned_query(
 ###############
 
 
+@task
+def parse_comma_separated_string_to_list(text: str) -> List[str]:
+    """
+    Parses a comma separated string to a list.
+
+    Args:
+        text: The text to parse.
+
+    Returns:
+        A list of strings.
+    """
+    if text is None or text == "":
+        return []
+    # Remove extras.
+    text = text.replace("\n", "")
+    text = text.replace("\r", "")
+    text = text.replace("\t", "")
+    while text.find(",,") != -1:
+        text = text.replace(",,", ",")
+    while text.endswith(","):
+        text = text[:-1]
+    result = [x.strip() for x in text.split(",")]
+    result = [item for item in result if item != "" and item is not None]
+    return result
+
+
 @task(
     max_retries=constants.TASK_MAX_RETRIES.value,
     retry_delay=timedelta(seconds=constants.TASK_RETRY_DELAY.value),
@@ -201,7 +229,7 @@ def dump_batches_to_csv(
     database: Database,
     batch_size: int,
     prepath: Union[str, Path],
-    partition_column: str = None,
+    partition_columns: List[str] = None,
     wait=None,  # pylint: disable=unused-argument
 ) -> Path:
     """
@@ -217,6 +245,11 @@ def dump_batches_to_csv(
 
     prepath = Path(prepath)
     log(f"Got prepath: {prepath}")
+
+    if len(partition_columns) == 0 or partition_columns[0] == "":
+        partition_column = None
+    else:
+        partition_column = partition_columns[0]
 
     if not partition_column:
         log("NO partition column specified! Writing unique files")
@@ -238,10 +271,12 @@ def dump_batches_to_csv(
         if not partition_column:
             dataframe_to_csv(dataframe, prepath / f"{eventid}-{idx}.csv")
         else:
-            dataframe, partition_columns = parse_date_columns(
+            dataframe, date_partition_columns = parse_date_columns(
                 dataframe, partition_column
             )
-            to_partitions(dataframe, partition_columns, prepath)
+            to_partitions(
+                dataframe, date_partition_columns + partition_columns[1:], prepath
+            )
         # Get next batch
         batch = database.fetch_batch(batch_size)
         idx += 1
