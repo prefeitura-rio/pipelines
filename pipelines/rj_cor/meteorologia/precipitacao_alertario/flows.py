b1bc9655ea4a5852a94852b4edad04c76c47f4eb
patriciacatandi <patriciabcatandi@gmail.com>
2022-04-29 15:26:33 -0300

    aidção de flow do dbt

--- pipelines/rj_cor/meteorologia/precipitacao_alertario/flows.py
+++ pipelines/rj_cor/meteorologia/precipitacao_alertario/flows.py
@@ -2,9 +2,12 @@
 """
 Flows for precipitacao_alertario
 """
+from prefect import case
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 from pipelines.constants import constants
+from pipelines.utils.constants import constants as utils_constants
 from pipelines.rj_cor.meteorologia.precipitacao_alertario.tasks import (
     download,
     tratar_dados,
@@ -14,7 +17,10 @@ from pipelines.rj_cor.meteorologia.precipitacao_alertario.schedules import (
     minute_schedule,
 )
 from pipelines.utils.decorators import Flow
-from pipelines.utils.tasks import create_table_and_upload_to_gcs
+from pipelines.utils.tasks import (
+    create_table_and_upload_to_gcs,
+    get_current_flow_labels,
+)
 
 with Flow(
     name="COR: Meteorologia - Precipitacao ALERTARIO",
@@ -26,6 +32,9 @@ with Flow(
     DATASET_ID = "meio_ambiente_clima"
     TABLE_ID = "taxa_precipitacao_alertario"
     DUMP_TYPE = "append"
+    MATERIALIZE_AFTER_DUMP = True
+    MATERIALIZE_TO_DATARIO = False
+    MATERIALIZATION_MODE = "dev"
 
     filename, current_time = download()
     dados = tratar_dados(filename=filename)
@@ -39,6 +48,29 @@ with Flow(
         dump_type=DUMP_TYPE,
         wait=path,
     )
+
+    # Trigger DBT flow run
+    with case(MATERIALIZE_AFTER_DUMP, True):
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": DATASET_ID,
+                "table_id": TABLE_ID,
+                "mode": MATERIALIZATION_MODE,
+                "materialize_to_datario": MATERIALIZE_TO_DATARIO,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {DATASET_ID}.{TABLE_ID}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
 
 # para rodar na cloud
 cor_meteorologia_precipitacao_alertario.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
