c5b3924164a7fd5d3b53e829e33495d7dc3a3f2a
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-05-18 16:31:04 -0300

    fix: linting issues

--- pipelines/utils/dump_db/tasks.py
+++ pipelines/utils/dump_db/tasks.py
@@ -263,7 +263,7 @@ def parse_comma_separated_string_to_list(text: str) -> List[str]:
     retry_delay=timedelta(seconds=constants.TASK_RETRY_DELAY.value),
     nout=2,
 )
-def dump_batches_to_csv(  # pylint: disable=too-many-locals
+def dump_batches_to_csv(  # pylint: disable=too-many-locals,too-many-statements
     database: Database,
     batch_size: int,
     prepath: Union[str, Path],
