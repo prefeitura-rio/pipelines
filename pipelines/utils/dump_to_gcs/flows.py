5bc1b4ba1fc02ec43b8bbc3196ab54f9044ae0f8
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-06-03 14:51:47 -0300

    feat: add redis crontab for dump to GCS

--- pipelines/utils/dump_to_gcs/flows.py
+++ pipelines/utils/dump_to_gcs/flows.py
@@ -2,7 +2,7 @@
 """
 Flows for dumping data directly from BigQuery to GCS.
 """
-from prefect import Parameter
+from prefect import Parameter, case
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
@@ -10,7 +10,12 @@ from pipelines.constants import constants
 from pipelines.utils.constants import constants as utils_constants
 from pipelines.utils.decorators import Flow
 from pipelines.utils.dump_to_gcs.constants import constants as dump_to_gcs_constants
-from pipelines.utils.dump_to_gcs.tasks import download_data_to_gcs
+from pipelines.utils.dump_to_gcs.tasks import (
+    download_data_to_gcs,
+    get_project_id,
+    trigger_cron_job,
+    update_last_trigger,
+)
 from pipelines.utils.tasks import rename_current_flow_run_dataset_table
 
 with Flow(
@@ -35,21 +40,45 @@ with Flow(
         required=False,
         default=dump_to_gcs_constants.MAX_BYTES_PROCESSED_PER_TABLE.value,
     )
+    desired_crontab = Parameter(
+        "desired_crontab",
+        required=False,
+        default="* * * * *",
+    )
 
     rename_flow_run = rename_current_flow_run_dataset_table(
         prefix="Dump to GCS: ", dataset_id=dataset_id, table_id=table_id
     )
 
-    download_data_to_gcs(
+    project_id = get_project_id(project_id=project_id, bd_project_mode=bd_project_mode)
+
+    trigger_download, execution_time = trigger_cron_job(
         project_id=project_id,
         dataset_id=dataset_id,
         table_id=table_id,
-        query=query,
-        jinja_query_params=jinja_query_params,
-        bd_project_mode=bd_project_mode,
-        billing_project_id=billing_project_id,
-        maximum_bytes_processed=maximum_bytes_processed,
+        cron_expression=desired_crontab,
     )
+
+    with case(trigger_download, True):
+        download_task = download_data_to_gcs(  # pylint: disable=C0103
+            project_id=project_id,
+            dataset_id=dataset_id,
+            table_id=table_id,
+            query=query,
+            jinja_query_params=jinja_query_params,
+            bd_project_mode=bd_project_mode,
+            billing_project_id=billing_project_id,
+            maximum_bytes_processed=maximum_bytes_processed,
+        )
+
+        update_task = update_last_trigger(  # pylint: disable=C0103
+            project_id=project_id,
+            dataset_id=dataset_id,
+            table_id=table_id,
+            execution_time=execution_time,
+        )
+        update_task.set_upstream(download_task)
+
 
 dump_to_gcs_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 dump_to_gcs_flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
