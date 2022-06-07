06784406fa3c659a084d94f0c944d9f6cb327804
hellcassius <caiorogerio.santos@gmail.com>
2022-06-07 18:56:37 -0300

    add standard bd config

--- pipelines/rj_smtr/utils.py
+++ pipelines/rj_smtr/utils.py
@@ -40,6 +40,8 @@ from pipelines.utils.utils import log
 from pipelines.utils.utils import get_vault_secret, send_discord_message
 from pipelines.rj_smtr.constants import constants
 
+bd.config.from_file = True
+
 
 def log_critical(message: str, secret_path: str = constants.CRITICAL_SECRET_PATH.value):
     """Logs message to critical discord channel specified
