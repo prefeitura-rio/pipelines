f712c086b0acba5fe41e64e7da3a209123d09df5
Fernanda Scovino <fscovinom@gmail.com>
2022-06-08 14:43:00 -0300

    fix costants import

--- pipelines/rj_smtr/br_rj_riodejaneiro_sigmob/tasks.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_sigmob/tasks.py
@@ -2,52 +2,6 @@
 """
 Tasks for br_rj_riodejaneiro_sigmob
 """
-
-###############################################################################
-#
-# Aqui é onde devem ser definidas as tasks para os flows do projeto.
-# Cada task representa um passo da pipeline. Não é estritamente necessário
-# tratar todas as exceções que podem ocorrer durante a execução de uma task,
-# mas é recomendável, ainda que não vá implicar em  uma quebra no sistema.
-# Mais informações sobre tasks podem ser encontradas na documentação do
-# Prefect: https://docs.prefect.io/core/concepts/tasks.html
-#
-# De modo a manter consistência na codebase, todo o código escrito passará
-# pelo pylint. Todos os warnings e erros devem ser corrigidos.
-#
-# As tasks devem ser definidas como funções comuns ao Python, com o decorador
-# @task acima. É recomendado inserir type hints para as variáveis.
-#
-# Um exemplo de task é o seguinte:
-#
-# -----------------------------------------------------------------------------
-# from prefect import task
-#
-# @task
-# def my_task(param1: str, param2: int) -> str:
-#     """
-#     My task description.
-#     """
-#     return f'{param1} {param2}'
-# -----------------------------------------------------------------------------
-#
-# Você também pode usar pacotes Python arbitrários, como numpy, pandas, etc.
-#
-# -----------------------------------------------------------------------------
-# from prefect import task
-# import numpy as np
-#
-# @task
-# def my_task(a: np.ndarray, b: np.ndarray) -> str:
-#     """
-#     My task description.
-#     """
-#     return np.add(a, b)
-# -----------------------------------------------------------------------------
-#
-# Abaixo segue um código para exemplificação, que pode ser removido.
-#
-###############################################################################
 import traceback
 from datetime import timedelta
 from pathlib import Path
@@ -57,7 +11,7 @@ import pendulum
 from prefect import task
 import requests
 
-from pipelines.rj_smtr.br_rj_riodejaneiro_sigmob.constants import constants
+from pipelines.rj_smtr.constants import constants
 from pipelines.rj_smtr.utils import generate_df_and_save, log_critical
 from pipelines.utils.utils import log
 
