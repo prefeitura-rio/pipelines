4482139bb4101bfbd104f86341769c33ef36822b
d116626 <d116626@gmail.com>
2022-06-24 10:01:02 -0300

    feat: change sme:aluno_historico and add some ergon tables

--- pipelines/utils/dump_db/utils.py
+++ pipelines/utils/dump_db/utils.py
@@ -119,6 +119,7 @@ def generate_dump_db_schedules(  # pylint: disable=too-many-arguments,too-many-l
 
         if "interval" in parameters:
             interval = parameters["interval"]
+
         clocks.append(
             IntervalClock(
                 interval=interval,
