dac33e9dc49aa54f9844c88dadda567d7aed087f
patriciacatandi <patriciabcatandi@gmail.com>
2022-07-05 11:02:10 -0300

    adequando pylint

--- pipelines/rj_cor/meteorologia/utils.py
+++ pipelines/rj_cor/meteorologia/utils.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+# pylint: disable=W0106
+"""
+General utilities for meteorologia.
+"""
 import pandas as pd
-from redis_pal import RedisPal
 
 from pipelines.utils.utils import (
     get_redis_client,
@@ -8,7 +11,7 @@ from pipelines.utils.utils import (
 
 
 def save_updated_rows_on_redis(
-    df: pd.DataFrame, dataset_id: str, table_id: str, mode: str = "prod"
+    dfr: pd.DataFrame, dataset_id: str, table_id: str, mode: str = "prod"
 ) -> pd.DataFrame:
     """
     Acess redis to get the last time each id_estacao was updated, return
@@ -28,15 +31,15 @@ def save_updated_rows_on_redis(
     # Example > {"12": "2022-06-06 14:45:00"}
     updates = {k.decode("utf-8"): v.decode("utf-8") for k, v in updates.items()}
 
-    # Convert dictionary to df
+    # Convert dictionary to dfr
     updates = pd.DataFrame(updates.items(), columns=["id_estacao", "last_update"])
 
-    # df and updates need to have the same index, in our case id_estacao
-    missing_in_df = [
-        i for i in updates.id_estacao.unique() if i not in df.id_estacao.unique()
+    # dfr and updates need to have the same index, in our case id_estacao
+    missing_in_dfr = [
+        i for i in updates.id_estacao.unique() if i not in dfr.id_estacao.unique()
     ]
     missing_in_updates = [
-        i for i in df.id_estacao.unique() if i not in updates.id_estacao.unique()
+        i for i in dfr.id_estacao.unique() if i not in updates.id_estacao.unique()
     ]
 
     # If id_estacao doesn't exists on updates we create a fake date for this station on updates
@@ -47,24 +50,25 @@ def save_updated_rows_on_redis(
                 ignore_index=True,
             )
 
-    # If id_estacao doesn't exists on df we remove this stations from updates
-    if len(missing_in_df) > 0:
-        updates = updates[~updates.id_estacao.isin(missing_in_df)]
+    # If id_estacao doesn't exists on dfr we remove this stations from updates
+    if len(missing_in_dfr) > 0:
+        updates = updates[~updates.id_estacao.isin(missing_in_dfr)]
 
     # Set the index with the id_estacao
-    df.set_index(df.id_estacao.unique(), inplace=True)
+    dfr.set_index(dfr.id_estacao.unique(), inplace=True)
     updates.set_index(updates.id_estacao.unique(), inplace=True)
 
-    # Keep on df only the stations that has a time after the one that is saved on redis
-    df = df.where(
-        (df.id_estacao == updates.id_estacao) & (df.data_medicao > updates.last_update)
+    # Keep on dfr only the stations that has a time after the one that is saved on redis
+    dfr = dfr.where(
+        (dfr.id_estacao == updates.id_estacao)
+        & (dfr.data_medicao > updates.last_update)
     ).dropna(subset=["id_estacao"])
 
     # Convert stations with the new updates dates in a dictionary
-    df.set_index("id_estacao", inplace=True)
-    new_updates = df["data_medicao"].astype(str).to_dict()
+    dfr.set_index("id_estacao", inplace=True)
+    new_updates = dfr["data_medicao"].astype(str).to_dict()
 
     # Save this new information on redis
     [redis_client.hset(key, k, v) for k, v in new_updates.items()]
 
-    return df.reset_index()
+    return dfr.reset_index()
