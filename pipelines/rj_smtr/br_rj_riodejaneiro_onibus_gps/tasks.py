8b9d1cb3fe0447a1d23c695fdb75624187d7e8fc
Fernanda Scovino <fscovinom@gmail.com>
2022-07-22 19:33:41 -0300

    set full treat inside task to update run_cloud

--- pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps/tasks.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps/tasks.py
@@ -6,6 +6,7 @@ Tasks for br_rj_riodejaneiro_onibus_gps
 import traceback
 import pandas as pd
 from prefect import task
+from datetime import timedelta
 
 # EMD Imports #
 
@@ -14,8 +15,6 @@ from pipelines.utils.utils import log
 # SMTR Imports #
 
 from pipelines.rj_smtr.constants import constants
-from pipelines.rj_smtr.utils import log_critical
-from pipelines.rj_smtr.br_rj_riodejaneiro_onibus_gps.utils import sppo_filters
 
 # Tasks #
 
@@ -36,62 +35,77 @@ def pre_treatment_br_rj_riodejaneiro_onibus_gps(
         df: pandas.core.DataFrame containing the treated data.
     """
 
+    # Check for previous errors
     if status_dict["error"] is not None:
         return {"df": pd.DataFrame(), "error": status_dict["error"]}
+    else:
+        error = None
 
-    error = None
     data = status_dict["data"]
-    timezone = constants.TIMEZONE.value
     timestamp = status_dict["timestamp"]
 
-    log(f"data={data.json()[:50]}")
-    data = data.json()
+    log(f"data={data[:50]}")
     df = pd.DataFrame(data)  # pylint: disable=c0103
-    timestamp_captura = pd.to_datetime(timestamp)
-    df["timestamp_captura"] = timestamp_captura
-    log(f"Before converting, datahora is: \n{df['datahora']}")
+    df["timestamp_captura"] = pd.to_datetime(timestamp)
 
     # Remove timezone and force it to be config timezone
-
     if version == 1:
         timestamp_cols = ["datahora"]
     elif version == 2:
         timestamp_cols = ["datahora", "datahoraenvio"]
     if recapture:
         timestamp_cols.append("datahoraservidor")
+
     for col in timestamp_cols:
         print(f"Before converting, {col} is: \n{df[col].head()}")  # log
-        # Remove timezone and force it to be config timezone
         df[col] = (
             pd.to_datetime(df[col].astype(float), unit="ms")
             .dt.tz_localize(tz="UTC")
-            .dt.tz_convert(timezone)
+            .dt.tz_convert(constants.TIMEZONE.value)
         )
         log(f"After converting the timezone, {col} is: \n{df[col].head()}")
 
-    # Filter data for 0 <= time diff <= 1min
+    # Filter data
     try:
-        # filters
-        log(f"Shape antes da filtragem: {df.shape}")
-        df = sppo_filters(  # pylint: disable=C0103
-            frame=df, version=version, recapture=recapture
+        log(f"Shape before filtering: {df.shape}")
+        # Filter capture time delays
+        if version == 1:
+            filter_col = "timestamp_captura"
+            time_delay = constants.GPS_SPPO_CAPTURE_DELAY_V1.value
+        elif version == 2:
+            filter_col = "datahoraenvio"
+            time_delay = constants.GPS_SPPO_CAPTURE_DELAY_V2.value
+        if recapture:
+            server_mask = (df["datahoraenvio"] - df["datahoraservidor"]) <= timedelta(
+                minutes=constants.GPS_SPPO_RECAPTURE_DELAY_V2.value
+            )
+            df = df[server_mask]
+
+        mask = (df[filter_col] - df["datahora"]).apply(
+            lambda x: timedelta(seconds=0) <= x <= timedelta(minutes=time_delay)
         )
-        log(f"Shape após a filtragem: {df.shape}")
+        df = df[mask]
+
+        # Select and drop duplicated data
+        cols = [
+            "ordem",
+            "latitude",
+            "longitude",
+            "datahora",
+            "velocidade",
+            "linha",
+            "timestamp_captura",
+        ]
+        df = df[cols].drop_duplicates(["ordem", "datahora"])
+
+        log(f"Shape after filtering: {df.shape}")
+
         if df.shape[0] == 0:
             error = ValueError("After filtering, the dataframe is empty!")
-            log_critical(f"@here\nFailed to filter SPPO data: \n{error}")
-        if version == 2:
-            df = df.drop(  # pylint: disable=C0103
-                columns=["datahoraenvio", "datahoraservidor"]
-            )
-    except Exception:  # pylint: disable = W0703
-        err = traceback.format_exc()
-        log_critical(f"@here\nFailed to filter SPPO data: \n{err}")
-    # log_critical(f"@here\n Got SPPO data at {timestamp} sucessfully")
+            log(error)
 
-    return {
-        "df": df.drop_duplicates(
-            ["ordem", "latitude", "longitude", "datahora", "timestamp_captura"]
-        ),
-        "error": error,
-    }
+    except Exception:  # pylint: disable = W0703
+        error = traceback.format_exc()
+        log(error)
+
+    return {"df": df, "error": error, "timestamp": timestamp}
