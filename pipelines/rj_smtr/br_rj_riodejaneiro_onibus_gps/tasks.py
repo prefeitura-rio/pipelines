7b745ce38a3537bd473d1c212893d67daa5aceb4
Fernanda Scovino <fscovinom@gmail.com>
2022-07-28 15:32:57 -0300

    remove utils from flow

--- pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps/tasks.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps/tasks.py
@@ -16,7 +16,6 @@ from pipelines.utils.utils import log, get_vault_secret
 # SMTR Imports #
 
 from pipelines.rj_smtr.constants import constants
-from pipelines.rj_smtr.br_rj_riodejaneiro_onibus_gps.utils import sppo_filters
 
 # Tasks #
 
@@ -70,7 +69,7 @@ def pre_treatment_br_rj_riodejaneiro_onibus_gps(
     timezone = constants.TIMEZONE.value
     timestamp = status_dict["timestamp"]
 
-    log(f"data={data.json()[:50]}")
+    log(f"Data received to treat: \n{data.json()[:5]}")
     data = data.json()
     df = pd.DataFrame(data)  # pylint: disable=c0103
     timestamp_captura = pd.to_datetime(timestamp)
@@ -95,20 +94,40 @@ def pre_treatment_br_rj_riodejaneiro_onibus_gps(
         )
         log(f"After converting the timezone, {col} is: \n{df[col].head()}")
 
-    # Filter data for 0 <= time diff <= 1min
+    # Filter data
     try:
-        # filters
-        log(f"Shape antes da filtragem: {df.shape}")
-        df = sppo_filters(  # pylint: disable=C0103
-            frame=df, version=version, recapture=recapture
+        log(f"Shape before filtering: {df.shape}")
+        if version == 1:
+            filter_col = "timestamp_captura"
+            time_delay = constants.GPS_SPPO_CAPTURE_DELAY_V1.value
+        elif version == 2:
+            filter_col = "datahoraenvio"
+            time_delay = constants.GPS_SPPO_CAPTURE_DELAY_V2.value
+        if recapture:
+            server_mask = (df["datahoraenvio"] - df["datahoraservidor"]) <= timedelta(
+                minutes=constants.GPS_SPPO_RECAPTURE_DELAY_V2.value
+            )
+            df = df[server_mask]
+
+        mask = (df[filter_col] - df["datahora"]).apply(
+            lambda x: timedelta(seconds=0) <= x <= timedelta(minutes=time_delay)
         )
-        log(f"Shape após a filtragem: {df.shape}")
+
+        # Select and drop duplicated data
+        cols = [
+            "ordem",
+            "latitude",
+            "longitude",
+            "datahora",
+            "velocidade",
+            "linha",
+            "timestamp_captura",
+        ]
+        df = df = df[mask][cols]
+
+        log(f"Shape after filtering: {df.shape}")
         if df.shape[0] == 0:
             error = ValueError("After filtering, the dataframe is empty!")
-        if version == 2:
-            df = df.drop(  # pylint: disable=C0103
-                columns=["datahoraenvio", "datahoraservidor"]
-            )
     except Exception:  # pylint: disable = W0703
         error = traceback.format_exc()
 
@@ -117,4 +136,5 @@ def pre_treatment_br_rj_riodejaneiro_onibus_gps(
             ["ordem", "latitude", "longitude", "datahora", "timestamp_captura"]
         ),
         "error": error,
+        "timestamp": timestamp,
     }
