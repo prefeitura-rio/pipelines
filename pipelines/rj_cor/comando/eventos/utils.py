524aa0ab56d565c17eb1a5619f26515b5da6f893
patriciacatandi <patriciabcatandi@gmail.com>
2022-08-02 15:19:15 -0300

    salvando erros de atividades id no redis + correções

--- pipelines/rj_cor/comando/eventos/utils.py
+++ pipelines/rj_cor/comando/eventos/utils.py
@@ -2,17 +2,18 @@
 """
 General purpose functions for the comando project
 """
+import json
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
 from pipelines.utils.utils import get_vault_secret, log
 
 
-def build_redis_key(dataset_id: str, table_id: str, mode: str = "prod"):
+def build_redis_key(dataset_id: str, table_id: str, name: str, mode: str = "prod"):
     """
     Helper function for building a key where to store the last updated time
     """
-    key = dataset_id + "." + table_id
+    key = dataset_id + "." + table_id + "." + name
     if mode == "dev":
         key = f"{mode}.{key}"
     return key
@@ -29,6 +30,7 @@ def get_token():
     return requests.post(host, json=payload).text
 
 
+# pylint: disable=W0703
 def get_url(url, parameters: dict = None, token: str = None):  # pylint: disable=W0102
     """Make request to comando's API"""
     if not parameters:
@@ -39,12 +41,13 @@ def get_url(url, parameters: dict = None, token: str = None):  # pylint: disable
     retries = Retry(total=5, backoff_factor=1.5)
     sess.mount("http://", HTTPAdapter(max_retries=retries))
     headers = {"Authorization": token}
+
+    log(f"\n\n>>>>>>> headers {headers} \n\n")
+    log(f"\n\n>>>>>>> parameters {parameters} \n\n")
     try:
         response = sess.get(url, json=parameters, headers=headers)
-        return response.json()
+        response = response.json()
     except Exception as exc:
-        log(
-            f"Response was: {response}: {response.text}\n\n\n"
-            + "This resulted in the following error: {exc}"
-        )
-        raise exc
+        log(f"This resulted in the following error: {exc}")
+        response = {"response": None}
+    return response
