7d22b84d95f6046ad8b2c0a43bdce96c2199295d
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-08-04 14:40:27 -0300

    feat: implement flow runs query and mutation

--- pipelines/rj_escritorio/__init__.py
+++ pipelines/rj_escritorio/__init__.py
@@ -9,6 +9,7 @@ from pipelines.rj_escritorio.dados_mestres_dump_datario.flows import *
 from pipelines.rj_escritorio.template_pipeline.flows import *
 from pipelines.rj_escritorio.tweets_flamengo.flows import *
 from pipelines.rj_escritorio.birthdays.flows import *
+from pipelines.rj_escritorio.cleanup.flows import *
 from pipelines.rj_escritorio.waze.flows import *
 from pipelines.rj_escritorio.geolocator.flows import *
 from pipelines.rj_escritorio.seconserva_buracos_refresh_data.flows import *
