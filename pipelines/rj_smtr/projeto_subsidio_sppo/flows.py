8e3b5d514834dd11c694f0a28712cc6b3712108e
Fernanda Scovino <fscovinom@gmail.com>
2022-08-11 20:17:50 -0300

    fix dbt params + add planejado

--- pipelines/rj_smtr/projeto_subsidio_sppo/flows.py
+++ pipelines/rj_smtr/projeto_subsidio_sppo/flows.py
@@ -33,7 +33,9 @@ from pipelines.rj_smtr.schedules import (
 
 # Flows #
 
-with Flow("SMTR - Subsídio SPPO", code_owners=["caio", "fernanda"]) as subsidio_sppo:
+with Flow(
+    "SMTR - Subsídio SPPO - Viagens", code_owners=["caio", "fernanda"]
+) as subsidio_sppo_viagens:
     # Get default parameters #
     run_date = Parameter("run_date", default=None)
     LABELS = get_current_flow_labels()
@@ -44,7 +46,7 @@ with Flow("SMTR - Subsídio SPPO", code_owners=["caio", "fernanda"]) as subsidio
 
     # Rename flow run
     rename_flow_run = rename_current_flow_run_now_time(
-        prefix="Subsídio SPPO: ", now_time=run_date
+        prefix="Subsídio SPPO - Viagens: ", now_time=run_date
     )
 
     # Set dbt client #
@@ -63,12 +65,57 @@ with Flow("SMTR - Subsídio SPPO", code_owners=["caio", "fernanda"]) as subsidio
         model=constants.SUBSIDIO_SPPO_DATASET_ID.value,
         upstream=False,
         exclude="+viagem_planejada",
-        _vars=[run_date, dataset_sha],
+        _vars=[{"run_date": run_date}, dataset_sha],
     )
 
-subsidio_sppo.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
-subsidio_sppo.run_config = KubernetesRun(
+subsidio_sppo_viagens.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
+subsidio_sppo_viagens.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
     labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
 )
-subsidio_sppo.schedule = every_day_hour_six_dev
+subsidio_sppo_viagens.schedule = every_day_hour_six_dev
+
+
+with Flow(
+    "SMTR - Subsídio SPPO - Planejado", code_owners=["caio", "fernanda"]
+) as subsidio_sppo_planejado:
+    # Get default parameters #
+    shapes_date = Parameter("shapes_date", default="2022-08-01")
+    frequencies_date = Parameter("frequencies_date", default="2022-08-01")
+    LABELS = get_current_flow_labels()
+    MODE = get_current_flow_mode(LABELS)
+
+    # Rename flow run
+    rename_flow_run = rename_current_flow_run_now_time(
+        prefix="Subsídio SPPO - Planejado: ", now_time=frequencies_date
+    )
+
+    # Set dbt client #
+    dbt_client = get_k8s_dbt_client(mode=MODE, wait=rename_flow_run)
+    # Use the command below to get the dbt client in dev mode:
+    # dbt_client = get_local_dbt_client(host="localhost", port=3001)
+
+    # Set specific run parameters #
+    dataset_sha = fetch_dataset_sha(
+        dataset_id=constants.SUBSIDIO_SPPO_DATASET_ID.value,
+    )
+
+    # Run materialization #
+    RUN = run_dbt_model(
+        dbt_client=dbt_client,
+        model="viagem_planejada",
+        upstream=True,
+        exclude="br_rj_riodejaneiro_sigmob",
+        _vars=[
+            {"shapes_date": shapes_date},
+            {"frequencies_date": frequencies_date},
+            dataset_sha,
+        ],
+    )
+
+subsidio_sppo_planejado.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
+subsidio_sppo_planejado.run_config = KubernetesRun(
+    image=emd_constants.DOCKER_IMAGE.value,
+    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
+)
+# subsidio_sppo_planejado.schedule = every_day_hour_six_dev
