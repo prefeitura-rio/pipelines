9207fa78e2b71a765984ab2573348a87a67b7e93
hellcassius <caiorogerio.santos@gmail.com>
2022-09-13 16:53:15 -0300

    change labels to prod

--- pipelines/rj_smtr/br_rj_riodejaneiro_brt_gps/flows.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_brt_gps/flows.py
@@ -19,7 +19,7 @@ from pipelines.utils.tasks import get_now_time, rename_current_flow_run_now_time
 from pipelines.rj_smtr.constants import constants
 
 from pipelines.rj_smtr.schedules import (
-    every_minute_dev,
+    every_minute,
     every_hour,
 )
 from pipelines.rj_smtr.tasks import (
@@ -178,6 +178,6 @@ with Flow(
 captura_brt.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 captura_brt.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
-    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
+    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
 )
-captura_brt.schedule = every_minute_dev
+captura_brt.schedule = every_minute
