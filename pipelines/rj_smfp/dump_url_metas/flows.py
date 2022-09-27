6963f2888debe1b1e4e26a07d9e20472fd98315b
Bruno Almeida <brunodepauloalmeida@gmail.com>
2022-09-27 16:46:54 -0300

    (add): pipeline to dump goals' dashboard data from Google Sheets to BQ

---
+++ pipelines/rj_smfp/dump_url_metas/flows.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Database dumping flows for segovi project
+"""
+
+from copy import deepcopy
+
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+
+from pipelines.constants import constants
+
+from pipelines.rj_smfp.dump_url_metas.schedules import gsheets_daily_update_schedule
+from pipelines.utils.dump_url.flows import dump_url_flow
+from pipelines.utils.utils import set_default_parameters
+
+smfp_gsheets_flow = deepcopy(dump_url_flow)
+smfp_gsheets_flow.name = "SMFP: Google Sheets - Ingerir tabelas de URL"
+smfp_gsheets_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+smfp_gsheets_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_SMFP_AGENT_LABEL.value,
+    ],
+)
+
+smfp_gsheets_default_parameters = {
+    "dataset_id": "planejamento_gestao_dashboard_metas",
+}
+smfp_gsheets_flow = set_default_parameters(
+    smfp_gsheets_flow, default_parameters=smfp_gsheets_default_parameters
+)
+
+smfp_gsheets_flow.schedule = gsheets_daily_update_schedule
---
+++ pipelines/rj_smfp/dump_url_metas/schedules.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Schedules for the database dump pipeline
+"""
+
+from datetime import datetime, timedelta
+
+import pytz
+from prefect.schedules import Schedule
+from pipelines.constants import constants
+from pipelines.utils.dump_url.utils import generate_dump_url_schedules
+from pipelines.utils.utils import untuple_clocks as untuple
+
+#####################################
+#
+# EGPWeb Schedules
+#
+#####################################
+
+gsheets_urls = {
+    "metas_planejamento_estrategico": {
+        "dump_mode": "overwrite",
+        "url": "https://docs.google.com/spreadsheets/d/1lWbNoBSPDLi7nhZvt1G3vEYBWF460Su8PKALXvQJH5w\
+            /edit#gid=917050709",
+        "url_type": "google_sheet",
+        "gsheets_sheet_name": "METAS CONSOLIDADO",
+        "materialize_after_dump": True,
+    },
+    "metas_acordo_resultados_planilha": {
+        "dump_mode": "overwrite",
+        "url": "https://docs.google.com/spreadsheets/d/1BnOqRKZSzlwyAWaWmaape9XS1Zrh_b-C6ApZjlaRo3E\
+            /edit#gid=827909827",
+        "url_type": "google_sheet",
+        "gsheets_sheet_name": "METAS_AR",
+        "materialize_after_dump": True,
+    },
+    "orgaos": {
+        "dump_mode": "overwrite",
+        "url": "https://docs.google.com/spreadsheets/d/1nqnwgigE0_Ac6-jkWiNwAdB4EMJfz1WV5nn0HaOhfu0\
+            /edit#gid=1236673479",
+        "url_type": "google_sheet",
+        "gsheets_sheet_name": "Lista Órgãos",
+        "materialize_after_dump": True,
+    },
+    "relacao_metas": {
+        "dump_mode": "overwrite",
+        "url": "https://docs.google.com/spreadsheets/d/1s9-PY6ayAYbtOY8_jUCvaMZSPP5xGdgNkERxS5QQCsw\
+            /edit#gid=118781852",
+        "url_type": "google_sheet",
+        "gsheets_sheet_name": "Relação MetaPE-AR2022",
+        "materialize_after_dump": True,
+    },
+}
+
+
+gsheets_clocks = generate_dump_url_schedules(
+    interval=timedelta(days=1),
+    start_date=datetime(2022, 9, 28, 7, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[
+        constants.RJ_SMFP_AGENT_LABEL.value,
+    ],
+    dataset_id="planejamento_gestao_dashboard_metas",
+    table_parameters=gsheets_urls,
+)
+
+gsheets_daily_update_schedule = Schedule(clocks=untuple(gsheets_clocks))
