d2b5e90fbe42569b94de17e091ad1dc07fb200f3
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-10-03 16:01:13 -0300

    feat: implement download

--- pipelines/rj_escritorio/inea/flows.py
+++ pipelines/rj_escritorio/inea/flows.py
@@ -2,11 +2,12 @@
 """
 Flows for INEA.
 """
+from prefect import Parameter
 from prefect.run_configs import LocalRun
 from prefect.storage import GCS
 
 from pipelines.constants import constants
-from pipelines.rj_escritorio.inea.tasks import print_environment_variables
+from pipelines.rj_escritorio.inea.tasks import convert_vol_files, fetch_vol_files
 from pipelines.utils.decorators import Flow
 
 with Flow(
@@ -15,7 +16,10 @@ with Flow(
         "gabriel",
     ],
 ) as inea_test_flow:
-    print_environment_variables()
+    date = Parameter("date")
+    FETCH_TASK = fetch_vol_files(date=date)
+    CONVERT_TASK = convert_vol_files()
+    CONVERT_TASK.set_upstream(FETCH_TASK)
 
 inea_test_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 inea_test_flow.run_config = LocalRun(labels=[constants.INEA_AGENT_LABEL.value])
--- pipelines/rj_escritorio/inea/tasks.py
+++ pipelines/rj_escritorio/inea/tasks.py
@@ -3,8 +3,11 @@
 Tasks for INEA.
 """
 from os import environ
+from pathlib import Path
 
+from paramiko import SSHClient
 from prefect import task
+from scp import SCPClient
 
 from pipelines.utils.utils import log
 
@@ -17,3 +20,42 @@ def print_environment_variables():
     log("Environment variables:")
     for key, value in environ.items():
         log(f"{key}={value}")
+
+
+@task
+def fetch_vol_files(date: str, output_directory: str = "/var/escritoriodedados/temp/"):
+    """
+    Fetch files from INEA server
+
+    Args:
+        date (str): Date of the files to be fetched (e.g. 20220125)
+    """
+    log("Fetching files from INEA server...")
+    # Creating temporary directory
+    output_directory_path = Path(output_directory)
+    output_directory_path.mkdir(parents=True, exist_ok=True)
+    # Open SSH client
+    ssh_client = SSHClient()
+    ssh_client.load_system_host_keys()
+    ssh_client.connect(hostname="a9921")
+    # Open SCP client
+    scp = SCPClient(ssh_client.get_transport())
+    # Fetch VOL files
+    fname = f"/var/opt/edge/vols/9921GUA{date}*.vol"
+    scp.get(fname, recursive=True, local_path=output_directory)
+    # Close connection
+    scp.close()
+
+
+@task
+def convert_vol_files(output_directory: str = "/var/escritoriodedados/temp/"):
+    """
+    Convert VOL files to NetCDF using the `volconvert` CLI tool.
+    """
+    # List all files in the output directory
+    output_directory_path = Path(output_directory)
+    files = output_directory_path.glob("*.vol")
+    # Log each file and then delete it
+    for file in files:
+        log(f"Converting {file} to NetCDF...")
+        file.unlink()
