bab1b44f3d639df17b19df1e44bf44b9157b9732
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-10-11 10:17:39 -0300

    feat: list remote files (introduces raised error for debugging)

--- pipelines/rj_escritorio/inea/flows.py
+++ pipelines/rj_escritorio/inea/flows.py
@@ -9,6 +9,7 @@ from prefect.utilities.edges import unmapped
 
 from pipelines.constants import constants
 from pipelines.rj_escritorio.inea.tasks import (
+    build_regex_expression,
     convert_vol_file,
     execute_shell_command,
     fetch_vol_files,
@@ -22,7 +23,7 @@ with Flow(
         "gabriel",
     ],
 ) as inea_test_flow:
-    date = Parameter("date")
+    date = Parameter("date", default=None, required=False)
     bucket_name = Parameter("bucket_name")
     prefix = Parameter("prefix")
     mode = Parameter("mode", default="prod", required=False)
@@ -34,7 +35,9 @@ with Flow(
         default="-f=Whole -k=CFext -r=Short -p=Radar -M=All -z",
         required=False,
     )
-    downloaded_files = fetch_vol_files(date=date)
+    greater_than = Parameter("greater_than", default=None, required=False)
+    regex_expression = build_regex_expression(greater_than=greater_than)
+    downloaded_files = fetch_vol_files(date=date, regex_expression=regex_expression)
     converted_files = convert_vol_file.map(
         downloaded_file=downloaded_files,
         output_format=unmapped(output_format),
--- pipelines/rj_escritorio/inea/tasks.py
+++ pipelines/rj_escritorio/inea/tasks.py
@@ -5,7 +5,8 @@ Tasks for INEA.
 from datetime import datetime, timedelta
 from os import environ, getenv
 from pathlib import Path
-from typing import List
+import subprocess
+from typing import Callable, List
 
 from google.cloud import storage
 from paramiko import SSHClient
@@ -26,17 +27,78 @@ def print_environment_variables():
         log(f"{key}={value}")
 
 
+@task
+def build_regex_expression(
+    greater_than: str,
+    working_directory: str = "/var/opt/edge/vols",
+    filename_prefix: str = "9921GUA",
+    file_extension: str = "vol",
+):
+    """
+    Builds the regex expression for including only the files that we want to
+    download. The format is the following:
+
+    <working_directory>/<filename_prefix><greater_than>.<file_extension>
+
+    `greater_than` must be numeric only.
+    """
+    if greater_than is None:
+        return ""
+    # Assert that greater_than is numeric
+    for char in greater_than:
+        assert (
+            char in "0123456789"
+        ), f"{char} is not numeric. greater_than must be numeric only."
+    # Add trailing slash
+    working_directory = (
+        f"{working_directory}/"
+        if not working_directory.endswith("/")
+        else working_directory
+    )
+    # Build initial expression
+    expression = f"{working_directory}{filename_prefix}"
+    # Adds greater than expression
+    for char in greater_than:
+        expression += f"[{char}-9]"
+    # Adds anything after greater than expression
+    expression += ".*"
+    # Adds file extension
+    expression += f"{file_extension}"
+    log(f"Regex expression: {expression}")
+    return expression
+
+
 @task(
     max_retries=2,
     retry_delay=timedelta(seconds=30),
 )
-def fetch_vol_files(date: str, output_directory: str = "/var/escritoriodedados/temp/"):
+def fetch_vol_files(
+    date: str,
+    regex_expression: str = "",
+    output_directory: str = "/var/escritoriodedados/temp/",
+):
     """
     Fetch files from INEA server
 
     Args:
         date (str): Date of the files to be fetched (e.g. 20220125)
     """
+
+    def run_shell_command(command: str, stdout_callback: Callable = log) -> int:
+        popen = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, universal_newlines=True
+        )
+        for stdout_line in iter(popen.stdout.readline, ""):
+            stdout_callback(stdout_line)
+        popen.stdout.close()
+        return_code = popen.wait()
+        if return_code:
+            raise subprocess.CalledProcessError(return_code, command)
+        return return_code
+
+    # Either date or regex_expression must be provided
+    assert date or regex_expression, "Either date or regex_expression must be provided."
+
     log("Fetching files from INEA server...")
     # Creating temporary directory
     output_directory_path = Path(output_directory) / date
@@ -48,6 +110,20 @@ def fetch_vol_files(date: str, output_directory: str = "/var/escritoriodedados/t
     ssh_client = SSHClient()
     ssh_client.load_system_host_keys()
     ssh_client.connect(hostname="a9921", username="root", password=ssh_password)
+    # List remote files
+    log("Listing remote files...")
+    if date:
+        _, stdout, _ = ssh_client.exec_command(
+            f"find /var/opt/edge/vols -name '9921GUA{date}*.vol'"
+        )
+    else:
+        _, stdout, _ = ssh_client.exec_command(
+            f"find /var/opt/edge/vols -name '{regex_expression}'"
+        )
+    remote_files = stdout.read().decode("utf-8").splitlines()
+    log(f"Found {len(remote_files)} files.")
+    log(f"Remote files: {remote_files}")
+    raise ValueError("Test")
     # Open SCP client
     scp = SCPClient(ssh_client.get_transport(), sanitize=lambda x: x)
     # Fetch VOL files
