9b345417ef046be8fb044da6184e36a1db0c44ce
Caiolaurenti <caio.laurenti@gmail.com>
2022-10-26 16:20:23 -0300

    Teste formação CLaurenti

---
+++ pipelines/formacao/exemplo/flows.py
@@ -0,0 +1,34 @@
+#-*- coding: utf-8 -*-
+"""
+Example flow
+"""
+from prefect import Parameter
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+
+from pipelines.constants import constants
+from pipelines.formacao.exemplo.tasks import download_data, parse_data, save_report
+from pipelines.utils.decorators import Flow
+from pipelines.utils.tasks import create_table_and_upload_to_gcs
+
+with Flow("EMD: formacao - Exemplo de flow do Prefect CHLL") as formacao_exemplo_flow:
+    # Parâmetros
+    n_users = Parameter("n_users", default=10)
+
+    # Tasks
+    data = download_data(n_users)
+    dataframe = parse_data(data)
+    path = save_report(dataframe)
+    create_table_and_upload_to_gcs(
+        data_path = path, 
+        dataset_id = 'teste_claurenti', 
+        table_id = 'teste_claurenti', 
+        dump_mode = 'overwrite'
+    )
+
+formacao_exemplo_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+formacao_exemplo_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[constants.RJ_COR_AGENT_LABEL.value],
+)
+formacao_exemplo_flow.schedule = None
---
+++ pipelines/formacao/exemplo/tasks.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Tasks for the example flow
+"""
+from io import StringIO
+
+import pandas as pd
+from prefect import task
+import requests
+
+from pipelines.utils.utils import log
+
+@task
+def download_data(n_users: int) -> str:
+    """
+    Baixa dados da API https://randomuser.me e retorna um texto em formato CSV.
+
+    Args:
+        n_users (int): número de usuários a serem baixados.
+
+    Returns:
+        str: texto em formato CSV.
+    """
+    response = requests.get(
+        "https://randomuser.me/api/?results={}&format=csv".format(n_users)
+    )
+    log("Dados baixados com sucesso!")
+    return response.text
+
+@task
+def parse_data(data: str) -> pd.DataFrame:
+    """
+    Transforma os dados em formato CSV em um DataFrame do Pandas, para facilitar sua manipulação.
+
+    Args:
+        data (str): texto em formato CSV.
+
+    Returns:
+        pd.DataFrame: DataFrame do Pandas.
+    """
+    df = pd.read_csv(StringIO(data))
+    log("Dados convertidos em DataFrame com sucesso!")
+    return df
+
+@task
+def save_report(dataframe: pd.DataFrame) -> None:
+    """
+    Salva o DataFrame em um arquivo CSV.
+
+    Args:
+        dataframe (pd.DataFrame): DataFrame do Pandas.
+    """
+    dataframe.to_csv("/tmp/report.csv", index=False)
+    log("Dados salvos em report.csv com sucesso!")
+    return '/tmp/report.csv'
