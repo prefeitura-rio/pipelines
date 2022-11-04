66715419690c6cc989bfd1698c16a58e5e37bff3
Bruno Almeida <brunodepauloalmeida@gmail.com>
2022-11-04 14:55:54 -0300

    add: example flow for getting data from sql base

--- pipelines/formacao/__init__.py
+++ pipelines/formacao/__init__.py
@@ -6,3 +6,4 @@ Prefect flows for emd project
 # Automatically managed, please do not touch
 ###############################################################################
 from pipelines.formacao.dump_url_formacao.flows import *
+from pipelines.formacao.dump_db_formacao.flows import *
