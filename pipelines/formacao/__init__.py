951401c5539c0009cadf61bb8b63b607dd87f895
Bruno Almeida <brunodepauloalmeida@gmail.com>
2022-11-04 14:40:53 -0300

    add: example flow for getting data from gsheets url

---
+++ pipelines/formacao/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Prefect flows for emd project
+"""
+###############################################################################
+# Automatically managed, please do not touch
+###############################################################################
+from pipelines.formacao.dump_url_formacao.flows import *
