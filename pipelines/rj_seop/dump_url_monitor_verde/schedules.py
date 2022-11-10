93981c7fc39f3f5c9e00effbf6ff7796dd4c9c33
d116626 <d116626@gmail.com>
2022-11-10 17:50:07 -0300

    fix: change data link for monitor verde

--- pipelines/rj_seop/dump_url_monitor_verde/schedules.py
+++ pipelines/rj_seop/dump_url_monitor_verde/schedules.py
@@ -20,7 +20,7 @@ from pipelines.utils.utils import untuple_clocks as untuple
 gsheets_urls = {
     "alertas_desmatamento": {
         "dump_mode": "overwrite",
-        "url": "https://drive.google.com/file/d/1PKBRaivrKNilRc4dBs-tZk03I6UHfdp8/view?usp=share_link",
+        "url": "https://drive.google.com/file/d/1Q5MbIStFAWzM1q9Vf82xzKBYAZHNt9l6/view?usp=share_link",
         "url_type": "google_drive",
         "materialize_after_dump": False,
     }
@@ -29,9 +29,7 @@ gsheets_urls = {
 gsheets_clocks = generate_dump_url_schedules(
     interval=timedelta(days=365),
     start_date=datetime(2022, 11, 4, 20, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
-    labels=[
-        constants.RJ_SEOP_AGENT_LABEL.value,
-    ],
+    labels=[constants.RJ_SEOP_AGENT_LABEL.value,],
     dataset_id="conservacao_ambiental_monitor_verde",
     table_parameters=gsheets_urls,
 )
