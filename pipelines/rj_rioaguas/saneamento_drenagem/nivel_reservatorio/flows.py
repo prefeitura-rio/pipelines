719114a0389ffd388c893a06e65670378c41e0ca
Anderson <anderson.guilherme@cor.rio>
2022-11-16 16:49:12 -0300

    adicionado deepcopy(dump_url_flow) script e schedules.py

--- pipelines/rj_rioaguas/saneamento_drenagem/nivel_reservatorio/flows.py
+++ pipelines/rj_rioaguas/saneamento_drenagem/nivel_reservatorio/flows.py
@@ -1,30 +1,45 @@
 # -*- coding: utf-8 -*-
+"""
+Database dumping flows for nivel_reservatorio project
+"""
 
-from prefect import Parameter
+from copy import deepcopy
+
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
 from pipelines.constants import constants
-from pipelines.utils.decorators import Flow
-from pipelines.rj_rioaguas.saneamento_drenagem.nivel_reservatorio import (
-    download_data,
-    parse_data,
-    save_report,
+
+from pipelines.rj_rioaguas.saneamento_drenagem.nivel_reservatorio.schedules import (
+    gsheets_five_minute_update_schedule,
 )
 
+from pipelines.utils.dump_url.flows import dump_url_flow
+from pipelines.utils.utils import set_default_parameters
 
-with Flow(
-    "RIOAGUAS: nivel - Nivel dos reservatorios nas Prcs Varnhagen, Niteroi e Bandeira",
-    code_owners=["Anderson"],
-) as saneamento_drenagem_nivel_reservatorio:
-
-    # Tasks
-    data = download_data()
-    dataframe = parse_data(data)
-    save_report(dataframe)
-
-saneamento_drenagem_nivel_reservatorio.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
-saneamento_drenagem_nivel_reservatorio.run_config = KubernetesRun(
-    image=constants.DOCKER_IMAGE.value, labels=[constants.RJ_RIOAGUAS_AGENT_LABEL.value]
+nivel_gsheets_flow = deepcopy(dump_url_flow)
+nivel_gsheets_flow.name = (
+    "RIOAGUAS: nivel - Nivel dos reservatorios nas Prcs Varnhagen, Niteroi e Bandeira"
 )
-saneamento_drenagem_nivel_reservatorio.schedule = None
+nivel_gsheets_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+nivel_gsheets_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_RIOAGUAS_AGENT_LABEL.value,
+    ],
+)
+
+nivel_gsheets_flow_parameters = {
+    "dataset_id": "saneamento_drenagem.nivel_reservatorio",
+    "dump_mode": "overwrite",
+    "url": "https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vQd3-V6K_hOcrVySYJKk0tevS9TCI0MpwQ5W7IY-_fIUUR4uZ0JVttqmaHeA9Pm-BJsAXUmjTvLZaDt/pubhtml?widget=true&headers=false#gid=1343658906",
+    "url_type": "google_sheet",
+    "gsheets_sheet_name": "Reservatórios",
+    "table_id": "test_table",
+}
+
+nivel_gsheets_flow = set_default_parameters(
+    nivel_gsheets_flow, default_parameters=nivel_gsheets_flow_parameters
+)
+
+nivel_gsheets_flow.schedule = gsheets_five_minute_update_schedule
---
+++ pipelines/rj_rioaguas/saneamento_drenagem/nivel_reservatorio/schedules.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Schedules for the database dump pipeline
+"""
+
+from datetime import datetime, timedelta
+
+import pytz
+from prefect.schedules import Schedule
+from pipelines.constants import constants
+from pipelines.utils.dump_url.utils import generate_dump_url_schedules
+from pipelines.utils.utils import untuple_clocks as untuple
+
+#####################################
+#
+# EGPWeb Schedules
+#
+#####################################
+
+gsheets_urls = {
+    "test_table": {
+        "dump_mode": "overwrite",
+        "url": "https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vQd3-V6K_hOcrVySYJKk0tevS9TCI0MpwQ5W7IY-_fIUUR4uZ0JVttqmaHeA9Pm-BJsAXUmjTvLZaDt/pubhtml?widget=true&headers=false#gid=1343658906",
+        "url_type": "google_sheet",
+        "gsheets_sheet_name": "Reservatórios",
+    },
+}
+
+
+gsheets_clocks = generate_dump_url_schedules(
+    interval=timedelta(minutes=1),
+    start_date=datetime(2022, 11, 17, 12, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[
+        constants.RJ_ESCRITORIO_DEV_AGENT_LABEL.value,
+    ],
+    dataset_id="saneamento_drenagem.nivel_reservatorio",
+    table_parameters=gsheets_urls,
+)
+
+gsheets_five_minute_update_schedule = Schedule(clocks=untuple(gsheets_clocks))
