b1c9a2ae1b2af558b4fa1efcb0c7bb545bfe474a
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2022-11-29 17:58:56 -0300

    Revert "resolve merge conflict"
    This reverts commit ac511fc9a345926a4e6aacc35bc8df08227218c1, reversing
    changes made to 232950bf0f8dd89e8fb25080248a4768be5ff515.

--- pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps/tasks.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps/tasks.py
@@ -96,6 +96,8 @@ def pre_treatment_br_rj_riodejaneiro_onibus_realocacao(
         df_realocacao.dataSaida == "1971-01-01 00:00:00", "dataSaida"
     ] = ""
 
+    # TODO: separar os filtros num dicionario
+
     # Renomeia colunas
     cols = {
         "veiculo": "id_veiculo",
