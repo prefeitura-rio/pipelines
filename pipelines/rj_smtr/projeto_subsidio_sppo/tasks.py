733da316fa0965434b61a5bfe7c418a0add96015
Fernanda Scovino <fscovinom@gmail.com>
2022-12-08 21:51:19 -0300

    revert task changes

--- pipelines/rj_smtr/projeto_subsidio_sppo/tasks.py
+++ pipelines/rj_smtr/projeto_subsidio_sppo/tasks.py
@@ -12,19 +12,16 @@ from pipelines.utils.tasks import log, get_now_date
 
 
 @task
-def get_run_dates(date_range_start: str, date_range_end: str = None) -> List:
+def get_run_dates(date_range_start: str, date_range_end: str) -> List:
     """
     Generates a list of dates between date_range_start and date_range_end.
     """
-    if (date_range_end is None) and (date_range_start is not False):
-        dates = [{"run_date": date_range_start}]
-    elif (date_range_start is False) or (date_range_end is False):
-        dates = [{"run_date": get_now_date.run()}]
+    if (date_range_start is False) or (date_range_end is False):
+        dates = [{"run_date": datetime.date.today().strftime("%Y-%m-%d")}]
     else:
         dates = [
             {"run_date": d.strftime("%Y-%m-%d")}
             for d in pd.date_range(start=date_range_start, end=date_range_end)
         ]
-
     log(f"Will run the following dates: {dates}")
     return dates
