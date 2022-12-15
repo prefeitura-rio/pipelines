d143995e3b24997efbd7bc89e16cdb9661fde2f7
Replit user <>
2022-12-15 12:50:25 +0000

    modificando pasta de lugar

--- pipelines/rj_rioaguas/__init__.py
+++ pipelines/rj_rioaguas/__init__.py
@@ -3,4 +3,5 @@
 Prefect flows para níveis dos reservatórios
 """
 from pipelines.rj_rioaguas.clima_fluviometro.nivel_lagoa.flows import *
-from pipelines.rj_rioaguas.nivel_lamina_agua.flows import *
+from pipelines.rj_rioaguas.saneamento_drenagem.nivel_lamina_agua.flows import *
+from pipelines.rj_rioaguas.saneamento_drenagem.nivel_reservatorio.flows import *
