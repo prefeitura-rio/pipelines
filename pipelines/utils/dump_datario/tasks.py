3eaca13f3c388bed242e95d8a6b3d77e0bc31646
d116626 <d116626@gmail.com>
2022-12-20 21:54:10 -0300

    feat: add geometry_wkt

--- pipelines/utils/dump_datario/tasks.py
+++ pipelines/utils/dump_datario/tasks.py
@@ -9,6 +9,7 @@ from typing import Union
 from datetime import datetime, timedelta
 
 import pandas as pd
+from geojsplit import geojsplit
 import geopandas as gpd
 from prefect import task
 import requests
@@ -79,53 +80,61 @@ def transform_geodataframe(
     save_path = path / "csv_data" / f"{eventid}.csv"
     save_path.parent.mkdir(parents=True, exist_ok=True)
 
-    with pd.read_csv(file_path, chunksize=chunksize) as reader:
-        for index, dataframe in enumerate(reader):
-            geodataframe = gpd.GeoDataFrame(dataframe)
-            log(f"{index}: Geodatagrame loaded")
-            log(f"{index}: Original columns: {geodataframe.columns.tolist()}")
-            geodataframe.columns = remove_columns_accents(geodataframe)
-            log(f"{index}: New columns: {geodataframe.columns.tolist()}")
+    geojson = geojsplit.GeoJSONBatchStreamer(file_path)
 
-            geodataframe["geometry_wkt"] = geodataframe[geometry_column].copy()
+    for index, feature_collection in enumerate(geojson.stream(batch=chunksize)):
 
-            if convert_to_crs_4326:
+        geodataframe = gpd.GeoDataFrame.from_features(feature_collection["features"])
 
-                try:
-                    geodataframe.crs = "epsg:4326"
-                    geodataframe[geometry_column] = geodataframe[
-                        geometry_column
-                    ].to_crs("epsg:4326")
-                except Exception as err:
-                    log(f"{index}:Error converting to crs 4326: {err}")
-                    raise err
+        cols = geodataframe.columns.tolist()
+        cols.remove(geometry_column)
+        cols.append(geometry_column)
+        geodataframe.columns = cols
 
-                log(f"{index}: geometry converted to crs 4326")
+        log(f"{index}: Geodatagrame loaded")
+        log(f"{index}: Original columns: {geodataframe.columns.tolist()}")
+        geodataframe.columns = remove_columns_accents(geodataframe)
+        log(f"{index}: New columns: {geodataframe.columns.tolist()}")
 
-            if geometry_3d_to_2d:
-                try:
-                    geodataframe[geometry_column] = (
-                        geodataframe[geometry_column].astype(str).apply(load_wkt)
-                    )
+        geodataframe["geometry_wkt"] = geodataframe[geometry_column].copy()
 
-                    geodataframe[geometry_column] = geodataframe[geometry_column].apply(
-                        lambda geom: remove_third_dimension(geom)
-                    )
-                except Exception as err:
-                    log(f"{index}: Error converting 3d to 2d: {err}")
-                    raise err
+        if convert_to_crs_4326:
 
-                log("geometry converted 3D to 2D")
+            try:
+                geodataframe.crs = "epsg:4326"
+                geodataframe[geometry_column] = geodataframe[geometry_column].to_crs(
+                    "epsg:4326"
+                )
+            except Exception as err:
+                log(f"{index}:Error converting to crs 4326: {err}")
+                raise err
 
-            log(f"{index}:  New columns: {geodataframe.columns.tolist()}")
+            log(f"{index}: geometry converted to crs 4326")
 
-            geodataframe.to_csv(
-                save_path,
-                index=False,
-                encoding="utf-8",
-                mode="a",
-                header=not save_path.exists(),
-            )
-            log(f"{index}: Data saved")
+        if geometry_3d_to_2d:
+            try:
+                geodataframe[geometry_column] = (
+                    geodataframe[geometry_column].astype(str).apply(load_wkt)
+                )
+
+                geodataframe[geometry_column] = geodataframe[geometry_column].apply(
+                    lambda geom: remove_third_dimension(geom)
+                )
+            except Exception as err:
+                log(f"{index}: Error converting 3d to 2d: {err}")
+                raise err
+
+            log("geometry converted 3D to 2D")
+
+        log(f"{index}:  New columns: {geodataframe.columns.tolist()}")
+
+        geodataframe.to_csv(
+            save_path,
+            index=False,
+            encoding="utf-8",
+            mode="a",
+            header=not save_path.exists(),
+        )
+        log(f"{index}: Data saved")
 
     return save_path
