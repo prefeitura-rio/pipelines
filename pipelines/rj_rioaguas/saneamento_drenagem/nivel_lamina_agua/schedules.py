287f02b406268ebfbb49b28e34f438f5fd8743b6
João Paulo Fraga <69409538+jpfraga@users.noreply.github.com>
2022-12-21 21:59:56 -0300

    alterando parametros para permitir materializacao

--- pipelines/rj_rioaguas/saneamento_drenagem/nivel_lamina_agua/schedules.py
+++ pipelines/rj_rioaguas/saneamento_drenagem/nivel_lamina_agua/schedules.py
@@ -12,6 +12,18 @@ MINUTE_SCHEDULE = Schedule(
         IntervalClock(
             interval=timedelta(minutes=10),
             start_date=datetime(2021, 1, 1, 0, 1, 0),
+            labels=[
+                constants.RJ_RIOAGUAS_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+                "dataset_id": "saneamento_dragagem",
+                "table_id": "nivel_lamina_agua_via",
+                "dump_mode": "append",
+                "materialize_after_dump": True,
+                "materialization_mode": "dev",
+                "materialize_to_datario": False,
+                "dump_to_gcs": False,
+            }
         ),
     ]
 )
