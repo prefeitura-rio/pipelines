6d01caf82267b78af93756a799e3ef8d2f2a99dd
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-01-06 18:29:20 -0300

    update datario flow param

--- pipelines/rj_smtr/projeto_subsidio_sppo/flows.py
+++ pipelines/rj_smtr/projeto_subsidio_sppo/flows.py
@@ -140,7 +140,7 @@ with Flow(
             "dataset_id": "transporte_rodoviario_municipal",
             "table_id": "viagem_onibus",
             "mode": "prod",
-            "dbt_model_parameters": dict(date_range_end=run_date),
+            "dbt_model_parameters": dict(date_range_end=end_date),
         },
     )
 
