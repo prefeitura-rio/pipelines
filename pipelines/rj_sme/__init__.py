8c7fb066c8e4c09da31076d9447af5bac9c86677
Bruno Almeida <brunodepauloalmeida@gmail.com>
2023-01-11 14:53:08 -0300

    typo

--- pipelines/rj_sme/__init__.py
+++ pipelines/rj_sme/__init__.py
@@ -4,4 +4,4 @@ Prefect flows for sme project
 """
 
 from pipelines.rj_sme.dump_db_educacao_basica.flows import *
-from pipelines.rj_sme.dump_url_educacao_basica import *
+from pipelines.rj_sme.dump_url_educacao_basica.flows import *
