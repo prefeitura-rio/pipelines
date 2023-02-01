5defd3bce9cc61a7a6ef86b8d6b474c9fc58bc9b
José Felipe - Paquetá <josefelipe@rioeduca.net>
2023-02-01 18:53:07 -0300

    Ajuste no nome do flows import

--- pipelines/rj_sme/__init__.py
+++ pipelines/rj_sme/__init__.py
@@ -5,4 +5,4 @@ Prefect flows for sme project
 
 from pipelines.rj_sme.dump_db_educacao_basica.flows import *
 from pipelines.rj_sme.dump_url_educacao_basica.flows import *
-from pipelines.rj_sme.dump_prova_rio_2009_a_2016_anonimizado import *
+from pipelines.rj_sme.dump_prova_rio_2009_a_2016_anonimizado.flows import *
