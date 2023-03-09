650bcdda67050956267d618ae84cd2eff35b3cb4
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-03-09 15:33:52 -0300

    update task pre_treatment_sppo_infracao

--- pipelines/rj_smtr/veiculo/tasks.py
+++ pipelines/rj_smtr/veiculo/tasks.py
@@ -16,9 +16,7 @@ from pipelines.utils.utils import log  # ,get_vault_secret
 
 from pipelines.rj_smtr.veiculo.constants import constants
 from pipelines.rj_smtr.utils import (
-    check_not_null,
     data_info_str,
-    filter_null,
     filter_data,
 )
 
@@ -110,7 +108,6 @@ def pre_treatment_sppo_licenciamento(status: dict, timestamp: datetime):
     return {"data": data, "error": error}
 
 
-# todo: update treatment from above task # pylint: disable=W0102
 @task
 def pre_treatment_sppo_infracao(status: dict, timestamp: datetime):
     """Basic data treatment for violation data. Apply filtering to raw data.
@@ -128,56 +125,63 @@ def pre_treatment_sppo_infracao(status: dict, timestamp: datetime):
     if status["error"] is not None:
         return {"data": pd.DataFrame(), "error": status["error"]}
 
-    error = None
-    data = pd.json_normalize(status["data"])
+    try:
+        error = None
+        data = pd.json_normalize(status["data"])
 
-    log(
-        f"""
-    Received inputs:
-    - timestamp:\n{timestamp}
-    - data:\n{data.head()}"""
-    )
+        log(
+            f"""
+        Received inputs:
+        - timestamp:\n{timestamp}
+        - data:\n{data.head()}"""
+        )
 
-    log(f"Raw data:\n{data_info_str(data)}", level="info")
+        log(f"Raw data:\n{data_info_str(data)}", level="info")
 
-    # Rename columns
-    columns = constants.SPPO_INFRACAO_MAPPING_KEYS.value
-    data = data.rename(columns=columns)
+        log("Renaming columns...", level="info")
+        data = data.rename(columns=constants.SPPO_INFRACAO_MAPPING_KEYS.value)
 
-    # Strip str columns
-    for col in columns.values():
-        data[col] = data[col].str.strip().replace("", np.nan)
+        log("Adding captured timestamp column...", level="info")
+        data["timestamp_captura"] = timestamp
 
-    # Update valor type to float
-    data["valor"] = data["valor"].str.replace(",", ".").astype(float)
+        log("Striping string columns and replacing empty strings...", level="info")
+        for col in data.columns[data.dtypes == "object"].to_list():
+            data[col] = data[col].str.strip().replace("", np.nan)
 
-    # Check primary keys
-    pk_columns = ["placa", "id_auto_infracao"]
-    check_new_data = f"data_infracao == '{timestamp.strftime('%Y-%m-%d')}'"
+        log("Updating valor type to float...", level="info")
+        data["valor"] = data["valor"].str.replace(",", ".").astype(float)
 
-    check_not_null(data, pk_columns, subset_query=check_new_data)
+        filters = ["modo != 'ONIBUS'"]
+        log(f"Filtering '{filters}'...", level="info")
+        data = filter_data(data, filters)
 
-    # Filter data
-    filters = ["modo != 'ONIBUS'"]
+        log("Filtering null primary keys...", level="info")
+        primary_key = ["placa", "id_auto_infracao"]
+        data.dropna(subset=primary_key, inplace=True)
 
-    data = filter_data(data, filters)
+        # Check primary keys
+        # pk_columns = ["placa", "id_auto_infracao"]
+        # check_new_data = f"data_infracao == '{timestamp.strftime('%Y-%m-%d')}'"
 
-    # Filter null values in primary keys
-    filter_new_data = f"data_infracao == '{timestamp.strftime('%Y-%m-%d')}'"
+        # check_not_null(data, pk_columns, subset_query=check_new_data)
 
-    data = filter_null(data, pk_columns, subset_query=filter_new_data)
+        log(
+            f"Finished cleaning! Pre-treated data:\n{data_info_str(data)}", level="info"
+        )
 
-    log(f"Pre-treated data:\n{data_info_str(data)}", level="info")
+        log("Creating nested structure...", level="info")
+        data = (
+            data.groupby(primary_key + ["timestamp_captura"])
+            .apply(lambda x: x[data.columns.difference(primary_key)].to_json())
+            .reset_index(name="content")
+        )
 
-    # Create nested structure
-    df_treated = data[pk_columns].copy()
+        log(
+            f"Finished nested structure! Pre-treated data:\n{data_info_str(data)}",
+            level="info",
+        )
 
-    df_treated["content"] = data[data.columns.difference(pk_columns)].apply(
-        lambda x: x.to_json(), axis=1
-    )
+    except Exception as exp:  # pylint: disable=W0703
+        error = exp
 
-    df_treated["timestamp_captura"] = timestamp
-
-    log(f"Pre-treated data:\n{data_info_str(df_treated)}", level="info")
-
-    return {"data": df_treated, "error": error}
+    return {"data": data, "error": error}
