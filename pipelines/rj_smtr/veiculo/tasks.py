c9c6bcd4ab2f3994d7e3f91ad780e22ef2de17f3
Fernanda Scovino <fscovinom@gmail.com>
2023-03-09 11:13:35 -0300

    refactor pre_treatment_sppo_licenciamento

--- pipelines/rj_smtr/veiculo/tasks.py
+++ pipelines/rj_smtr/veiculo/tasks.py
@@ -17,8 +17,6 @@ from pipelines.utils.utils import log  # ,get_vault_secret
 from pipelines.rj_smtr.veiculo.constants import constants
 from pipelines.rj_smtr.utils import (
     check_not_null,
-    convert_boolean,
-    check_relation,
     data_info_str,
     filter_null,
     filter_data,
@@ -44,84 +42,67 @@ def pre_treatment_sppo_licenciamento(status: dict, timestamp: datetime):
     if status["error"] is not None:
         return {"data": pd.DataFrame(), "error": status["error"]}
 
-    error = None
-    data = pd.json_normalize(status["data"])
+    try:
+        error = None
+        data = pd.json_normalize(status["data"])
 
-    log(
-        f"""
-    Received inputs:
-    - timestamp:\n{timestamp}
-    - data:\n{data.head()}"""
-    )
+        log(
+            f"""
+        Received inputs:
+        - timestamp:\n{timestamp}
+        - data:\n{data.head()}"""
+        )
 
-    log(f"Raw data:\n{data_info_str(data)}", level="info")
+        log(f"Raw data:\n{data_info_str(data)}", level="info")
 
-    # Rename columns
-    columns = constants.SPPO_LICENCIAMENTO_MAPPING_KEYS.value
-    data = data.rename(columns=columns)
+        log("Renaming columns...", level="info")
+        data = data.rename(columns=constants.SPPO_LICENCIAMENTO_MAPPING_KEYS.value)
 
-    # Strip str columns
-    str_columns = data.columns[data.dtypes == "object"].to_list()
-    for col in str_columns:
-        data[col] = data[col].str.strip()
+        log("Adding captured timestamp column...", level="info")
+        data["timestamp_captura"] = timestamp
 
-    # Convert to boolean
-    boolean_cols = data.columns[data.columns.str.contains("indicador")].to_list()
-    data = convert_boolean(
-        data=data, columns=boolean_cols, dict_keys={"Sim": True, "Nao": False}
-    )
+        log("Striping string columns...", level="info")
+        for col in data.columns[data.dtypes == "object"].to_list():
+            data[col] = data[col].str.strip()
 
-    # Check data
-    check_columns = [["id_veiculo", "placa"], ["tipo_veiculo", "id_planta"]]
+        log("Converting boolean values...", level="info")
+        for col in data.columns[data.columns.str.contains("indicador")].to_list():
+            data[col] = data[col].map({"Sim": True, "Nao": False})
 
-    check_relation(data, check_columns)
+        # Check data
+        # check_columns = [["id_veiculo", "placa"], ["tipo_veiculo", "id_planta"]]
 
-    # Check primary keys
-    pk_columns = ["id_veiculo"]
+        # check_relation(data, check_columns)
 
-    check_not_null(data, pk_columns)
+        log("Filtering null primary keys...", level="info")
+        primary_key = "id_veiculo"
+        data.dropna(subset=[primary_key], inplace=True)
 
-    # Check relevant columns
-    relevant_columns = ["tipo_veiculo"]
+        log("Update indicador_ar_condicionado based on tipo_veiculo...", level="info")
+        data["indicador_ar_condicionado"] = data["tipo_veiculo"].map(
+            lambda x: None if isinstance(x) != str else bool("C/AR" in x.replace(" ", ""))
+        )
 
-    check_not_null(data, relevant_columns)
+        log(f"Finished cleaning! Pre-treated data:\n{data_info_str(data)}", level="info")
 
-    # Filter data
-    # Filter Veículos rodoviários (TBD)
-    filters = ["tipo_veiculo.str.contains('ROD')"]
+        log("Creating nested structure...", level="info")
+        data = (data.groupby([primary_key, "timestamp_captura"])
+            .apply(lambda x: x[data.columns.difference(primary_key)].to_dict('records'))
+            .reset_index()
+            .rename(columns={0: "content"})
+            .to_json(orient='records')
+        )
+    
+    except Exception as exp: # pylint: disable=W0703
+        error = exp
+    
+    if error is not None:
+        log(f"[CATCHED] Task failed with error: \n{error}", level="error")
 
-    data = filter_data(data, filters)
-
-    # Filter null primary keys
-    data = filter_null(data, pk_columns)
-
-    # Filter null relevant columns
-    data = filter_null(data, relevant_columns)
-
-    # Update indicador_ar_condicionado based on tipo_veiculo
-    data["indicador_ar_condicionado"] = data["tipo_veiculo"].map(
-        lambda x: True if "C/AR" in x.replace(" ", "") else False
-    )
-
-    # Add status
-    data["status"] = "Licenciado"
-
-    log(f"Pre-treated data:\n{data_info_str(data)}", level="info")
-
-    # Create nested structure
-    df_treated = data[pk_columns].copy()
-
-    df_treated["content"] = data[data.columns.difference(pk_columns)].apply(
-        lambda x: x.to_json(), axis=1
-    )
-
-    df_treated["timestamp_captura"] = timestamp
-
-    log(f"Pre-treated data:\n{data_info_str(df_treated)}", level="info")
-
-    return {"data": df_treated, "error": error}
+    return {"data": data, "error": error}
 
 
+# todo: update treatment from above task # pylint: disable=W0102
 @task
 def pre_treatment_sppo_infracao(status: dict, timestamp: datetime):
     """Basic data treatment for violation data. Apply filtering to raw data.
