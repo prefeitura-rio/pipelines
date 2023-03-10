bb4a75e3eecc06e265df5c52bdf73e470e72dc9e
Fernanda Scovino <fscovinom@gmail.com>
2023-03-10 16:43:03 -0300

    fix content

--- pipelines/rj_smtr/veiculo/tasks.py
+++ pipelines/rj_smtr/veiculo/tasks.py
@@ -175,7 +175,7 @@ def pre_treatment_sppo_infracao(status: dict, timestamp: datetime):
         log("Creating nested structure...", level="info")
         data = (
             data.groupby(primary_key + ["timestamp_captura"])
-            .apply(lambda x: x[data.columns.difference(primary_key)].to_json(orient="records")))
+            .apply(lambda x: x[data.columns.difference(primary_key)].to_json(orient="records"))
             .reset_index(name="content")[primary_key + ["content", "timestamp_captura"]]
         )
 
