a7795eef1d1501a4f67c5ffc12f7eb634e589898
hellcassius <caiorogerio.santos@gmail.com>
2023-03-14 13:06:54 -0300

    change code owner

--- pipelines/rj_smtr/br_rj_riodejaneiro_stpl_gps/flows.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_stpl_gps/flows.py
@@ -39,9 +39,7 @@ from pipelines.rj_smtr.br_rj_riodejaneiro_stpl_gps.tasks import (
 )
 
 
-with Flow(
-    "SMTR: GPS STPL - Captura",
-) as captura_stpl:
+with Flow("SMTR: GPS STPL - Captura", code_owners=["caio"]) as captura_stpl:
 
     # DEFAULT PARAMETERS #
     dataset_id = Parameter(
