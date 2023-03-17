ac9a9c8ffdf61c222bf8ab90835d07e15cfbc7f3
Fernanda Scovino <fscovinom@gmail.com>
2023-03-17 14:59:50 -0300

    run test in dev

--- pipelines/rj_smtr/br_rj_riodejaneiro_brt_gps/flows.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_brt_gps/flows.py
@@ -193,6 +193,6 @@ with Flow(
 captura_brt.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 captura_brt.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
-    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
+    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
 )
 captura_brt.schedule = every_minute_dev
