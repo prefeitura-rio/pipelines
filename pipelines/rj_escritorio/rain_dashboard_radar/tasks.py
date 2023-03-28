0c95fefeb1407bfb5bae2929ecd3ff1a4ec407f3
Replit user <>
2023-03-28 18:17:30 +0000

    bugfix

--- pipelines/rj_escritorio/rain_dashboard_radar/tasks.py
+++ pipelines/rj_escritorio/rain_dashboard_radar/tasks.py
@@ -6,46 +6,13 @@ Tasks for setting rain dashboard using radar data.
 import json
 from pathlib import Path
 import os
-from google.cloud import storage
 import pendulum
 from prefect import task
 
-from pipelines.rj_escritorio.rain_dashboard_radar.utils import download_blob
-
-
-@task()
-def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
-    """Lists all the blobs in the bucket that begin with the prefix.
-
-    This can be used to list all blobs in a "folder", e.g. "public/".
-
-    The delimiter argument can be used to restrict the results to only the
-    "files" in the given "folder". Without the delimiter, the entire tree under
-    the prefix is returned. For example, given these blobs:
-
-        a/1.txt
-        a/b/2.txt
-
-    If you specify prefix ='a/', without a delimiter, you'll get back:
-
-        a/1.txt
-        a/b/2.txt
-
-    However, if you specify prefix='a/' and delimiter='/', you'll get back
-    only the file directly under 'a/':
-
-        a/1.txt
-
-    As part of the response, you'll also get back a blobs.prefixes entity
-    that lists the "subfolders" under `a/`:
-
-        a/b/
-    """
-
-    storage_client = storage.Client()
-    blobs = storage_client.list_blobs(bucket_name, prefix=prefix, delimiter=delimiter)
-
-    return blobs
+from pipelines.rj_escritorio.rain_dashboard_radar.utils import (
+    download_blob,
+    list_blobs_with_prefix,
+)
 
 
 @task()
--- pipelines/rj_escritorio/rain_dashboard_radar/utils.py
+++ pipelines/rj_escritorio/rain_dashboard_radar/utils.py
@@ -6,6 +6,40 @@ General utils for setting rain dashboard using radar data.
 from google.cloud import storage
 
 
+def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
+    """Lists all the blobs in the bucket that begin with the prefix.
+
+    This can be used to list all blobs in a "folder", e.g. "public/".
+
+    The delimiter argument can be used to restrict the results to only the
+    "files" in the given "folder". Without the delimiter, the entire tree under
+    the prefix is returned. For example, given these blobs:
+
+        a/1.txt
+        a/b/2.txt
+
+    If you specify prefix ='a/', without a delimiter, you'll get back:
+
+        a/1.txt
+        a/b/2.txt
+
+    However, if you specify prefix='a/' and delimiter='/', you'll get back
+    only the file directly under 'a/':
+
+        a/1.txt
+
+    As part of the response, you'll also get back a blobs.prefixes entity
+    that lists the "subfolders" under `a/`:
+
+        a/b/
+    """
+
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name, prefix=prefix, delimiter=delimiter)
+
+    return blobs
+
+
 def download_blob(bucket_name, source_blob_name, destination_file_name) -> None:
     """
     Downloads a blob mencioned on source_blob_name from bucket_name
