46172d0726d21caa2d9cd4323400dc61a37c1583
Edison Moreira <moreira.edison@gmail.com>
2023-04-06 13:18:22 -0300

    feat: change name folder and variable

--- pipelines/rj_escritorio/formacao_infra_edison/flows.py
+++ pipelines/rj_escritorio/formacao_infra_edison/flows.py
@@ -10,17 +10,15 @@ from pipelines.rj_escritorio.formacao_infra_edison.tasks import (
 from pipelines.utils.decorators import Flow
 
 with Flow(
-    "EMD: formacao - Infraestrutura Edison Moreira",
+    "EMD: Formacao - Infraestrutura Edison Moreira",
     code_owners=["gabriel", "diego", "edisonmoreira"],
-) as rj_escritorio_formacao_infra_example_flow:
+) as rj_escritorio_formacao_infra_edison_flow:
     name = Parameter("name", default="Cosmos")
     hello_name(name=name)
 
 
-rj_escritorio_formacao_infra_example_flow.storage = GCS(
-    constants.GCS_FLOWS_BUCKET.value
-)
-rj_escritorio_formacao_infra_example_flow.run_config = KubernetesRun(
+rj_escritorio_formacao_infra_edison_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+rj_escritorio_formacao_infra_edison_flow.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value,
     labels=[constants.RJ_ESCRITORIO_DEV_AGENT_LABEL.value],
 )
