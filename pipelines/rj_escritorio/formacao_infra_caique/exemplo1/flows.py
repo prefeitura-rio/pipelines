dc8e64b41dfaee37a518e68f93ee987da01263cd
caiquerj <caique.rj@gmail.com>
2023-04-07 19:15:46 -0300

    Primeiro exercicio com exemplo de flow - Caique

--- pipelines/rj_escritorio/formacao_infra_caique/exemplo1/flows.py
+++ pipelines/rj_escritorio/formacao_infra_caique/exemplo1/flows.py
@@ -4,22 +4,24 @@ from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
 from pipelines.constants import constants
-from pipelines.rj_escritorio.formacao_infra_caique.tasks import (
+from pipelines.rj_escritorio.formacao_infra_caique.exemplo1.tasks import (
     hello_name,
 )
 from pipelines.utils.decorators import Flow
 
 with Flow(
-    "EMD: formacao - Exemplo formação em infraestrutura - Caique",
+    "EMD: formacao - Exemplo 1 do curso de formação em infraestrutura - Caique",
     code_owners=[
         "gabriel",
         "diego",
     ],
-) as rj_escritorio_formacao_infra_caique_flow:
-    name = Parameter("name", default="World")
+) as rj_escritorio_formacao_infra_caique_exemplo1_flow:
+    name = Parameter("name", default="Caique")
     hello_name(name=name)
 
-rj_escritorio_formacao_infra_caique_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+rj_escritorio_formacao_infra_caique_exemplo1_flow.storage = GCS(
+    constants.GCS_FLOWS_BUCKET.value
+)
 GCS.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value,
     labels=[constants.RJ_ESCRITORIO_DEV_AGENT_LABEL.value],
