4c27118b2fe38f8b656476bbb0249024395f6614
patriciacatandi <patriciabcatandi@gmail.com>
2023-04-10 21:10:48 -0300

    alteração query dashboard radar

--- pipelines/rj_cor/meteorologia/radar/precipitacao/constants.py
+++ pipelines/rj_cor/meteorologia/radar/precipitacao/constants.py
@@ -29,10 +29,11 @@ class constants(Enum):  # pylint: disable=c0103
         SELECT
             id_h3,
             bairro,
-            chuva_15min,
+            CASE
+                WHEN chuva_15min<= 0.05 THEN 0 ELSE ROUND(chuva_15min, 2) END AS chuva_15min,
             estacoes,
             CASE
-                WHEN chuva_15min> 0     AND chuva_15min<= 1.25 THEN 'chuva fraca'
+                WHEN chuva_15min> 0.05  AND chuva_15min<= 1.25 THEN 'chuva fraca'
                 WHEN chuva_15min> 1.25  AND chuva_15min<= 6.25 THEN 'chuva moderada'
                 WHEN chuva_15min> 6.25  AND chuva_15min<= 12.5 THEN 'chuva forte'
                 WHEN chuva_15min> 12.5                         THEN 'chuva muito forte'
