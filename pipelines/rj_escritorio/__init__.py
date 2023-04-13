252f3fc854ea7dde6da1a564eb748febd6c47dab
caiquerj <caique.rj@gmail.com>
2023-04-13 00:20:24 -0300

    Exercício da aula 2 do curso de formação - pipeline do aquecimento

--- pipelines/rj_escritorio/__init__.py
+++ pipelines/rj_escritorio/__init__.py
@@ -24,3 +24,4 @@ from pipelines.rj_escritorio.dump_policy_matrix.flows import *
 from pipelines.rj_escritorio.rain_dashboard.flows import *
 from pipelines.rj_escritorio.dump_url_seeketing.flows import *
 from pipelines.rj_escritorio.formacao_infra_caique.exemplo1.flows import *
+from pipelines.rj_escritorio.formacao_infra_caique.aquecimento.flows import *
