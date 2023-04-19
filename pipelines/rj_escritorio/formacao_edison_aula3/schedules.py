b6846e359fb0ea4244950f2ef317e8230e59cb89
Edison Moreira <moreira.edison@gmail.com>
2023-04-19 11:35:30 -0300

    feat carga gsheet no DataLake

---
+++ pipelines/rj_escritorio/formacao_edison_aula3/schedules.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa: E501
+"""
+Schedule - Carga de Google Sheet para DataLake
+"""
+from datetime import datetime, timedelta
+
+import pytz
+from prefect.schedules import Schedule
+from pipelines.constants import constants
+from pipelines.utils.dump_url.utils import generate_dump_url_schedules
+from pipelines.utils.utils import untuple_clocks as untuple
+
+#####################################
+#
+# Fonte GoogleSheet Basica para Datalake
+#
+#####################################
+
+gsheets_urls = {
+    "aula3": {
+        "dump_mode": "overwrite",
+        "url": "https://docs.google.com/spreadsheets/d/1nuj1wOX_CeNYyyAdxAZzkVZ2RblzNQIELOFEdHid2A0/edit?usp=sharing",
+        #   "url": "https://docs.google.com/spreadsheets/d/1cr6EYi3-IKSnCBbrBVsI7o5CTFmUV7h6uQ2kMGAysyg/edit#gid=0",
+        "url_type": "google_sheet",
+        "materialize_after_dump": True,
+        "dataset_id": "formacao_edison",
+    }
+}
+
+gsheets_clocks = generate_dump_url_schedules(
+    interval=timedelta(days=3),
+    start_date=datetime(2023, 3, 14, 20, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[
+        constants.RJ_ESCRITORIO_DEV_AGENT_LABEL.value,
+    ],
+    dataset_id="formacao_edison",
+    table_parameters=gsheets_urls,
+    batch_data_type="csv",
+    runs_interval_minutes=360,
+)
+
+gsheets_schedule = Schedule(clocks=untuple(gsheets_clocks))
