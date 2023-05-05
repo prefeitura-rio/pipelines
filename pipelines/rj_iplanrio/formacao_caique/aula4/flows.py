4c3b8e3ec4d80df5c298d6ca2f44536b6cfe1786
Carlos Henrique Santos da Silva <55252238+caiquerj@users.noreply.github.com>
2023-05-04 23:08:42 -0300

    Update flows.py

--- pipelines/rj_iplanrio/formacao_caique/aula4/flows.py
+++ pipelines/rj_iplanrio/formacao_caique/aula4/flows.py
@@ -40,7 +40,7 @@ rj_iplanrio_formacao_caique_dump_1746_default_parameters = {
     "db_type": "sql_server",
     "db_database": "REPLICA1746",
     # parametros do destino
-    "dataset_id": "formacao_1746_teste",
+    "dataset_id": "formacao_1746_caique",
     # credenciais
     "vault_secret_path": "clustersql2",
 }
