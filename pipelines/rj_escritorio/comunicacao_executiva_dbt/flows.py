996ceb31df575c2190a1569854f1ce9c82a2882d
Bruno Almeida <brunodepauloalmeida@gmail.com>
2023-05-16 15:26:54 -0300

    add: dbt pipeline to update all tables last update date

---
+++ pipelines/rj_escritorio/comunicacao_executiva_dbt/flows.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+DBT-related flows.
+"""
+
+from copy import deepcopy
+
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+
+from pipelines.constants import constants
+from pipelines.rj_escritorio.comunicacao_executiva_dbt.schedules import (
+    comunicacao_executiva_daily_update_schedule,
+)
+from pipelines.utils.execute_dbt_model.flows import utils_run_dbt_model_flow
+
+run_dbt_comunicacao_executiva_flow = deepcopy(utils_run_dbt_model_flow)
+run_dbt_comunicacao_executiva_flow.name = "EMD: Comunicação Executiva - Materializar tabelas"
+run_dbt_comunicacao_executiva_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+run_dbt_comunicacao_executiva_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value
+)
+run_dbt_comunicacao_executiva_flow.schedule = comunicacao_executiva_daily_update_schedule
---
+++ pipelines/rj_escritorio/comunicacao_executiva_dbt/schedules.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Schedules for the database dump pipeline
+"""
+
+from datetime import datetime, timedelta
+
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
+import pytz
+
+from pipelines.constants import constants
+from pipelines.utils.utils import untuple_clocks as untuple
+
+#####################################
+#
+# Comunicação Executiva Schedules
+#
+#####################################
+
+comunicacao_executiva_tables = {
+    "data_atualizacao": "data_atualizacao",
+}
+
+comunicacao_executiva_clocks = [
+    IntervalClock(
+        interval=timedelta(days=1),
+        start_date=datetime(2023, 5, 16, 17, 0, tzinfo=pytz.timezone("America/Sao_Paulo"))
+        + timedelta(minutes=1 * count),
+        labels=[
+            constants.RJ_ESCRITORIO_DEV_AGENT_LABEL.value,
+        ],
+        parameter_defaults={
+            "dataset_id": "comunicacao_executiva",
+            "table_id": table_id,
+            "mode": "dev",
+        },
+    )
+    for count, (_, table_id) in enumerate(comunicacao_executiva_tables.items())
+]
+comunicacao_executiva_daily_update_schedule = Schedule(
+    clocks=untuple(comunicacao_executiva_clocks)
+)
