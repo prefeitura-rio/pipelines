79dec9ca5e2d42d0b7ae99674e1b80eeabdff077
Joao Vieira <tonhocarioca@gmail.com>
2023-05-18 23:13:31 +0000

    feat:SICOP Joao-v0

--- pipelines/rj_iplanrio/sicop/flows.py
+++ pipelines/rj_iplanrio/sicop/flows.py
@@ -11,7 +11,9 @@ from prefect.storage import GCS
 from pipelines.constants import constants
 
 # importa o schedule
-from pipelines.rj_iplanrio import sicop_infra_daily_update_schedule
+from pipelines.rj_iplanrio.sicop.schedules import (
+    sicop_infra_daily_update_schedule,
+)
 from pipelines.utils.dump_db.flows import dump_sql_flow
 from pipelines.utils.utils import set_default_parameters
 
