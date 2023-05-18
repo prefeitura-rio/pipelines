7a4e0c2d3ecc52d7fd785b36e2274eba54e1a50e
d116626 <d116626@gmail.com>
2023-05-18 11:24:08 -0300

    chore: set biglake_table

--- pipelines/rj_iplanrio/processorio/schedules.py
+++ pipelines/rj_iplanrio/processorio/schedules.py
@@ -23,6 +23,7 @@ _processorio_infra_query = {
     "classificacao": {
         # "partition_columns": "dt_inicio",
         # "lower_bound_date": "2021-01-01",
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
@@ -31,6 +32,7 @@ _processorio_infra_query = {
         "execute_query": "SELECT ID_CLASSIFICACAO, DESCR_CLASSIFICACAO, HIS_ATIVO, CODIFICACAO FROM SIGA.VW_CLASSIFICACAO",  # noqa
     },
     "documento_tempo": {
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
@@ -39,6 +41,7 @@ _processorio_infra_query = {
         "execute_query": "SELECT SIGLA_DOC, DT_PRIMEIRAASSINATURA, ULTIMO_ID_MOV, DT_FINALIZACAO, ARQUIVADO, ID_MOBIL, TEMPO_TRAMITACAO, ID_LOTA_RESP, LOTACAO_RESP, DATA_COM_RESP_ATUAL FROM SIGA.DOCUMENTOS_TEMPO",  # noqa
     },
     "forma_documento": {
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
@@ -47,6 +50,7 @@ _processorio_infra_query = {
         "execute_query": "SELECT ID_FORMA_DOC, DESCR_FORMA_DOC, SIGLA_FORMA_DOC, ID_TIPO_FORMA_DOC FROM SIGA.VW_FORMA_DOCUMENTO",  # noqa
     },
     "lotacao": {
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
@@ -55,6 +59,7 @@ _processorio_infra_query = {
         "execute_query": "SELECT ID_LOTACAO, DATA_INI_LOT, DATA_FIM_LOT, NOME_LOTACAO, ID_LOTACAO_PAI, SIGLA_LOTACAO, ID_ORGAO_USU, IS_EXTERNA_LOTACAO FROM CORPORATIVO.VW_LOTACAO",  # noqa
     },
     "mobil": {
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
@@ -63,6 +68,7 @@ _processorio_infra_query = {
         "execute_query": "SELECT ID_MOBIL, ID_DOC FROM SIGA.VW_MOBIL",  # noqa
     },
     "modelo": {
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
@@ -71,6 +77,7 @@ _processorio_infra_query = {
         "execute_query": "SELECT ID_MOD, NM_MOD, DESC_MOD, HIS_ID_INI, HIS_IDC_INI, HIS_IDC_FIM, HIS_ATIVO, IS_PETICIONAMENTO FROM SIGA.VW_MODELO",  # noqa
     },
     "movimentacao": {
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
@@ -79,6 +86,7 @@ _processorio_infra_query = {
         "execute_query": "SELECT ID_MOV, ID_TP_MOV, ID_CADASTRANTE, ID_LOTA_CADASTRANTE, DT_MOV, DT_FIM_MOV, ID_MOV_REF, ID_MOBIL FROM SIGA.VW_MOVIMENTACAO",  # noqa
     },
     "nivel_acesso": {
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
@@ -87,6 +95,7 @@ _processorio_infra_query = {
         "execute_query": "SELECT ID_NIVEL_ACESSO, NM_NIVEL_ACESSO FROM SIGA.VW_NIVEL_ACESSO",  # noqa
     },
     "orgao_usuario": {
+        "biglake_table": True,
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
