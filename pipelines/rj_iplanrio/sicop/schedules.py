5ac915849e324315cc268d9a2af88b3836462d21
Joao Vieira <tonhocarioca@gmail.com>
2023-05-19 17:37:35 +0000

    feat:SICOP Joao-v1

--- pipelines/rj_iplanrio/sicop/schedules.py
+++ pipelines/rj_iplanrio/sicop/schedules.py
@@ -18,31 +18,8 @@ from pipelines.utils.utils import untuple_clocks as untuple
 #
 #####################################
 
-QUERY_SICOP_APENSO = """
-SELECT CHAVE, NUM_PROCESSO_PRINCIPAL, NUM_PROCESSO_APENSADO, I22005_COD_OPER FROM SICOP.VW_APENSO;
-"""
-QUERY_SICOP_ASSUNTO = """
-SELECT IDENT, COD, SEQ, DESC_ASSUNTO FROM SICOP.VW_ASSUNTO;
-"""
-QUERY_SICOP_DOCUMENTO = """
-SELECT ORG_RESP, ORG_TRANSC, TIPO_DOC, SEC_SIST2, ANO_SIST2, MES_SIST2, DIA_SIST2,
-NUM_DOCUMENTO, REQUERENTE, COD_ASSUN, MAT_TRANSC, ASSUN_COMP FROM SICOP.VW_DOCUMENTO;
-"""
-QUERY_SICOP_ORGAO = """
-SELECT ORG_SICOP, COD_ORCTO FROM SICOP.VW_ORGAO;
-"""
-QUERY_SICOP_PROCESSO = """
-SELECT ORG_TRANSC, NUM_PROCESSO, CPF_CGC, IDENT, REQUERENTE, DIA_PROC2, MES_PROC2, SEC_PROC2,
-ANO_PROC2, COD_ASSU_P, ANO_SIST2, SEC_SIST2, MES_SIST2, DIA_SIST2, DESC_ASSUN, MAT_TRANSC, STATUS
-FROM SICOP.VW_PROCESSO;
-"""
-QUERY_SICOP_TRAMITACAO = """
-SELECT ORG_RESP, TIPO_DOC, NUM_DOCUMENTO, SEQ, SEC_REMES2, ANO_REMES2, MES_REMES2,
-DIA_REMES2, DESTINO, COD_DESP FROM SICOP.VW_TRAMITACAO_DOCUMENTO;
-"""
-
 _sicop_queries = {
-    "sicop_apenso": {
+    "apenso": {
         #        "partition_columns": "dt_inicio",
         #        "lower_bound_date": "2021-01-01",
         "materialize_after_dump": False,
@@ -50,52 +27,102 @@ _sicop_queries = {
         "materialize_to_datario": False,
         "dump_to_gcs": False,
         "dump_mode": "overwrite",
-        "execute_query": QUERY_SICOP_APENSO,
+        "execute_query": """SELECT
+                                CHAVE,
+                                NUM_PROCESSO_PRINCIPAL,
+                                NUM_PROCESSO_APENSADO,
+                                I22005_COD_OPER
+                                FROM SICOP.VW_APENSO""",
     },
-    "sicop_assunto": {
+    "assunto": {
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
         "dump_to_gcs": False,
         "dump_mode": "overwrite",
-        "execute_query": QUERY_SICOP_ASSUNTO,
+        "execute_query": "SELECT IDENT, COD, SEQ, DESC_ASSUNTO FROM SICOP.VW_ASSUNTO",
     },
-    "sicop_documento": {
+    "documento": {
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
         "dump_to_gcs": False,
         "dump_mode": "overwrite",
-        "execute_query": QUERY_SICOP_DOCUMENTO,
+        "execute_query": """SELECT
+                                ORG_RESP,
+                                ORG_TRANSC,
+                                TIPO_DOC,
+                                SEC_SIST2,
+                                ANO_SIST2,
+                                MES_SIST2,
+                                DIA_SIST2,
+                                NUM_DOCUMENTO,
+                                REQUERENTE,
+                                COD_ASSUN,
+                                MAT_TRANSC,
+                                ASSUN_COMP
+                                FROM SICOP.VW_DOCUMENTO""",
     },
-    "sicop_orgao": {
+    "orgao": {
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
         "dump_to_gcs": False,
         "dump_mode": "overwrite",
-        "execute_query": QUERY_SICOP_ORGAO,
+        "execute_query": """SELECT
+                                ORG_SICOP,
+                                COD_ORCTO
+                                FROM SICOP.VW_ORGAO""",
     },
-    "sicop_processo": {
+    "processo": {
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
         "dump_to_gcs": False,
         "dump_mode": "overwrite",
-        "execute_query": QUERY_SICOP_PROCESSO,
+        "execute_query": """SELECT
+                                ORG_TRANSC,
+                                NUM_PROCESSO,
+                                CPF_CGC,
+                                IDENT,
+                                REQUERENTE,
+                                DIA_PROC2,
+                                MES_PROC2,
+                                SEC_PROC2,
+                                ANO_PROC2,
+                                COD_ASSU_P,
+                                ANO_SIST2,
+                                SEC_SIST2,
+                                MES_SIST2,
+                                DIA_SIST2,
+                                DESC_ASSUN,
+                                MAT_TRANSC,
+                                STATUS
+                                FROM SICOP.VW_PROCESSO""",
     },
-    "sicop_tramitacao": {
+    "tramitacao": {
         "materialize_after_dump": False,
         "materialization_mode": "prod",
         "materialize_to_datario": False,
         "dump_to_gcs": False,
         "dump_mode": "overwrite",
-        "execute_query": QUERY_SICOP_TRAMITACAO,
+        "execute_query": """SELECT
+                                ORG_RESP,
+                                TIPO_DOC,
+                                NUM_DOCUMENTO,
+                                SEQ,
+                                SEC_REMES2,
+                                ANO_REMES2,
+                                MES_REMES2,
+                                DIA_REMES2,
+                                DESTINO,
+                                COD_DESP
+                                FROM SICOP.VW_TRAMITACAO_DOCUMENTO""",
     },
 }
 sicop_infra_clocks = generate_dump_db_schedules(
     interval=timedelta(days=1),
-    start_date=datetime(2023, 5, 18, 13, 30, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    start_date=datetime(2023, 5, 19, 2, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
     labels=[
         constants.RJ_IPLANRIO_AGENT_LABEL.value,
     ],
