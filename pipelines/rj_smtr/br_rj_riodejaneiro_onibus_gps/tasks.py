2f6f0014f131567161d622fbcd0f610ccf993d2d
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-08-04 19:37:14 -0300

    fix get_realocacao_recapture_timestamps

--- pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps/tasks.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps/tasks.py
@@ -292,7 +292,7 @@ def get_realocacao_recapture_timestamps(start_date: str, end_date: str) -> List:
             interval_minutes=10,
         )
 
-        errors = errors + [errors_temp] * len(timestamps_temp)
+        errors = errors + ([errors_temp] * len(timestamps_temp))
         timestamps = timestamps + timestamps_temp
         previous_errors = previous_errors + previous_errors_temp
 
