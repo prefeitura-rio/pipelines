fd4fc6859bb3c44310bf470b33bfdb50883e7d2c
thiagotrabach <thiago.trabach@gmail.com>
2023-08-12 09:40:53 -0300

    first working extract and load

--- pipelines/rj_sms/farmacia_estoque/flows.py
+++ pipelines/rj_sms/farmacia_estoque/flows.py
@@ -1,20 +1,44 @@
-from prefect import Flow, Parameter
-from pipelines.utils.utils import(
-    get_vault_secret,
-)
-from tasks import (
-    download_blob,
-)
-    
-with Flow(
-    "SMS: Farmacia - Captura de dados TPC",
-    code_owners=["thiago", "andre"]
-) as captura_tpc:
-    
-    connection_string = get_vault_secret("estoque_tpc")
-    print(connection_string)
-    #container_name = Parameter("container_name", default="tpc")
-    #blob_name = Parameter("blob_name", default="report.csv")
-    #destination_file_path = f"/home/thiagotrabach/projects/poc-pipeline/data/{blob_name}"
+from prefect import Parameter, Flow
+#from pipelines.utils.decorators import Flow
+from pipelines.utils.utils import(get_vault_secret,)
+from pipelines.utils.tasks import(create_table_and_upload_to_gcs,)
+from pipelines.rj_sms.farmacia_estoque.tasks import (download_azure_blob,)
+import os
 
-    #download_blob(connection_string, container_name, blob_name, destination_file_path)
+
+with Flow(name = "SMS: Farmacia - Captura de dados TPC", ) as captura_tpc:
+    
+    # Get crendetials
+    connection_string = get_vault_secret(secret_path = "estoque_tpc")["data"]["connection_string"]
+    
+    # Set Parameters
+    ## Azure
+    container_name = Parameter("container_name", default="tpc")
+    blob_name = Parameter("blob_name", default="report.csv")
+    destination_folder_path = os.path.expanduser('~')
+    ## GCP
+    dataset_id = "estoque"
+    table_id = "tpc_current"
+    dump_mode = "append" # append or overwrite
+
+    # Declare tasks
+    download_task = download_azure_blob(
+        connection_string, 
+        container_name, 
+        blob_name,
+        destination_folder_path
+        )
+    
+    data_path = destination_folder_path + "/report.csv"
+
+    upload_task = create_table_and_upload_to_gcs(
+        data_path = data_path,
+        dataset_id = dataset_id,
+        table_id = table_id,
+        dump_mode = dump_mode, 
+        biglake_table = True,
+        wait = None
+    )
+
+    # Set taks dependencies
+    download_task.set_downstream(upload_task)
--- pipelines/rj_sms/farmacia_estoque/tasks.py
+++ pipelines/rj_sms/farmacia_estoque/tasks.py
@@ -1,21 +1,22 @@
 from prefect import task
-from azure.storage.blob import BlobServiceClient
 from pipelines.utils.utils import log
-
+from azure.storage.blob import BlobServiceClient
 
 
 @task
-def download_blob(connection_string, container_name, blob_name, destination_file_path):
+def download_azure_blob(connection_string, container_name, blob_name, destination_folder_path):
     """
     Download a blob from Azure Blob Storage to a local file.
 
     :param connection_string: Azure Blob Storage connection string
     :param container_name: Name of the container where the blob is located
     :param blob_name: Name of the blob to download
-    :param destination_file_path: Local file path to save the downloaded blob
+    :param destination_folder_path: Local folder path to save the downloaded blob
     """
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    destination_file_path = f"{destination_folder_path}/{blob_name}"
 
     with open(destination_file_path, "wb") as blob_file:
         blob_data = blob_client.download_blob()
@@ -24,7 +25,7 @@ def download_blob(connection_string, container_name, blob_name, destination_file
     log(f"Blob '{blob_name}' downloaded to '{destination_file_path}'.")
 
 @task
-def list_blobs(connection_string, container_name):
+def list_azure_blobs(connection_string, container_name):
     """
     List blobs inside a container in Azure Blob Storage.
 
