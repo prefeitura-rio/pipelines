c49cc0d55b124ddfe01a40eca2eb0f3a0d6531b2
thiagotrabach <thiago.trabach@gmail.com>
2023-08-17 15:21:45 -0300

    wip upload organizations

--- pipelines/rj_sms/farmacia_estoque/flows.py
+++ pipelines/rj_sms/farmacia_estoque/flows.py
@@ -104,3 +104,31 @@ captura_vitai.run_config = KubernetesRun(
         constants.RJ_SMS_DEV_AGENT_LABEL.value,
     ],
 )
+
+with Flow(
+    name="SMS: Farmacia - Upload Organization", code_owners=["thiago", "andre"]
+) as upload_organizations:
+
+    origin = Parameter("origin", default="/home/thiagotrabach/tmp/tbEstabelecimento202307_REV.csv")
+    #  GCP
+    dataset_id = "dump_cnes"
+    table_id = "tbEstabelecimento"
+    dump_mode = "overwrite"  # append or overwrite
+
+    upload_task = create_table_and_upload_to_gcs(
+        data_path= origin,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode=dump_mode,
+        biglake_table=True,
+        wait=None,
+    )
+
+
+upload_organizations.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+upload_organizations.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_SMS_DEV_AGENT_LABEL.value,
+    ],
+)
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from pipelines.rj_sms.farmacia_estoque.flows import captura_tpc, captura_vitai
+from pipelines.rj_sms.farmacia_estoque.flows import captura_tpc, captura_vitai, upload_organizations
 from pipelines.utils.utils import run_local
 import argparse
 
@@ -14,3 +14,5 @@ if args.flow == "captura_tpc":
     run_local(captura_tpc)
 elif args.flow == "captura_vitai":
     run_local(captura_vitai)
+elif args.flow == "upload_organizations":
+    run_local(upload_organizations)
