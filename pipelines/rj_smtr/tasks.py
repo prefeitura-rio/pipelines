ccde64c139bad62f7e063398de76681cc12804b2
hellcassius <caiorogerio.santos@gmail.com>
2023-08-24 19:03:08 -0300

    add log

--- pipelines/rj_smtr/tasks.py
+++ pipelines/rj_smtr/tasks.py
@@ -643,9 +643,7 @@ def get_materialization_date_range(  # pylint: disable=R0913
     )
     # if there's no timestamp set on redis, get max timestamp on source table
     if last_run is None:
-        print(
-            "Failed to fetch key from Redis...\n Querying tables for last suceeded run"
-        )
+        log("Failed to fetch key from Redis...\n Querying tables for last suceeded run")
         if Table(dataset_id=dataset_id, table_id=table_id).table_exists("prod"):
             last_run = get_table_min_max_value(
                 query_project_id=bq_project(),
@@ -654,7 +652,7 @@ def get_materialization_date_range(  # pylint: disable=R0913
                 field_name=table_date_column_name,
                 kind="max",
             )
-            print(
+            log(
                 f"""
             Queried last run from {dataset_id}.{table_id}
             Got:
@@ -669,7 +667,7 @@ def get_materialization_date_range(  # pylint: disable=R0913
                 field_name=table_date_column_name,
                 kind="max",
             )
-        print(
+        log(
             f"""
             Queried last run from {raw_dataset_id}.{raw_table_id}
             Got:
