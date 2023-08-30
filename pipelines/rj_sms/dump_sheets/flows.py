bc487685aa989e2b5cdafa455a6d9bfb66c6a3f2
thiagotrabach <thiago.trabach@gmail.com>
2023-08-30 18:01:05 -0300

    add unidade de saude flow

--- pipelines/rj_sms/dump_sheets/flows.py
+++ pipelines/rj_sms/dump_sheets/flows.py
@@ -30,13 +30,36 @@ medicamentos_flow_parameters = {
     "url": "https://docs.google.com/spreadsheets/d/15dQSia-oL-c5nP-wKQOjlAQl7MVHiDFNv6W0RcYgcSY/edit?usp=sharing",
     "url_type": "google_sheet",
     "gsheets_sheet_name": "dados_mestres",
-    "table_id": "medicamentos2",
+    "table_id": "medicamentos",
     "dataset_id": "dados_mestres",
-    "dump_mode": "append",
+    "dump_mode": "overwrite",
 }
 
 medicamentos_flow = set_default_parameters(
     medicamentos_flow, default_parameters=medicamentos_flow_parameters
 )
 
-# medicamentos_flow.schedule = gsheets_one_minute_update_schedule
+#medicamentos_flow.schedule = gsheets_one_minute_update_schedule
+
+unidade_saude_flow = deepcopy(dump_url_flow)
+unidade_saude_flow.name = "SMS: Dump Unidades de Saude - Ingerir tabela auxiliar"
+unidade_saude_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+unidade_saude_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_SMS_DEV_AGENT_LABEL.value,
+    ],
+)
+
+unidade_saude_flow_parameters = {
+    "url": "https://docs.google.com/spreadsheets/d/1EkYfxuN2bWD_q4OhHL8hJvbmQKmQKFrk0KLf6D7nKS4/edit?usp=sharing",
+    "url_type": "google_sheet",
+    "gsheets_sheet_name": "Sheet1",
+    "table_id": "unidades_saude_aux",
+    "dataset_id": "dados_mestres",
+    "dump_mode": "overwrite",
+}
+
+unidade_saude_flow = set_default_parameters(
+    unidade_saude_flow , default_parameters = unidade_saude_flow_parameters
+)
