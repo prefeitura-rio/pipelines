70f1ac67fc0a2a5dd4c18977f0de18b224075684
thiagotrabach <thiago.trabach@gmail.com>
2023-09-11 14:39:45 -0300

    add vitacare flow to init file

--- pipelines/rj_sms/__init__.py
+++ pipelines/rj_sms/__init__.py
@@ -5,5 +5,6 @@ Prefect flows for rj_sms project
 
 from pipelines.rj_sms.dump_db_sivep.flows import *
 from pipelines.rj_sms.pubsub.flows import *
+from pipelines.rj_sms.dump_api_vitacare.flows import *
 from pipelines.rj_sms.dump_api_vitai.flows import *
 from pipelines.rj_sms.dump_azureblob_tpc.flows import *
