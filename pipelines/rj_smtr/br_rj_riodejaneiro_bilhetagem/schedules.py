7a61d85588cd8495835ad8ad7f69e4bb7b9936a6
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-09-15 12:02:29 -0300

    update schedules

--- pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/schedules.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/schedules.py
@@ -3,29 +3,46 @@
 Schedules for br_rj_riodejaneiro_bilhetagem
 """
 
-from datetime import timedelta, datetime
+from datetime import timedelta
 
 from prefect.schedules import Schedule
-import pytz
 
 from pipelines.constants import constants as emd_constants
 from pipelines.utils.utils import untuple_clocks as untuple
 
 from pipelines.rj_smtr.constants import constants
-from pipelines.rj_smtr.br_rj_riodejaneiro_bilhetagem.utils import (
-    generate_execute_bilhetagem_schedules,
+from pipelines.rj_smtr.utils import (
+    generate_execute_schedules,
 )
 
-bilhetagem_clocks = generate_execute_bilhetagem_schedules(
+from pipelines.rj_smtr.br_rj_riodejaneiro_bilhetagem.utils import (
+    create_bilhetagem_request_params,
+)
+
+bilhetagem_principal_clocks = generate_execute_schedules(
     interval=timedelta(days=1),
-    start_date=datetime(
-        2020, 1, 1, tzinfo=pytz.timezone(emd_constants.DEFAULT_TIMEZONE.value)
-    ),
     labels=[
         emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value,
     ],
     table_parameters=constants.BILHETAGEM_TABLES_PARAMS.value,
+    dataset_id=constants.BILHETAGEM_DATASET_ID.value,
+    secret_path=constants.BILHETAGEM_SECRET_PATH.value,
+    create_request_params_func=create_bilhetagem_request_params,
     runs_interval_minutes=15,
 )
 
-bilhetagem_daily_schedule = Schedule(clocks=untuple(bilhetagem_clocks))
+bilhetagem_principal_schedule = Schedule(clocks=untuple(bilhetagem_principal_clocks))
+
+bilhetagem_transacao_clocks = generate_execute_schedules(
+    interval=timedelta(minutes=1),
+    labels=[
+        emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value,
+    ],
+    table_parameters=constants.BILHETAGEM_TRANSACAO_TABLE_PARAMS.value,
+    dataset_id=constants.BILHETAGEM_DATASET_ID.value,
+    secret_path=constants.BILHETAGEM_SECRET_PATH.value,
+    create_request_params_func=create_bilhetagem_request_params,
+    runs_interval_minutes=0,
+)
+
+bilhetagem_transacao_schedule = Schedule(clocks=untuple(bilhetagem_transacao_clocks))
