4277635b6c4e60201e67b555130a164da1db28ac
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-09-15 11:53:35 -0300

    update flows

--- pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/flows.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/flows.py
@@ -3,172 +3,44 @@
 Flows for br_rj_riodejaneiro_bilhetagem
 """
 
+from copy import deepcopy
+
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
-from prefect import case, Parameter
-from prefect.tasks.control_flow import merge
-
-from copy import deepcopy
 
 # EMD Imports #
 
 from pipelines.constants import constants as emd_constants
-from pipelines.utils.utils import set_default_parameters
-from pipelines.utils.decorators import Flow
-from pipelines.utils.tasks import (
-    rename_current_flow_run_now_time,
-)
 
 # SMTR Imports #
 
-from pipelines.rj_smtr.constants import constants
-
-from pipelines.rj_smtr.tasks import (
-    create_date_partition,
-    create_date_hour_partition,
-    create_local_partition_path,
-    get_current_timestamp,
-    get_raw,
-    parse_timestamp_to_string,
-    save_raw_local,
-    save_treated_local,
-    upload_logs_to_bq,
-    bq_upload,
-    transform_to_nested_structure,
-)
-
-from pipelines.rj_smtr.schedules import every_minute, every_day, every_minute_dev
-
-from pipelines.rj_smtr.br_rj_riodejaneiro_bilhetagem.tasks import (
-    get_bilhetagem_request_params,
-    get_datetime_range,
-)
+from pipelines.rj_smtr.flows import captura_generico
 
 from pipelines.rj_smtr.br_rj_riodejaneiro_bilhetagem.schedules import (
-    bilhetagem_daily_schedule,
+    bilhetagem_principal_schedule,
+    bilhetagem_transacao_schedule,
 )
 
 # Flows #
 
-BILHETAGEM_AUXILIAR_FLOW_NAME = "SMTR: Bilhetagem Auxiliar (captura)"
-
-with Flow(
-    BILHETAGEM_AUXILIAR_FLOW_NAME,
-    code_owners=["caio", "fernanda", "boris", "rodrigo"],
-) as bilhetagem_auxiliar_captura:
-    # SETUP #
-
-    tables_params = Parameter("tables_params", default=None)
-    timestamp_param = Parameter("timestamp", default=None)
-    interval_minutes_param = Parameter("interval_minutes", default=1440)
-
-    timestamp = get_current_timestamp(timestamp_param)
-
-    datetime_range = get_datetime_range(
-        timestamp, interval_minutes=interval_minutes_param
-    )
-
-    DATASET_ID = constants.BILHETAGEM_DATASET_ID.value
-
-    rename_flow_run = rename_current_flow_run_now_time(
-        prefix=bilhetagem_auxiliar_captura.name
-        + " "
-        + tables_params["table_id"]
-        + ": ",
-        now_time=timestamp,
-    )
-
-    request_params, url = get_bilhetagem_request_params(
-        datetime_range=datetime_range,
-        database=tables_params["database"],
-        table_name=tables_params["table_name"],
-        table_column=tables_params["table_column"],
-        method=tables_params["method"],
-    )
-
-    with case(tables_params["flag_date_partition"], True):
-        date_partitions = create_date_partition(timestamp)
-
-    with case(tables_params["flag_date_partition"], False):
-        date_hour_partitions = create_date_hour_partition(timestamp)
-
-    partitions = merge(date_partitions, date_hour_partitions)
-
-    filename = parse_timestamp_to_string(timestamp)
-
-    filepath = create_local_partition_path(
-        dataset_id=DATASET_ID,
-        table_id=tables_params["table_id"],
-        filename=filename,
-        partitions=partitions,
-    )
-
-    raw_status = get_raw(
-        url=url,
-        headers=constants.BILHETAGEM_SECRET_PATH.value,
-        params=request_params,
-    )
-
-    raw_filepath = save_raw_local(status=raw_status, file_path=filepath)
-
-    # TREAT & CLEAN #
-    treated_status = transform_to_nested_structure(
-        status=raw_status,
-        timestamp=timestamp,
-        primary_key=tables_params["primary_key"],
-    )
-
-    treated_filepath = save_treated_local(status=treated_status, file_path=filepath)
-
-    # LOAD #
-    error = bq_upload(
-        dataset_id=DATASET_ID,
-        table_id=tables_params["table_id"],
-        filepath=treated_filepath,
-        raw_filepath=raw_filepath,
-        partitions=partitions,
-        status=treated_status,
-    )
-
-    upload_logs_to_bq(
-        dataset_id=DATASET_ID,
-        parent_table_id=tables_params["table_id"],
-        error=error,
-        timestamp=timestamp,
-    )
-
-bilhetagem_auxiliar_captura.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
-bilhetagem_auxiliar_captura.run_config = KubernetesRun(
-    image=emd_constants.DOCKER_IMAGE.value,
-    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
-)
-
 # BILHETAGEM TRANSAÇÃO - CAPTURA A CADA MINUTO #
 
-bilhetagem_transacao_captura = deepcopy(bilhetagem_auxiliar_captura)
+bilhetagem_transacao_captura = deepcopy(captura_generico)
 bilhetagem_transacao_captura.name = "SMTR: Bilhetagem Transação (captura)"
 bilhetagem_transacao_captura.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 bilhetagem_transacao_captura.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
     labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
 )
-bilhetagem_transacao_captura_parameters = {
-    "tables_params": constants.BILHETAGEM_TRANSACAO_TABLE_PARAMS.value,
-    "interval_minutes": 1,
-}
-bilhetagem_transacao_captura = set_default_parameters(
-    bilhetagem_transacao_captura,
-    default_parameters=bilhetagem_transacao_captura_parameters,
-)
-bilhetagem_transacao_captura.schedule = every_minute_dev
+bilhetagem_transacao_captura.schedule = bilhetagem_transacao_schedule
 
 # BILHETAGEM PRINCIPAL - CAPTURA DIÁRIA DE DIVERSAS TABELAS #
 
-bilhetagem_principal_captura = deepcopy(bilhetagem_auxiliar_captura)
+bilhetagem_principal_captura = deepcopy(captura_generico)
 bilhetagem_principal_captura.name = "SMTR: Bilhetagem Principal (captura)"
 bilhetagem_principal_captura.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 bilhetagem_principal_captura.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
     labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
 )
-bilhetagem_principal_captura.schedule = bilhetagem_daily_schedule
+bilhetagem_principal_captura.schedule = bilhetagem_principal_schedule
--- pipelines/rj_smtr/flows.py
+++ pipelines/rj_smtr/flows.py
@@ -3,70 +3,124 @@
 Flows for rj_smtr
 """
 
-###############################################################################
-#
-# Aqui é onde devem ser definidos os flows do projeto.
-# Cada flow representa uma sequência de passos que serão executados
-# em ordem.
-#
-# Mais informações sobre flows podem ser encontradas na documentação do
-# Prefect: https://docs.prefect.io/core/concepts/flows.html
-#
-# De modo a manter consistência na codebase, todo o código escrito passará
-# pelo pylint. Todos os warnings e erros devem ser corrigidos.
-#
-# Existem diversas maneiras de declarar flows. No entanto, a maneira mais
-# conveniente e recomendada pela documentação é usar a API funcional.
-# Em essência, isso implica simplesmente na chamada de funções, passando
-# os parâmetros necessários para a execução em cada uma delas.
-#
-# Também, após a definição de um flow, para o adequado funcionamento, é
-# mandatório configurar alguns parâmetros dele, os quais são:
-# - storage: onde esse flow está armazenado. No caso, o storage é o
-#   próprio módulo Python que contém o flow. Sendo assim, deve-se
-#   configurar o storage como o pipelines.rj_smtr
-# - run_config: para o caso de execução em cluster Kubernetes, que é
-#   provavelmente o caso, é necessário configurar o run_config com a
-#   imagem Docker que será usada para executar o flow. Assim sendo,
-#   basta usar constants.DOCKER_IMAGE.value, que é automaticamente
-#   gerado.
-# - schedule (opcional): para o caso de execução em intervalos regulares,
-#   deve-se utilizar algum dos schedules definidos em schedules.py
-#
-# Um exemplo de flow, considerando todos os pontos acima, é o seguinte:
-#
-# -----------------------------------------------------------------------------
-# from prefect import task
-# from prefect import Flow
-# from prefect.run_configs import KubernetesRun
-# from prefect.storage import GCS
-# from pipelines.constants import constants
-# from my_tasks import my_task, another_task
-# from my_schedules import some_schedule
-#
-# with Flow("my_flow") as flow:
-#     a = my_task(param1=1, param2=2)
-#     b = another_task(a, param3=3)
-#
-# flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
-# flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
-# flow.schedule = some_schedule
-# -----------------------------------------------------------------------------
-#
-# Abaixo segue um código para exemplificação, que pode ser removido.
-#
-###############################################################################
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from prefect import case, Parameter
+from prefect.tasks.control_flow import merge
 
+# EMD Imports #
 
-# from prefect import Flow
-# from prefect.run_configs import KubernetesRun
-# from prefect.storage import GCS
-# from pipelines.constants import constants
-# # from pipelines.rj_smtr.schedules import every_two_weeks
+from pipelines.constants import constants as emd_constants
+from pipelines.utils.decorators import Flow
+from pipelines.utils.tasks import (
+    rename_current_flow_run_now_time,
+)
 
-# with Flow("my_flow") as flow:
-#     say_hello()
+# SMTR Imports #
 
-# flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
-# flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
-# flow.schedule = every_two_weeks
+from pipelines.rj_smtr.tasks import (
+    create_date_partition,
+    create_date_hour_partition,
+    create_local_partition_path,
+    get_current_timestamp,
+    get_raw,
+    parse_timestamp_to_string,
+    save_raw_local,
+    save_treated_local,
+    upload_logs_to_bq,
+    bq_upload,
+    transform_to_nested_structure,
+)
+
+from pipelines.rj_smtr.tasks import (
+    create_request_params,
+    get_datetime_range,
+)
+
+CAPTURA_GENERICO_FLOW_NAME = "SMTR: Captura Genérico"
+
+with Flow(
+    CAPTURA_GENERICO_FLOW_NAME,
+    code_owners=["caio", "fernanda", "boris", "rodrigo"],
+) as captura_generico:
+    # SETUP #
+
+    table_params = Parameter("table_params", default=None)
+    timestamp_param = Parameter("timestamp", default=None)
+    interval_param = Parameter("interval", default=None)
+    create_request_params_func = Parameter("create_request_params_func", default=None)
+    dataset_id = Parameter("dataset_id", default=None)
+    secret_path = Parameter("secret_path", default=None)
+
+    timestamp = get_current_timestamp(timestamp_param)
+
+    datetime_range = get_datetime_range(timestamp, interval=interval_param)
+
+    rename_flow_run = rename_current_flow_run_now_time(
+        prefix=captura_generico.name + " " + table_params["table_id"] + ": ",
+        now_time=timestamp,
+    )
+
+    request_params, url = create_request_params(
+        datetime_range=datetime_range,
+        table_params=table_params,
+        secret_path=secret_path,
+        create_request_params_func=create_request_params_func,
+    )
+
+    with case(table_params["flag_date_partition"], True):
+        date_partitions = create_date_partition(timestamp)
+
+    with case(table_params["flag_date_partition"], False):
+        date_hour_partitions = create_date_hour_partition(timestamp)
+
+    partitions = merge(date_partitions, date_hour_partitions)
+
+    filename = parse_timestamp_to_string(timestamp)
+
+    filepath = create_local_partition_path(
+        dataset_id=dataset_id,
+        table_id=table_params["table_id"],
+        filename=filename,
+        partitions=partitions,
+    )
+
+    raw_status = get_raw(
+        url=url,
+        headers=secret_path,
+        params=request_params,
+    )
+
+    raw_filepath = save_raw_local(status=raw_status, file_path=filepath)
+
+    # TREAT & CLEAN #
+    treated_status = transform_to_nested_structure(
+        status=raw_status,
+        timestamp=timestamp,
+        primary_key=table_params["primary_key"],
+    )
+
+    treated_filepath = save_treated_local(status=treated_status, file_path=filepath)
+
+    # LOAD #
+    error = bq_upload(
+        dataset_id=dataset_id,
+        table_id=table_params["table_id"],
+        filepath=treated_filepath,
+        raw_filepath=raw_filepath,
+        partitions=partitions,
+        status=treated_status,
+    )
+
+    upload_logs_to_bq(
+        dataset_id=dataset_id,
+        parent_table_id=table_params["table_id"],
+        error=error,
+        timestamp=timestamp,
+    )
+
+captura_generico.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
+captura_generico.run_config = KubernetesRun(
+    image=emd_constants.DOCKER_IMAGE.value,
+    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
+)
