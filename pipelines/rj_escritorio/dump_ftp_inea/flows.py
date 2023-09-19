a889247b284641278cfd43b41245ac247c4e8a9e
patriciacatandi <patriciabcatandi@gmail.com>
2023-09-19 19:44:02 -0300

    starting inea flow using ftp

---
+++ pipelines/rj_escritorio/dump_ftp_inea/flows.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Dumping  data from INEA FTP to BigQuery
+"""
+# pylint: disable=E1101,C0103
+
+from copy import deepcopy
+
+from prefect import Parameter
+from prefect.run_configs import LocalRun
+from prefect.storage import GCS
+from prefect.utilities.edges import unmapped
+
+from pipelines.constants import constants
+from pipelines.rj_escritorio.dump_ftp_inea.tasks import (
+    get_ftp_client,
+    get_files_to_download,
+    download_files,
+    upload_file_to_gcs,
+)
+from pipelines.rj_escritorio.dump_ftp_inea.schedules import (
+    every_5_minutes,
+    every_5_minutes_mac,
+)
+from pipelines.utils.decorators import Flow
+
+
+with Flow(
+    "INEA: Captura dados de radar (Guaratiba)",
+    code_owners=[
+        "paty",
+    ],
+) as inea_ftp_radar_flow:
+    bucket_name = Parameter("bucket_name", default="rj-escritorio-dev", required=False)
+    prefix = Parameter(
+        "prefix", default="raw/meio_ambiente_clima/inea_radar_hdf5", required=False
+    )
+    mode = Parameter("mode", default="prod", required=False)
+    radar = Parameter("radar", default="mac", required=False)
+    product = Parameter("product", default="ppi", required=False)
+
+    client = get_ftp_client()
+
+    files_to_download = get_files_to_download(
+        client=client,
+        radar=radar,
+    )
+
+    files_to_upload = download_files(
+        client=client,
+        files=files_to_download,
+        radar=radar,
+    )
+
+    upload_file_to_gcs.map(
+        file_to_upload=files_to_upload,
+        bucket_name=unmapped(bucket_name),
+        prefix=unmapped(prefix),
+        mode=unmapped(mode),
+        radar=unmapped(radar),
+        product=unmapped(product),
+    )
+
+
+inea_ftp_radar_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+inea_ftp_radar_flow.run_config = LocalRun(labels=[constants.INEA_AGENT_LABEL.value])
+inea_ftp_radar_flow.schedule = every_5_minutes
+
+inea_ftp_radar_flow_mac = deepcopy(inea_ftp_radar_flow)
+inea_ftp_radar_flow_mac.name = "INEA: Captura dados de radar (Macaé)"
+inea_ftp_radar_flow_mac.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+inea_ftp_radar_flow_mac.run_config = LocalRun(labels=[constants.INEA_AGENT_LABEL.value])
+inea_ftp_radar_flow_mac.schedule = every_5_minutes_mac
---
+++ pipelines/rj_escritorio/dump_ftp_inea/tasks.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Tasks to dump data from a INEA FTP to BigQuery
+"""
+# pylint: disable=E0702,E1137,E1136,E1101,C0207,W0613
+from datetime import datetime, timedelta
+from pathlib import Path
+
+from google.cloud import storage
+from prefect import task
+
+from pipelines.utils.ftp.client import FTPClient
+from pipelines.utils.utils import (
+    log,
+    get_credentials_from_env,
+    get_vault_secret,
+)
+
+
+@task
+def get_ftp_client(wait=None):
+    """
+    Get FTP client
+    """
+    inea_secret = get_vault_secret("ftp_inea_radar")
+    hostname = inea_secret["data"]["hostname"]
+    username = inea_secret["data"]["username"]
+    password = inea_secret["data"]["password"]
+
+    return FTPClient(
+        hostname=hostname,
+        username=username,
+        password=password,
+    )
+
+
+@task(
+    max_retries=3,
+    retry_delay=timedelta(seconds=30),
+)
+def get_files_to_download(client, radar):
+    """
+    Get files to download FTP and GCS
+    """
+
+    client.connect()
+    files = client.list_files(path=f"./{radar.upper()}/")
+    files = files[-4:]
+    log(f"files: {files}")
+
+    return files
+
+
+@task(
+    max_retries=3,
+    retry_delay=timedelta(seconds=30),
+)
+def download_files(client, files, radar):
+    """
+    Download files from FTP
+    """
+
+    save_path = Path(radar.upper())
+    save_path.mkdir(parents=True, exist_ok=True)
+
+    client.connect()
+    files_downloaded = []
+    for file in files:
+        # file_path = save_path / file
+        file_path = file
+        client.download(remote_path=file, local_path=file_path)
+        files_downloaded.append(file_path)
+    log(f"files_downloaded: {files_downloaded}")
+    return files_downloaded
+
+
+@task(
+    max_retries=3,
+    retry_delay=timedelta(seconds=30),
+)
+# pylint: disable=too-many-arguments, too-many-locals
+def upload_file_to_gcs(
+    file_to_upload: str,
+    bucket_name: str,
+    prefix: str,
+    radar: str,
+    product: str,
+    mode="prod",
+    task_mode="partitioned",
+    unlink: bool = True,
+):
+    """
+    Upload files to GCS
+    """
+    credentials = get_credentials_from_env(mode=mode)
+    storage_client = storage.Client(credentials=credentials)
+
+    bucket = storage_client.bucket(bucket_name)
+
+    file = Path(file_to_upload)
+    if file.is_file():
+        if task_mode == "partitioned":
+            # Converted file path is in the format:
+            # /var/opt/edge/.../YYYYMMDD/<filename>.nc.gz
+            # We need to get the datetime for the file
+            date_str = file.parent.name
+            date = datetime.strptime(date_str, "%Y%m%d").strftime("%Y-%m-%d")
+            blob_name = f"{prefix}/radar={radar}/produto={product}/data_particao={date}/{file.name}"
+            blob_name = blob_name.replace("//", "/")
+        elif task_mode == "raw":
+            blob_name = f"{prefix}/{file.name}"
+        else:
+            raise ValueError(f"Invalid task_mode: {task_mode}")
+        log(f"Uploading file {file} to GCS...")
+        log(f"Blob name will be {blob_name}")
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(file)
+        log(f"File {file} uploaded to GCS.")
+        if unlink:
+            file.unlink()
