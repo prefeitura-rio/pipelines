b900910c2162368dac174fe03d8d82508fccce0d
Rafael <rafael.c.pinheiro96@gmail.com>
2023-09-20 15:27:44 -0300

    adapt bilhetagem schedules for the new model

--- pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/schedules.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/schedules.py
@@ -15,28 +15,33 @@ from pipelines.rj_smtr.utils import (
     generate_execute_schedules,
 )
 
+BILHETAGEM_PRINCIPAL_INTERVAL = timedelta(days=1)
 bilhetagem_principal_clocks = generate_execute_schedules(
-    interval=timedelta(days=1),
+    clock_interval=BILHETAGEM_PRINCIPAL_INTERVAL,
     labels=[
         emd_constants.RJ_SMTR_AGENT_LABEL.value,
     ],
+    runs_interval_minutes=15,
     table_parameters=constants.BILHETAGEM_TABLES_PARAMS.value,
+    interval=BILHETAGEM_PRINCIPAL_INTERVAL.total_seconds(),
     dataset_id=constants.BILHETAGEM_DATASET_ID.value,
     secret_path=constants.BILHETAGEM_SECRET_PATH.value,
-    runs_interval_minutes=15,
 )
 
 bilhetagem_principal_schedule = Schedule(clocks=untuple(bilhetagem_principal_clocks))
 
+BILHETAGEM_TRANSACAO_INTERVAL = timedelta(minutes=1)
 bilhetagem_transacao_clocks = generate_execute_schedules(
-    interval=timedelta(minutes=1),
+    clock_interval=BILHETAGEM_TRANSACAO_INTERVAL,
     labels=[
         emd_constants.RJ_SMTR_AGENT_LABEL.value,
     ],
+    runs_interval_minutes=0,
     table_parameters=constants.BILHETAGEM_TRANSACAO_TABLE_PARAMS.value,
+    interval=BILHETAGEM_TRANSACAO_INTERVAL.total_seconds(),
     dataset_id=constants.BILHETAGEM_DATASET_ID.value,
     secret_path=constants.BILHETAGEM_SECRET_PATH.value,
-    runs_interval_minutes=0,
 )
 
 bilhetagem_transacao_schedule = Schedule(clocks=untuple(bilhetagem_transacao_clocks))
+
