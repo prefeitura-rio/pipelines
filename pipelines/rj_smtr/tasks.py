b6089fa2bf7fd75fd5abb1fb8affe74eb63f3ff8
fernandascovino <fscovinom@gmail.com>
2023-09-25 19:09:32 -0300

    remove task de particao nao usada

--- pipelines/rj_smtr/tasks.py
+++ pipelines/rj_smtr/tasks.py
@@ -182,34 +182,6 @@ def parse_timestamp_to_string(timestamp: datetime, pattern="%Y-%m-%d-%H-%M-%S") 
 
 
 @task
-def create_current_date_hour_partition(capture_time=None):
-    """Create partitioned directory structure to save data locally based
-    on capture time.
-
-    Args:
-        capture_time(pendulum.datetime.DateTime, optional):
-            if recapturing data, will create partitions based
-            on the failed timestamps being recaptured
-
-    Returns:
-        dict: "filename" contains the name which to upload the csv, "partitions" contains
-        the partitioned directory path
-    """
-    if capture_time is None:
-        capture_time = datetime.now(tz=constants.TIMEZONE.value).replace(
-            minute=0, second=0, microsecond=0
-        )
-    date = capture_time.strftime("%Y-%m-%d")
-    hour = capture_time.strftime("%H")
-
-    return {
-        "filename": capture_time.strftime("%Y-%m-%d-%H-%M-%S"),
-        "partitions": f"data={date}/hora={hour}",
-        "timestamp": capture_time,
-    }
-
-
-@task
 def create_local_partition_path(
     dataset_id: str, table_id: str, filename: str, partitions: str = None
 ) -> str:
