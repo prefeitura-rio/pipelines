745103c76cbf07a7dc5f94b24e359aa6cbd83556
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-09-25 23:53:54 -0300

    Revert "update rdo agents for testing"
    This reverts commit 6cb36cbe8efc536c105ab715b10275ee650d81cb.

--- pipelines/rj_smtr/br_rj_riodejaneiro_rdo/flows.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_rdo/flows.py
@@ -93,7 +93,7 @@ with Flow("SMTR: SPPO RHO - Materialização") as sppo_rho_materialize:
 sppo_rho_materialize.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 sppo_rho_materialize.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
-    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
+    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
 )
 
 with Flow(
@@ -140,7 +140,7 @@ with Flow(
 captura_sppo_rho.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 captura_sppo_rho.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
-    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
+    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
 )
 captura_sppo_rho.schedule = every_day
 
@@ -188,7 +188,7 @@ with Flow(
 captura_sppo_rdo.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 captura_sppo_rdo.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
-    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
+    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
 )
 captura_sppo_rdo.schedule = every_day
 
