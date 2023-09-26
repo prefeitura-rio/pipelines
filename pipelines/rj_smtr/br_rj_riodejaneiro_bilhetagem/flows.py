caa36874b8096de2f6ca776059844c3ae74f947d
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-09-25 23:00:40 -0300

    Revert "update agent"
    This reverts commit 741a2a6e75d6fecfaea89ee714c8847b48b9a933.

--- pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/flows.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/flows.py
@@ -30,7 +30,7 @@ bilhetagem_transacao_captura.name = "SMTR: Bilhetagem Transação (captura)"
 bilhetagem_transacao_captura.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 bilhetagem_transacao_captura.run_config = KubernetesRun(
     image=emd_constants.DOCKER_IMAGE.value,
-    labels=[emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
+    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
 )
 bilhetagem_transacao_captura.schedule = bilhetagem_transacao_schedule
 
