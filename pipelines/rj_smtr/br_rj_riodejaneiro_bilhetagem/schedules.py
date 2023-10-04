27d0e8f7a9a51a6c3afc587db350f82f5e457fda
Rafael <rafael.c.pinheiro96@gmail.com>
2023-10-04 18:15:58 -0300

    remove schedules nao utilizados

--- pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/schedules.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_bilhetagem/schedules.py
@@ -15,29 +15,10 @@ from pipelines.rj_smtr.utils import (
     generate_execute_schedules,
 )
 
-BILHETAGEM_PRINCIPAL_INTERVAL = timedelta(days=1)
-bilhetagem_principal_clocks = generate_execute_schedules(
-    clock_interval=timedelta(
-        **constants.BILHETAGEM_GENERAL_CAPTURE_PARAMS.value["principal_run_interval"]
-    ),
-    labels=[
-        emd_constants.RJ_SMTR_AGENT_LABEL.value,
-    ],
-    table_parameters=constants.BILHETAGEM_CAPTURE_PARAMS.value,
-    dataset_id=constants.BILHETAGEM_DATASET_ID.value,
-    secret_path=constants.BILHETAGEM_SECRET_PATH.value,
-    source_type=constants.BILHETAGEM_GENERAL_CAPTURE_PARAMS.value["source_type"],
-    runs_interval_minutes=constants.BILHETAGEM_GENERAL_CAPTURE_PARAMS.value[
-        "principal_runs_interval_minutes"
-    ],
-)
-
-bilhetagem_principal_schedule = Schedule(clocks=untuple(bilhetagem_principal_clocks))
-
 BILHETAGEM_TRANSACAO_INTERVAL = timedelta(minutes=1)
 bilhetagem_transacao_clocks = generate_execute_schedules(
     clock_interval=timedelta(
-        **constants.BILHETAGEM_GENERAL_CAPTURE_PARAMS.value["transacao_run_interval"]
+        **constants.BILHETAGEM_CAPTURE_RUN_INTERVAL.value["transacao_run_interval"]
     ),
     labels=[
         emd_constants.RJ_SMTR_AGENT_LABEL.value,
@@ -52,17 +33,3 @@ bilhetagem_transacao_clocks = generate_execute_schedules(
 )
 
 bilhetagem_transacao_schedule = Schedule(clocks=untuple(bilhetagem_transacao_clocks))
-
-# bilhetagem_materializacao_clocks = generate_execute_schedules(
-#     clock_interval=timedelta(hours=1),
-#     labels=[
-#         emd_constants.RJ_SMTR_DEV_AGENT_LABEL.value,
-#     ],
-#     runs_interval_minutes=0,
-#     table_parameters=constants.BILHETAGEM_MATERIALIZACAO_PARAMS.value,
-#     dataset_id=constants.BILHETAGEM_DATASET_ID.value,
-# )
-
-# bilhetagem_materializacao_schedule = Schedule(
-#     clocks=untuple(bilhetagem_materializacao_clocks)
-# )
