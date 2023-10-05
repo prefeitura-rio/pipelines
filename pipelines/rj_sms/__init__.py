fb923f2a10e904117cf94eaf8604530e8ebc9f6b
thiagotrabach <thiago.trabach@gmail.com>
2023-10-05 09:29:54 -0300

    remove pubsub flow

--- pipelines/rj_sms/__init__.py
+++ pipelines/rj_sms/__init__.py
@@ -4,6 +4,5 @@ Prefect flows for rj_sms project
 """
 
 from pipelines.rj_sms.dump_db_sivep.flows import *
-from pipelines.rj_sms.pubsub.flows import *
 from pipelines.rj_sms.dump_api_vitacare.flows import *
 from pipelines.rj_sms.dump_api_vitai.flows import *
