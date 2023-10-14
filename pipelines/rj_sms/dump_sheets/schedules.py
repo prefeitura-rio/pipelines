1551a684c12697fc69b6c5e90bc50732e32bc7fb
thiagotrabach <thiago.trabach@gmail.com>
2023-10-14 10:52:10 -0300

    add sheets dump

---
+++ pipelines/rj_sms/dump_sheets/schedules.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=C0103
+"""
+Schedules for the sheets dump pipeline
+"""
+
+from datetime import timedelta
+import pendulum
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
+from pipelines.constants import constants
+
+every_sunday_at_six_am = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=7),
+            start_date=pendulum.datetime(2023, 10, 8, 6, 0, 0, tz="America/Sao_Paulo"),
+            labels=[
+                constants.RJ_SMS_DEV_AGENT_LABEL.value,
+            ],
+        )
+    ]
+)
