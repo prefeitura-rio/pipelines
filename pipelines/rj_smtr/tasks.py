b172a63ff9638352dbc8c01765d3591b933855fd
Rafael <rafael.c.pinheiro96@gmail.com>
2023-10-18 11:24:44 -0300

    remover log de teste

--- pipelines/rj_smtr/tasks.py
+++ pipelines/rj_smtr/tasks.py
@@ -836,8 +836,6 @@ def upload_staging_data_to_gcs(
             error = traceback.format_exc()
             log(f"[CATCHED] Task failed with error: \n{error}", level="error")
 
-    log(f"previous_error = {previous_error}")
-
     upload_run_logs_to_bq(
         dataset_id=f"{dataset_id}_dev",
         parent_table_id=table_id,
