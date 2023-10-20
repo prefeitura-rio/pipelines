0cb1bf02ddb926d399d91d5af215143eb4c65db7
eng-rodrigocunha <engtransportes.rodrigocunha@gmail.com>
2023-10-20 18:05:06 -0300

    cria parâmetros para captura e/ou materialização

--- pipelines/rj_smtr/br_rj_riodejaneiro_gtfs/flows.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_gtfs/flows.py
@@ -10,7 +10,7 @@ from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 from prefect.utilities.edges import unmapped
-from prefect import Parameter
+from prefect import Parameter, case
 
 
 # EMD Imports #
@@ -61,6 +61,8 @@ with Flow(
     # SETUP
 
     data_versao_gtfs = Parameter("data_versao_gtfs", default=None)
+    capture = Parameter("capture", default=True)
+    materialize = Parameter("materialize", default=True)
 
     timestamp = get_current_timestamp()
 
@@ -71,44 +73,46 @@ with Flow(
 
     LABELS = get_current_flow_labels()
 
-    gtfs_capture_parameters = [
-        {"timestamp": data_versao_gtfs, **d}
-        for d in constants.GTFS_TABLE_CAPTURE_PARAMS.value
-    ]
+    with case(capture, True):
+        gtfs_capture_parameters = [
+            {"timestamp": data_versao_gtfs, **d}
+            for d in constants.GTFS_TABLE_CAPTURE_PARAMS.value
+        ]
 
-    run_captura = create_flow_run.map(
-        flow_name=unmapped(gtfs_captura.name),
-        project_name=unmapped("staging"),
-        parameters=gtfs_capture_parameters,
-        labels=unmapped(LABELS),
-    )
+        run_captura = create_flow_run.map(
+            flow_name=unmapped(gtfs_captura.name),
+            project_name=unmapped("staging"),
+            parameters=gtfs_capture_parameters,
+            labels=unmapped(LABELS),
+        )
 
-    wait_captura = wait_for_flow_run.map(
-        run_captura,
-        stream_states=unmapped(True),
-        stream_logs=unmapped(True),
-        raise_final_state=unmapped(True),
-    )
+        wait_captura = wait_for_flow_run.map(
+            run_captura,
+            stream_states=unmapped(True),
+            stream_logs=unmapped(True),
+            raise_final_state=unmapped(True),
+        )
 
-    gtfs_materializacao_parameters = {
-        "dataset_id": constants.GTFS_DATASET_ID.value,
-        "dbt_vars": {"data_versao_gtfs": data_versao_gtfs},
-    }
+    with case(materialize, True):
+        gtfs_materializacao_parameters = {
+            "dataset_id": constants.GTFS_DATASET_ID.value,
+            "dbt_vars": {"data_versao_gtfs": data_versao_gtfs},
+        }
 
-    run_materializacao = create_flow_run(
-        flow_name=unmapped(gtfs_materializacao.name),
-        project_name=unmapped("staging"),
-        parameters=unmapped(gtfs_materializacao_parameters),
-        labels=unmapped(LABELS),
-        upstream_tasks=[wait_captura],
-    )
+        run_materializacao = create_flow_run(
+            flow_name=unmapped(gtfs_materializacao.name),
+            project_name=unmapped("staging"),
+            parameters=unmapped(gtfs_materializacao_parameters),
+            labels=unmapped(LABELS),
+            upstream_tasks=[wait_captura],
+        )
 
-    wait_materializacao = wait_for_flow_run(
-        run_materializacao,
-        stream_states=unmapped(True),
-        stream_logs=unmapped(True),
-        raise_final_state=unmapped(True),
-    )
+        wait_materializacao = wait_for_flow_run(
+            run_materializacao,
+            stream_states=unmapped(True),
+            stream_logs=unmapped(True),
+            raise_final_state=unmapped(True),
+        )
 
 gtfs_captura_tratamento.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 gtfs_captura_tratamento.run_config = KubernetesRun(
