4679df39b8029746853c27ab2aad56c40f28bda0
Karina Passos <kaadrain@gmail.com>
2023-10-31 13:05:06 +0000

    testando flow

--- pipelines/rj_cor/meteorologia/meteorologia_redemet/flows.py
+++ pipelines/rj_cor/meteorologia/meteorologia_redemet/flows.py
@@ -173,7 +173,7 @@ with Flow(
     # data = slice_data(current_time=CURRENT_TIME)
     dados = tratar_dados_estacao(data_inicio_, data_fim_)
     print("hi")
-    print(dados.columns)
+    # print(dados.columns)
     # PATH = salvar_dados(dados=dados)
 
     # Create table in BigQuery
