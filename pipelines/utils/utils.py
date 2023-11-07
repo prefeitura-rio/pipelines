fcaae4448e3993962a905227eb5bfdf3b05ac69e
d116626 <d116626@gmail.com>
2023-11-07 12:55:34 -0300

    feat: add paramenter log_number_of_batches in the flow

--- pipelines/utils/utils.py
+++ pipelines/utils/utils.py
@@ -65,7 +65,7 @@ def log_mod(msg: Any, level: str = "info", index: int = 0, mod: int = 1):
     Only logs a message if the index is a multiple of mod.
     """
     if index % mod == 0 or index == 0:
-        log(msg=msg, level=level)
+        log(msg=f"iteration {index}:\n {msg}", level=level)
 
 
 ###############
