e490eed781c2af39f6d8390f6a5b7c4796a19740
ThiagoTrabach <thiago.trabach@gmail.com>
2023-11-22 18:22:28 -0300

    initial commit

---
+++ pipelines/rj_sms/dump_sheets/flows.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Database dumping flows for sheets dump.
+"""
+
+from copy import deepcopy
+
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+
+from pipelines.constants import constants
+from pipelines.utils.dump_url.flows import dump_url_flow
+from pipelines.utils.utils import set_default_parameters
+from pipelines.rj_sms.dump_sheets.schedules import sms_sheets_daily_update_schedule
+
+
+dump_sms_sheets_flow = deepcopy(dump_url_flow)
+dump_sms_sheets_flow.name = "SMS: Dump Google Sheets - Ingerir planilhas do Google Sheets"
+dump_sms_sheets_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+
+dump_sms_sheets_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_SMS_DEV_AGENT_LABEL.value,
+    ],
+)
+
+
+#dump_sms_sheets_flow = set_default_parameters(
+#    dump_sms_sheets_flow)
+dump_sms_sheets_flow.schedule = sms_sheets_daily_update_schedule
