e7a23bba606468b2cf82b4c3cff06c165b2e6a8b
Carolina Gomes <gsv.lina@gmail.com>
2023-11-22 15:19:36 -0300

    wip

--- pipelines/rj_smtr/constants.py
+++ pipelines/rj_smtr/constants.py
@@ -540,7 +540,7 @@ class constants(Enum):  # pylint: disable=c0103
         "extract_params": {
             "token": "",
             "$select": "id,protocol,createdDate",
-            "$filter": "{dates} and lifetimeWorkingTime eq {minutes} \
+            "$filter": "{dates} and lifetimeWorkingTime {minutes} \
                 and serviceFull/any(serviceFull: {service})",
             "$expand": "customFieldValues,customFieldValues($expand=items)",
             "$orderby": "createdDate asc",
