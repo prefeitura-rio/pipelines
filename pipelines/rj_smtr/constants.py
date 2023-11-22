c26ae6479ccc7a0fb2ccb793c1272fa5c0d4565f
Carolina Gomes <gsv.lina@gmail.com>
2023-11-22 16:39:50 -0300

    wip

--- pipelines/rj_smtr/constants.py
+++ pipelines/rj_smtr/constants.py
@@ -539,9 +539,8 @@ class constants(Enum):  # pylint: disable=c0103
         "dataset_id": SUBSIDIO_SPPO_RECURSOS_DATASET_ID,
         "extract_params": {
             "token": "",
-            "$select": "id,protocol,createdDate",
-            "$filter": "{dates} and serviceFull/any(serviceFull: {service}) \
-                and lifetimeWorkingTime eq {minutes}",
+            "$select": "id,protocol,createdDate,lifetimeWorkingTime",
+            "$filter": "{dates}{minutes} and serviceFull/any(serviceFull: {service})",
             "$expand": "customFieldValues,customFieldValues($expand=items)",
             "$orderby": "createdDate asc",
         },
