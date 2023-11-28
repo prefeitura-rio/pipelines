a0d76321f0135634dbc3b50f90d4395d80c98545
Carolina Gomes <gsv.lina@gmail.com>
2023-11-28 17:37:36 -0300

    retira id do igor das constants

--- pipelines/constants.py
+++ pipelines/constants.py
@@ -146,10 +146,6 @@ class constants(Enum):  # pylint: disable=c0103
             "user_id": "620000269392019469",
             "type": "user_nickname",
         },
-        "igorlaltuf": {
-            "user_id": "87545892319531008",
-            "type": "user_nickname",
-        },
         "karinappassos": {
             "user_id": "222842688117014528",
             "type": "user_nickname",
