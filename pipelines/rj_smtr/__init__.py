0f337d217f28bdd21572f97f2f8438cff6249a13
Rafael <rafael.c.pinheiro96@gmail.com>
2023-12-01 17:04:08 -0300

    criar inits

--- pipelines/rj_smtr/__init__.py
+++ pipelines/rj_smtr/__init__.py
@@ -20,3 +20,4 @@ from pipelines.rj_smtr.example.flows import *
 from pipelines.rj_smtr.br_rj_riodejaneiro_bilhetagem.flows import *
 from pipelines.rj_smtr.br_rj_riodejaneiro_gtfs.flows import *
 from pipelines.rj_smtr.br_rj_riodejaneiro_stu.flows import *
+from pipelines.rj_smtr.br_rj_riodejaneiro_diretorios.flows import *
