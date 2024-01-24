e8dbc884bff7c099c692c5770291309d9ee1b2b1
patriciacatandi <patriciabcatandi@gmail.com>
2024-01-24 17:36:32 -0300

    altering query

--- pipelines/rj_cor/comando/eventos/constants.py
+++ pipelines/rj_cor/comando/eventos/constants.py
@@ -36,9 +36,10 @@ class constants(Enum):  # pylint: disable=c0103
             -- FROM `rj-cor.adm_cor_comando_staging.ocorrencias`
             FROM `rj-cor.adm_cor_comando_staging.ocorrencias_nova_api`
             WHERE id_pop IN ("5", "6", "31", "32", "33")
-                AND data_particao >= CAST(DATE_TRUNC(TIMESTAMP_SUB(CURRENT_DATETIME("America/Sao_Paulo"), INTERVAL 1 day), day) AS STRING)
-                AND CAST(data_inicio AS DATETIME) >= TIMESTAMP_SUB(CURRENT_DATETIME("America/Sao_Paulo"), INTERVAL 1 hour)
+                AND data_particao >= CAST(DATE_TRUNC(TIMESTAMP_SUB(CURRENT_DATETIME("America/Sao_Paulo"), INTERVAL 15 MINUTE), day) AS STRING)
+                AND CAST(data_inicio AS DATETIME) >= TIMESTAMP_SUB(CURRENT_DATETIME("America/Sao_Paulo"), INTERVAL 15 MINUTE)
                 -- AND data_fim IS NULL
+                AND status = "Aberto"
             ),
 
             intersected_areas AS (
@@ -92,11 +93,7 @@ class constants(Enum):  # pylint: disable=c0103
         FROM final_table
         """,
         "query_update": """
-            SELECT
-                CAST(MAX(data_inicio) AS DATETIME) AS last_update
-            FROM
-                `rj-cor.adm_cor_comando_staging.ocorrencias_nova_api`
-                WHERE data_particao >= CAST(DATE_SUB(CURRENT_DATE('America/Sao_Paulo'), INTERVAL 1 DAY) AS STRING)
+            SELECT date_trunc(current_datetime("America/Sao_Paulo"), minute) AS last_update
         """,
     }
     RAIN_DASHBOARD_LAST_2H_FLOW_SCHEDULE_PARAMETERS = {
@@ -113,25 +110,45 @@ class constants(Enum):  # pylint: disable=c0103
                 END AS tipo,
                 ST_GEOGPOINT(CAST(longitude AS FLOAT64),
                 CAST(latitude AS FLOAT64)) AS geometry
-            FROM `rj-cor.adm_cor_comando_staging.ocorrencias`
+            -- FROM `rj-cor.adm_cor_comando_staging.ocorrencias`
+            FROM `rj-cor.adm_cor_comando_staging.ocorrencias_nova_api`
             WHERE id_pop IN ("5", "6", "31", "32", "33")
-                AND data_particao >= DATE_TRUNC(TIMESTAMP_SUB(CURRENT_DATETIME("America/Sao_Paulo"), INTERVAL 120 MINUTE), day)
+                AND data_particao >= CAST(DATE_TRUNC(TIMESTAMP_SUB(CURRENT_DATETIME("America/Sao_Paulo"), INTERVAL 120 MINUTE), day) AS STRING)
                 AND (CAST(data_fim AS DATETIME) >= TIMESTAMP_SUB(CURRENT_DATETIME("America/Sao_Paulo"), INTERVAL 120 MINUTE)
-                    OR data_fim IS NULL)
-            --    AND data_particao >= DATE_TRUNC(TIMESTAMP_SUB(CAST("2022-04-01 04:39:15" as datetime), INTERVAL 120 MINUTE), day)
-            --    AND (CAST(data_fim AS DATETIME) >= TIMESTAMP_SUB(CAST("2022-04-01 04:39:15" as datetime), INTERVAL 120 MINUTE)
-            --        OR data_fim IS NULL)
+                    -- OR data_fim IS NULL
+                    )
+                AND status = "Aberto"
             ),
+
+            intersected_areas AS (
+              SELECT
+                h3_grid.id AS id_h3,
+                bairros.nome AS bairro,
+                h3_grid.geometry,
+                -- bairros.geometry,
+                ST_AREA(ST_INTERSECTION(bairros.geometry, h3_grid.geometry)) AS intersection_area,
+                ROW_NUMBER() OVER (PARTITION BY h3_grid.id ORDER BY ST_AREA(ST_INTERSECTION(bairros.geometry, h3_grid.geometry)) DESC) AS row_num
+              FROM
+                `rj-cor.dados_mestres.h3_grid_res8` h3_grid
+              LEFT JOIN
+                `rj-cor.dados_mestres.bairro` AS bairros
+              ON
+                ST_INTERSECTS(bairros.geometry, h3_grid.geometry)
+              WHERE NOT ST_CONTAINS(ST_GEOGFROMTEXT('POLYGON((-43.35167114973923 -23.03719187431942, -43.21742224531541 -23.11411703410819, -43.05787930227852 -23.08560586153892, -43.13797293161925 -22.9854505090441, -43.24908435505957 -23.01309491285712, -43.29357259322761 -23.02302500142027, -43.35372293867113 -23.02286949608791, -43.35167114973923 -23.03719187431942))'), h3_grid.geometry)
+                AND NOT ST_CONTAINS(ST_GEOGFROMTEXT('POLYGON((-43.17255470033881 -22.80357287766821, -43.16164114820394 -22.8246787848653, -43.1435175784006 -22.83820699694974, -43.08831858222521 -22.79901386772875, -43.09812065965735 -22.76990583135868, -43.11917632397501 -22.77502040608505, -43.12252626904735 -22.74275730775724, -43.13510053525226 -22.7443347361711, -43.1568784870256 -22.79110122556994, -43.17255470033881 -22.80357287766821))'), h3_grid.geometry)
+                AND h3_grid.id not in ("88a8a06a31fffff", "88a8a069b5fffff", "88a8a3d357fffff", "88a8a3d355fffff", "88a8a068adfffff", "88a8a06991fffff", "88a8a06999fffff")
+
+            ),
+
             final_table AS (
             SELECT
-                h3_grid.id AS id_h3,
-                nome AS bairro,
+                id_h3,
+                bairro,
                 COALESCE(MAX(tipo), 0) AS tipo
-            FROM `rj-cor.dados_mestres.h3_grid_res8` h3_grid
-            LEFT JOIN `rj-cor.dados_mestres.bairro`
-                ON ST_CONTAINS(`rj-cor.dados_mestres.bairro`.geometry, ST_CENTROID(h3_grid.geometry))
+            FROM intersected_areas
             LEFT JOIN alagamentos
-                ON ST_CONTAINS(h3_grid.geometry, alagamentos.geometry)
+                ON ST_CONTAINS(intersected_areas.geometry, alagamentos.geometry)
+            WHERE  intersected_areas.row_num = 1
             GROUP BY id_h3, bairro
             )
 
@@ -159,11 +176,6 @@ class constants(Enum):  # pylint: disable=c0103
         FROM final_table
         """,
         "query_update": """
-            SELECT
-                CAST(MAX(data_inicio) AS DATETIME) AS last_update
-            FROM
-                `rj-cor.adm_cor_comando_staging.ocorrencias_nova_api`
-            WHERE
-                WHERE data_particao >= CAST(DATE_SUB(CURRENT_DATE('America/Sao_Paulo'), INTERVAL 1 DAY) AS STRING)
+            SELECT date_trunc(current_datetime("America/Sao_Paulo"), minute) AS last_update
         """,
     }
