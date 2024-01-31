d195fb03a496733084b853fe6f04b1c3ae06e523
hellcassius <caiorogerio.santos@gmail.com>
2024-01-31 15:43:23 -0300

    remove redis

--- pipelines/rj_smtr/janitor/tasks.py
+++ pipelines/rj_smtr/janitor/tasks.py
@@ -47,15 +47,7 @@ def get_prefect_client():
 
 @task
 def get_active_flow_names(prefix="%SMTR%"):
-    redis = get_redis_client()
-    try:
-        flow_names = redis.get("active_flow_names")
-    except ValueError:
-        flow_names = query_active_flow_names(prefix=prefix)
-        redis.set("active_flow_names", flow_names)
-    except TypeError:
-        flow_names = query_active_flow_names(prefix=prefix)
-        redis.set("active_flow_names", flow_names)
+    flow_names = query_active_flow_names(prefix=prefix)
     log(f"Got flow_names\n{flow_names[:10]}\n...\n{flow_names[-10:-1]}")
     return flow_names
 
