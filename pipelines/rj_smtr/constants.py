05bd47251b9517a295b5038fcb7e525cb1028299
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2024-02-05 11:13:56 -0300

    Bilhetagem Jae - Dados de clientes (#629)
    * adicionar captura da gratuidade
    
    * queries gratuidades
    
    * ajusta constantes
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pipelines/rj_smtr/constants.py
+++ pipelines/rj_smtr/constants.py
@@ -190,6 +190,10 @@ class constants(Enum):  # pylint: disable=c0103
                 "engine": "postgresql",
                 "host": "10.5.15.127",
             },
+            "gratuidade_db": {
+                "engine": "postgresql",
+                "host": "10.5.12.107",
+            },
         },
         "source_type": "db",
     }
@@ -349,10 +353,6 @@ class constants(Enum):  # pylint: disable=c0103
                         c.*
                     FROM
                         CLIENTE c
-                    JOIN
-                        OPERADORA_TRANSPORTE o
-                    ON
-                        c.CD_CLIENTE = o.CD_CLIENTE
                     WHERE
                         DT_CADASTRO BETWEEN '{start}'
                         AND '{end}'
@@ -362,6 +362,54 @@ class constants(Enum):  # pylint: disable=c0103
             "interval_minutes": BILHETAGEM_TRATAMENTO_INTERVAL,
             "save_bucket_name": BILHETAGEM_PRIVATE_BUCKET,
             "pre_treatment_reader_args": {"dtype": {"NR_DOCUMENTO": "object"}},
+        },
+        {
+            "table_id": "pessoa_fisica",
+            "partition_date_only": True,
+            "extract_params": {
+                "database": "principal_db",
+                "query": """
+                    SELECT
+                        p.*,
+                        c.DT_CADASTRO
+                    FROM
+                        PESSOA_FISICA p
+                    JOIN
+                        CLIENTE c
+                    ON
+                        p.CD_CLIENTE = c.CD_CLIENTE
+                    WHERE
+                        c.DT_CADASTRO BETWEEN '{start}'
+                        AND '{end}'
+                """,
+            },
+            "primary_key": ["CD_CLIENTE"],  # id column to nest data on
+            "interval_minutes": BILHETAGEM_TRATAMENTO_INTERVAL,
+            "save_bucket_name": BILHETAGEM_PRIVATE_BUCKET,
+        },
+        {
+            "table_id": "gratuidade",
+            "partition_date_only": True,
+            "extract_params": {
+                "database": "gratuidade_db",
+                "query": """
+                    SELECT
+                        g.*,
+                        t.descricao AS tipo_gratuidade
+                    FROM
+                        gratuidade g
+                    LEFT JOIN
+                        tipo_gratuidade t
+                    ON
+                        g.id_tipo_gratuidade = t.id
+                    WHERE
+                        g.dt_inclusao BETWEEN '{start}'
+                        AND '{end}'
+                """,
+            },
+            "primary_key": ["id"],  # id column to nest data on
+            "interval_minutes": BILHETAGEM_TRATAMENTO_INTERVAL,
+            "save_bucket_name": BILHETAGEM_PRIVATE_BUCKET,
         },
         {
             "table_id": "consorcio",
