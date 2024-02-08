e8c2a447645c937786b95c7988d3b35edb59276a
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2024-02-08 13:19:53 -0300

    Incorpora testes pós-modelo dbt na task subsidio_data_quality_check + remove bibliotecas

--- pipelines/rj_smtr/projeto_subsidio_sppo/tasks.py
+++ pipelines/rj_smtr/projeto_subsidio_sppo/tasks.py
@@ -5,8 +5,6 @@ Tasks for projeto_subsidio_sppo
 
 from prefect import task
 
-import basedosdados as bd
-import pandas as pd
 from datetime import datetime, timedelta
 
 from pipelines.constants import constants
@@ -26,7 +24,7 @@ def check_param(param: str) -> bool:
 
 @task
 def subsidio_data_quality_check(
-    params: dict, code_owners: list = None, check_params: dict = None
+    mode: str, params: dict, code_owners: list = None, check_params: dict = None
 ) -> bool:
     """
     Verifica qualidade de dados para o processo de apuração de subsídio
@@ -39,6 +37,9 @@ def subsidio_data_quality_check(
     Returns:
         test_check (bool): True if all checks passed, False otherwise
     """
+
+    if mode not in ["pre", "pos"]:
+        raise ValueError("Mode must be 'pre' or 'pos'")
 
     if check_params is None:
         check_params = smtr_constants.SUBSIDIO_SPPO_DATA_CHECKS_PARAMS.value
@@ -55,47 +56,110 @@ def subsidio_data_quality_check(
         ).strftime("%Y-%m-%d %H:%M:%S"),
     }
 
-    request_params = general_request_params | {
-        "interval": 1,
-        "dataset_id": smtr_constants.GPS_SPPO_RAW_DATASET_ID.value,
-        "table_id": smtr_constants.GPS_SPPO_RAW_TABLE_ID.value,
-    }
-    checks.append(
-        perform_check(
-            "Captura dos dados de GPS",
-            check_params.get("check_gps_capture"),
-            request_params,
+    if mode == "pre":
+        request_params = general_request_params | {
+            "interval": 1,
+            "dataset_id": smtr_constants.GPS_SPPO_RAW_DATASET_ID.value,
+            "table_id": smtr_constants.GPS_SPPO_RAW_TABLE_ID.value,
+        }
+        checks.append(
+            perform_check(
+                "Captura dos dados de GPS",
+                check_params.get("check_gps_capture"),
+                request_params,
+            )
         )
-    )
 
-    request_params = general_request_params | {
-        "interval": 10,
-        "dataset_id": smtr_constants.GPS_SPPO_RAW_DATASET_ID.value,
-        "table_id": smtr_constants.GPS_SPPO_REALOCACAO_RAW_TABLE_ID.value,
-    }
-    checks.append(
-        perform_check(
-            "Captura dos dados de GPS - Realocação",
-            check_params.get("check_gps_capture"),
-            request_params,
+        request_params = general_request_params | {
+            "interval": 10,
+            "dataset_id": smtr_constants.GPS_SPPO_RAW_DATASET_ID.value,
+            "table_id": smtr_constants.GPS_SPPO_REALOCACAO_RAW_TABLE_ID.value,
+        }
+        checks.append(
+            perform_check(
+                "Captura dos dados de GPS - Realocação",
+                check_params.get("check_gps_capture"),
+                request_params,
+            )
         )
-    )
 
-    checks.append(
-        perform_check(
-            "Tratamento dos dados de GPS",
-            check_params.get("check_gps_treatment"),
-            general_request_params,
+        checks.append(
+            perform_check(
+                "Tratamento dos dados de GPS",
+                check_params.get("check_gps_treatment"),
+                general_request_params,
+            )
         )
-    )
 
-    checks.append(
-        perform_check(
-            "Tratamento da sppo_veiculo_dia",
-            check_params.get("check_sppo_veiculo_dia"),
-            general_request_params,
+        checks.append(
+            perform_check(
+                "Tratamento da sppo_veiculo_dia",
+                check_params.get("check_sppo_veiculo_dia"),
+                general_request_params,
+            )
         )
-    )
+
+    if mode == "pos":
+        request_params = general_request_params | {
+            "dataset_id": smtr_constants.SUBSIDIO_SPPO_DASHBOARD_DATASET_ID.value,
+            "table_id": smtr_constants.SUBSIDIO_SPPO_DASHBOARD_TABLE_ID.value,
+        }
+
+        checks.append(
+            perform_check(
+                "sumario_servico_dia - valor_penalidade aceitos",
+                check_params.get(
+                    "accepted_values_sumario_servico_dia_valor_penalidade"
+                ),
+                request_params,
+            )
+        )
+
+        checks.append(
+            perform_check(
+                "sumario_servico_dia - Teto de Pagamento de Valor de Subsídio",
+                check_params.get(
+                    "teto_pagamento_sumario_servico_dia_valor_subsidio_pago"
+                ),
+                request_params,
+            )
+        )
+
+        request_params["expression"] = "data, servico"
+        checks.append(
+            perform_check(
+                "sumario_servico_dia - Unicidade da chave data-servico",
+                check_params.get("unique_combination"),
+                request_params,
+            )
+        )
+
+        expressions = {
+            "data IS NOT NULL": "Data não é nulo",
+            "tipo_dia IS NOT NULL": "Tipo de dia não é nulo",
+            "consorcio IS NOT NULL": "Consórcio não é nulo",
+            "servico IS NOT NULL": "Serviço não é nulo",
+            "viagens IS NOT NULL": "Quantidade de Viagens não é nulo",
+            "viagens >= 0": "Quantidade de Viagens >= 0",
+            "km_apurada IS NOT NULL": "Quilometragem Apurada não é nulo",
+            "km_apurada >= 0": "Quilometragem Apurada >= 0",
+            "km_planejada IS NOT NULL": "Quilometragem Planejada não é nulo",
+            "km_planejada >= 0": "Quilometragem Planejada >= 0",
+            "perc_km_planejada IS NOT NULL": "Percentual de Operação Diário não é nulo",
+            "perc_km_planejada >= 0": "Percentual de Operação Diário >= 0",
+            "valor_subsidio_pago IS NOT NULL": "Valor de Subsídio Pago não é nulo",
+            "valor_subsidio_pago >= 0": "Valor de Subsídio Pago >= 0",
+        }
+
+        for expression, description in expressions.items():
+            request_params["expression"] = expression
+            checks.append(
+                perform_check(
+                    f"sumario_servico_dia - {description}",
+                    check_params.get("expression_is_true"),
+                    request_params,
+                )
+            )
 
     log(checks)
 
@@ -105,13 +169,28 @@ def subsidio_data_quality_check(
         else f'{params["start_date"]} a {params["end_date"]}'
     )
     formatted_messages = [
-        f"**Data Quality Checks - Apuração de Subsídio - {date_range}**\n\n"
+        f"**{mode.capitalize()}-Data Quality Checks - Apuração de Subsídio - {date_range}**\n\n"
     ]
     test_check = all(check["status"] for check in checks)
 
     for check in checks:
         formatted_messages.append(
             f'{":white_check_mark:" if check["status"] else ":x:"} {check["desc"]}\n'
+        )
+
+    if mode == "pre":
+        formatted_messages.append(
+            ""
+            if test_check
+            else """:warning: **Status:** Run cancelada.
+            Necessidade de revisão dos dados de entrada!\n"""
+        )
+
+    if mode == "pos":
+        formatted_messages.append(
+            ":tada: **Status:** Sucesso"
+            if test_check
+            else ":warning: **Status:** Testes falharam. Necessidade de revisão dos dados finais!\n"
         )
 
     if not test_check:
@@ -130,7 +209,6 @@ def subsidio_data_quality_check(
         formatted_messages.extend(at_code_owners)
 
     formatted_messages.insert(0, ":green_circle: " if test_check else ":red_circle: ")
-    formatted_messages.insert(0, "[Teste Dev]\n\n")
     formatted_message = "".join(formatted_messages)
 
     log(formatted_message)
