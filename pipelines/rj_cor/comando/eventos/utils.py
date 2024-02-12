bdf09af533611369423882a17951e1bc1f4a54c5
patriciacatandi <patriciabcatandi@gmail.com>
2024-02-12 10:03:50 -0300

    forcing checks on github

--- pipelines/rj_cor/comando/eventos/utils.py
+++ pipelines/rj_cor/comando/eventos/utils.py
@@ -32,7 +32,6 @@ def format_date(first_date, last_date):
 def get_redis_output(redis_key, is_df: bool = False):
     """
     Get Redis output. Use .get to obtain a df from redis or hgetall if is a key value pair.
-    Example: {b'date': b'2023-02-27 07:29:04'}
     """
     redis_client = get_redis_client()  # (host="127.0.0.1")
 
