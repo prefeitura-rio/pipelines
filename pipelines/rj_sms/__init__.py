a90633ca08d8695fa4227f2a8dba87f19e021b83
ThiagoTrabach <thiago.trabach@gmail.com>
2024-03-15 11:47:08 -0300

    Remove legacy dumps

--- pipelines/rj_sms/__init__.py
+++ pipelines/rj_sms/__init__.py
@@ -3,9 +3,5 @@
 Prefect flows for rj_sms project
 """
 
-from pipelines.rj_sms.dump_db_sivep.flows import *
-from pipelines.rj_sms.dump_ftp_cnes.flows import *
 from pipelines.rj_sms.dump_azureblob_estoque_tpc.flows import *
-from pipelines.rj_sms.dump_api_prontuario_vitai.flows import *
-from pipelines.rj_sms.dump_api_prontuario_vitacare.flows import *
-from pipelines.rj_sms.dump_sheets.flows import *
+
