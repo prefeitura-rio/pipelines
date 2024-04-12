0aa3bb6616c52c5a94f64c63ae60e90a49ba78f3
Rafael <rafael.c.pinheiro96@gmail.com>
2024-04-12 10:32:22 -0300

    ativa materialização

--- pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps_zirix/constants.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps_zirix/constants.py
@@ -12,3 +12,4 @@ class constants(Enum):  # pylint: disable=c0103
     """
 
     GPS_SPPO_ZIRIX_RAW_DATASET_ID = "br_rj_riodejaneiro_onibus_gps_zirix"
+    GPS_SPPO_ZIRIX_TREATED_TABLE_ID = "gps_sppo_zirix"
--- pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps_zirix/flows.py
+++ pipelines/rj_smtr/br_rj_riodejaneiro_onibus_gps_zirix/flows.py
@@ -6,7 +6,7 @@ from prefect import Parameter, case
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
-# from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 from prefect.utilities.edges import unmapped
 
 
@@ -16,12 +16,12 @@ from pipelines.constants import constants as emd_constants
 from pipelines.utils.tasks import (
     rename_current_flow_run_now_time,
     get_now_time,
-    # get_current_flow_mode,
+    get_current_flow_mode,
     get_current_flow_labels,
 )
 from pipelines.utils.decorators import Flow
 
-# from pipelines.utils.execute_dbt_model.tasks import get_k8s_dbt_client
+from pipelines.utils.execute_dbt_model.tasks import get_k8s_dbt_client
 
 # SMTR Imports #
 
@@ -34,16 +34,16 @@ from pipelines.rj_smtr.br_rj_riodejaneiro_onibus_gps_zirix.constants import (
 from pipelines.rj_smtr.tasks import (
     create_date_hour_partition,
     create_local_partition_path,
-    # fetch_dataset_sha,
+    fetch_dataset_sha,
     get_current_timestamp,
-    # get_materialization_date_range,
+    get_materialization_date_range,
     # get_local_dbt_client,
     get_raw,
     parse_timestamp_to_string,
     query_logs,
     save_raw_local,
     save_treated_local,
-    # set_last_run_timestamp,
+    set_last_run_timestamp,
     upload_logs_to_bq,
     bq_upload,
 )
@@ -60,7 +60,7 @@ from pipelines.rj_smtr.schedules import (
     every_10_minutes,
 )
 
-# from pipelines.utils.execute_dbt_model.tasks import run_dbt_model
+from pipelines.utils.execute_dbt_model.tasks import run_dbt_model
 
 # Flows #
 
@@ -127,88 +127,92 @@ realocacao_sppo.run_config = KubernetesRun(
 realocacao_sppo.schedule = every_10_minutes
 
 
-# with Flow(
-#     "SMTR: GPS SPPO - Materialização (subflow)",
-#     code_owners=["caio", "fernanda", "boris", "rodrigo"],
-# ) as materialize_sppo:
-#     # Rename flow run
-#     rename_flow_run = rename_current_flow_run_now_time(
-#         prefix=materialize_sppo.name + ": ", now_time=get_now_time()
-#     )
+with Flow(
+    "SMTR: GPS SPPO Zirix - Materialização (subflow)",
+    code_owners=["caio", "fernanda", "boris", "rodrigo"],
+) as materialize_sppo:
+    # Rename flow run
+    rename_flow_run = rename_current_flow_run_now_time(
+        prefix=materialize_sppo.name + ": ", now_time=get_now_time()
+    )
 
-#     # Get default parameters #
-#     raw_dataset_id = Parameter(
-#         "raw_dataset_id", default=zirix_constants.GPS_SPPO_ZIRIX_RAW_DATASET_ID.value
-#     )
-#     raw_table_id = Parameter(
-#         "raw_table_id", default=constants.GPS_SPPO_RAW_TABLE_ID.value
-#     )
-#     dataset_id = Parameter("dataset_id", default=constants.GPS_SPPO_DATASET_ID.value)
-#     table_id = Parameter("table_id", default=constants.GPS_SPPO_TREATED_TABLE_ID.value)
-#     rebuild = Parameter("rebuild", False)
+    # Get default parameters #
+    raw_dataset_id = Parameter(
+        "raw_dataset_id", default=zirix_constants.GPS_SPPO_ZIRIX_RAW_DATASET_ID.value
+    )
+    raw_table_id = Parameter(
+        "raw_table_id", default=constants.GPS_SPPO_RAW_TABLE_ID.value
+    )
+    dataset_id = Parameter(
+        "dataset_id", default=zirix_constants.GPS_SPPO_ZIRIX_RAW_DATASET_ID.value
+    )
+    table_id = Parameter(
+        "table_id", default=zirix_constants.GPS_SPPO_ZIRIX_TREATED_TABLE_ID.value
+    )
+    rebuild = Parameter("rebuild", False)
 
-#     LABELS = get_current_flow_labels()
-#     MODE = get_current_flow_mode(LABELS)
+    LABELS = get_current_flow_labels()
+    MODE = get_current_flow_mode(LABELS)
 
-#     # Set dbt client #
-#     dbt_client = get_k8s_dbt_client(mode=MODE, wait=rename_flow_run)
-#     # Use the command below to get the dbt client in dev mode:
-#     # dbt_client = get_local_dbt_client(host="localhost", port=3001)
+    # Set dbt client #
+    dbt_client = get_k8s_dbt_client(mode=MODE, wait=rename_flow_run)
+    # Use the command below to get the dbt client in dev mode:
+    # dbt_client = get_local_dbt_client(host="localhost", port=3001)
 
-#     # Set specific run parameters #
-#     date_range = get_materialization_date_range(
-#         dataset_id=dataset_id,
-#         table_id=table_id,
-#         raw_dataset_id=raw_dataset_id,
-#         raw_table_id=raw_table_id,
-#         table_run_datetime_column_name="timestamp_gps",
-#         mode=MODE,
-#         delay_hours=constants.GPS_SPPO_MATERIALIZE_DELAY_HOURS.value,
-#     )
-#     dataset_sha = fetch_dataset_sha(
-#         dataset_id=dataset_id,
-#     )
+    # Set specific run parameters #
+    date_range = get_materialization_date_range(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        raw_dataset_id=raw_dataset_id,
+        raw_table_id=raw_table_id,
+        table_run_datetime_column_name="timestamp_gps",
+        mode=MODE,
+        delay_hours=constants.GPS_SPPO_MATERIALIZE_DELAY_HOURS.value,
+    )
+    dataset_sha = fetch_dataset_sha(
+        dataset_id=dataset_id,
+    )
 
-#     # Run materialization #
-#     with case(rebuild, True):
-#         RUN = run_dbt_model(
-#             dbt_client=dbt_client,
-#             dataset_id=dataset_id,
-#             table_id=table_id,
-#             upstream=True,
-#             exclude="+data_versao_efetiva",
-#             _vars=[date_range, dataset_sha],
-#             flags="--full-refresh",
-#         )
-#         set_last_run_timestamp(
-#             dataset_id=dataset_id,
-#             table_id=table_id,
-#             timestamp=date_range["date_range_end"],
-#             wait=RUN,
-#             mode=MODE,
-#         )
-#     with case(rebuild, False):
-#         RUN = run_dbt_model(
-#             dbt_client=dbt_client,
-#             dataset_id=dataset_id,
-#             table_id=table_id,
-#             exclude="+data_versao_efetiva",
-#             _vars=[date_range, dataset_sha],
-#             upstream=True,
-#         )
-#         set_last_run_timestamp(
-#             dataset_id=dataset_id,
-#             table_id=table_id,
-#             timestamp=date_range["date_range_end"],
-#             wait=RUN,
-#             mode=MODE,
-#         )
+    # Run materialization #
+    with case(rebuild, True):
+        RUN = run_dbt_model(
+            dbt_client=dbt_client,
+            dataset_id=dataset_id,
+            table_id=table_id,
+            upstream=True,
+            exclude="+data_versao_efetiva",
+            _vars=[date_range, dataset_sha],
+            flags="--full-refresh",
+        )
+        set_last_run_timestamp(
+            dataset_id=dataset_id,
+            table_id=table_id,
+            timestamp=date_range["date_range_end"],
+            wait=RUN,
+            mode=MODE,
+        )
+    with case(rebuild, False):
+        RUN = run_dbt_model(
+            dbt_client=dbt_client,
+            dataset_id=dataset_id,
+            table_id=table_id,
+            exclude="+data_versao_efetiva",
+            _vars=[date_range, dataset_sha],
+            upstream=True,
+        )
+        set_last_run_timestamp(
+            dataset_id=dataset_id,
+            table_id=table_id,
+            timestamp=date_range["date_range_end"],
+            wait=RUN,
+            mode=MODE,
+        )
 
-# materialize_sppo.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
-# materialize_sppo.run_config = KubernetesRun(
-#     image=emd_constants.DOCKER_IMAGE.value,
-#     labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
-# )
+materialize_sppo.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
+materialize_sppo.run_config = KubernetesRun(
+    image=emd_constants.DOCKER_IMAGE.value,
+    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
+)
 
 
 with Flow(
@@ -290,20 +294,20 @@ with Flow(
     rename_flow_run = rename_current_flow_run_now_time(
         prefix=recaptura.name + ": ", now_time=get_now_time(), wait=timestamps
     )
-    # with case(errors, False):
-    #     with case(materialize, True):
-    #         materialize_no_error = create_flow_run(
-    #             flow_name=materialize_sppo.name,
-    #             project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
-    #             labels=LABELS,
-    #             run_name=materialize_sppo.name,
-    #         )
-    #         wait_materialize_no_error = wait_for_flow_run(
-    #             materialize_no_error,
-    #             stream_states=True,
-    #             stream_logs=True,
-    #             raise_final_state=True,
-    #         )
+    with case(errors, False):
+        with case(materialize, True):
+            materialize_no_error = create_flow_run(
+                flow_name=materialize_sppo.name,
+                project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
+                labels=LABELS,
+                run_name=materialize_sppo.name,
+            )
+            wait_materialize_no_error = wait_for_flow_run(
+                materialize_no_error,
+                stream_states=True,
+                stream_logs=True,
+                raise_final_state=True,
+            )
     with case(errors, True):
         # SETUP #
         partitions = create_date_hour_partition.map(timestamps)
@@ -352,23 +356,23 @@ with Flow(
             timestamp=timestamps,
             recapture=unmapped(True),
         )
-    #     with case(materialize, True):
-    #         run_materialize = create_flow_run(
-    #             flow_name=materialize_sppo.name,
-    #             project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
-    #             labels=LABELS,
-    #             run_name=materialize_sppo.name,
-    #         )
-    #         wait_materialize = wait_for_flow_run(
-    #             run_materialize,
-    #             stream_states=True,
-    #             stream_logs=True,
-    #             raise_final_state=True,
-    #         )
-    # recaptura.set_dependencies(
-    #     task=run_materialize,
-    #     upstream_tasks=[UPLOAD_LOGS],
-    # )
+        with case(materialize, True):
+            run_materialize = create_flow_run(
+                flow_name=materialize_sppo.name,
+                project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
+                labels=LABELS,
+                run_name=materialize_sppo.name,
+            )
+            wait_materialize = wait_for_flow_run(
+                run_materialize,
+                stream_states=True,
+                stream_logs=True,
+                raise_final_state=True,
+            )
+    recaptura.set_dependencies(
+        task=run_materialize,
+        upstream_tasks=[UPLOAD_LOGS],
+    )
 
 recaptura.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 recaptura.run_config = KubernetesRun(
