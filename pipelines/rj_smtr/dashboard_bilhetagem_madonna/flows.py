649345ae95c81e8698519870faf2dbf4f5f6bbd3
vtr363 <victor.m.rocha.vr@hotmail.com>
2024-05-03 13:15:40 -0300

    comenta subflows roda o modelo diretamento no flow

--- pipelines/rj_smtr/dashboard_bilhetagem_madonna/flows.py
+++ pipelines/rj_smtr/dashboard_bilhetagem_madonna/flows.py
@@ -14,6 +14,7 @@ from pipelines.utils.decorators import Flow
 
 from pipelines.utils.tasks import (
     get_current_flow_labels,
+    get_current_flow_mode,
 )
 
 from pipelines.utils.utils import set_default_parameters
@@ -24,11 +25,14 @@ from pipelines.rj_smtr.flows import (
     default_materialization_flow,
 )
 
+from pipelines.utils.execute_dbt_model.tasks import get_k8s_dbt_client
 
 from pipelines.constants import constants as emd_constants
 from pipelines.rj_smtr.constants import constants
 
 from pipelines.rj_smtr.schedules import every_10_minutes
+
+from pipelines.utils.execute_dbt_model.tasks import run_dbt_model
 
 bilhetagem_materializacao_madonna = deepcopy(default_materialization_flow)
 bilhetagem_materializacao_madonna.name = (
@@ -58,38 +62,49 @@ bilhetagem_materializacao_madonna.schedule = every_10_minutes
 with Flow("SMTR: Bilhetagem Madonna - Tratamento") as bilhetagem_madonna:
     LABELS = get_current_flow_labels()
 
-    # Recaptura
-    run_recaptura_transacao = create_flow_run(
-        flow_name=bilhetagem_recaptura.name,
-        # project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
-        project_name="staging",
-        labels=emd_constants.RJ_SMTR_AGENT_LABEL.value,
-        parameters=constants.BILHETAGEM_TRANSACAO_CAPTURE_PARAMS.value,
-    )
+    ## RECAPTURA ##
 
-    wait_recaptura_transacao = wait_for_flow_run(
-        run_recaptura_transacao,
-        stream_states=True,
-        stream_logs=True,
-        raise_final_state=True,
-    )
+    # run_recaptura_transacao = create_flow_run(
+    #     flow_name=bilhetagem_recaptura.name,
+    #     # project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
+    #     project_name="staging",
+    #     labels=emd_constants.RJ_SMTR_AGENT_LABEL.value,
+    #     parameters=constants.BILHETAGEM_TRANSACAO_CAPTURE_PARAMS.value,
+    # )
 
-    # Materialização
-    run_materializacao_madonna = create_flow_run(
-        flow_name=bilhetagem_materializacao_madonna.name,
-        # project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
-        project_name="staging",
-        labels=LABELS,
-        # upstream_tasks=[
-        #     wait_recaptura_transacao,
-        # ],
-    )
+    # wait_recaptura_transacao = wait_for_flow_run(
+    #     run_recaptura_transacao,
+    #     stream_states=True,
+    #     stream_logs=True,
+    #     raise_final_state=True,
+    # )
 
-    wait_materializacao_madonna = wait_for_flow_run(
-        run_materializacao_madonna,
-        stream_states=True,
-        stream_logs=True,
-        raise_final_state=True,
+    ## MATERIALIZAÇÃO ##
+
+    # run_materializacao_madonna = create_flow_run(
+    #     flow_name=bilhetagem_materializacao_madonna.name,
+    #     # project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
+    #     project_name="staging",
+    #     labels=LABELS,
+    #     # upstream_tasks=[
+    #     #     wait_recaptura_transacao,
+    #     # ],
+    # )
+
+    # wait_materializacao_madonna = wait_for_flow_run(
+    #     run_materializacao_madonna,
+    #     stream_states=True,
+    #     stream_logs=True,
+    #     raise_final_state=True,
+    # )
+
+    MODE = get_current_flow_mode(LABELS)
+    dbt_client = get_k8s_dbt_client(mode=MODE)
+    RUNS = run_dbt_model(
+        dbt_client=dbt_client,
+        dataset_id="dashboard_bilhetagem_madonna",
+        table_id="transacao_gentileza",
+        upstream=True,
     )
 
 bilhetagem_madonna.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
