71d9b7655d3edf665651a997d2de0dc4eb45934c
Bruno Almeida <brunodepauloalmeida@gmail.com>
2024-05-03 14:10:21 -0300

    delete: comment flows that have been migrated

--- pipelines/rj_segovi/__init__.py
+++ pipelines/rj_segovi/__init__.py
@@ -4,5 +4,5 @@ Prefect flows for segovi project
 """
 
 from pipelines.rj_segovi.dump_db_1746.flows import *
-from pipelines.rj_segovi.dump_url_adm_processorio_sicop.flows import *
-from pipelines.rj_segovi.dump_ftp_adm_processorio_sicop.flows import *
+# from pipelines.rj_segovi.dump_url_adm_processorio_sicop.flows import *
+# from pipelines.rj_segovi.dump_ftp_adm_processorio_sicop.flows import *
