7536a4764dbcca6ddfb9b4da60be538fff50d58b
vtr363 <victor.m.rocha.vr@hotmail.com>
2024-05-03 12:02:09 -0300

    comenta recaptura

--- pipelines/rj_smtr/dashboard_bilhetagem_madonna/flows.py
+++ pipelines/rj_smtr/dashboard_bilhetagem_madonna/flows.py
@@ -58,21 +58,21 @@ bilhetagem_materializacao_madonna.schedule = every_10_minutes
 with Flow("SMTR: Bilhetagem Madonna - Tratamento") as bilhetagem_madonna:
     LABELS = get_current_flow_labels()
 
-    # Recaptura
-    run_recaptura_transacao = create_flow_run(
-        flow_name=bilhetagem_recaptura.name,
-        # project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
-        project_name="staging",
-        labels=emd_constants.RJ_SMTR_AGENT_LABEL.value,
-        parameters=constants.BILHETAGEM_TRANSACAO_CAPTURE_PARAMS.value,
-    )
+    # # Recaptura
+    # run_recaptura_transacao = create_flow_run(
+    #     flow_name=bilhetagem_recaptura.name,
+    #     # project_name=emd_constants.PREFECT_DEFAULT_PROJECT.value,
+    #     project_name="staging",
+    #     labels=emd_constants.RJ_SMTR_AGENT_LABEL.value,
+    #     parameters=constants.BILHETAGEM_TRANSACAO_CAPTURE_PARAMS.value,
+    # )
 
-    wait_recaptura_transacao = wait_for_flow_run(
-        run_recaptura_transacao,
-        stream_states=True,
-        stream_logs=True,
-        raise_final_state=True,
-    )
+    # wait_recaptura_transacao = wait_for_flow_run(
+    #     run_recaptura_transacao,
+    #     stream_states=True,
+    #     stream_logs=True,
+    #     raise_final_state=True,
+    # )
 
     # Materialização
     run_materializacao_madonna = create_flow_run(
