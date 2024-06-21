576360ef1547fe8d762b4114108301d2f19fa6bd
Guilherme Botelho <guilhermehcb@hotmail.com>
2024-06-21 12:41:25 -0300

    desativa schedules projeto_subsidio_sppo

--- pipelines/rj_smtr/constants.py
+++ pipelines/rj_smtr/constants.py
@@ -168,6 +168,37 @@ class constants(Enum):  # pylint: disable=c0103
     SUBSIDIO_SPPO_DASHBOARD_STAGING_DATASET_ID = "dashboard_subsidio_sppo_staging"
     SUBSIDIO_SPPO_DASHBOARD_TABLE_ID = "sumario_servico_dia"
     SUBSIDIO_SPPO_DATA_CHECKS_PARAMS = {
+        "check_trips_processing": {
+            "query": """SELECT
+                        s.data,
+                        s.tipo_dia,
+                        s.subtipo_dia,
+                        s.tipo_os,
+                        s.feed_version,
+                        s.feed_start_date AS feed_start_date_invalido,
+                        i.feed_start_date AS feed_start_date_valido,
+                        FROM (
+                            SELECT
+                                *
+                            FROM
+                                rj-smtr.projeto_subsidio_sppo.subsidio_data_versao_efetiva
+                            WHERE
+                                DATA >= "2024-04-01" -- DATA_SUBSIDIO_V6_INICIO (Feature trajetos alternativos)
+                                AND DATA BETWEEN DATE("{start_timestamp}")
+                                    AND DATE("{end_timestamp}")
+                                ) AS s
+                        LEFT JOIN
+                            rj-smtr.gtfs.feed_info AS i
+                        ON
+                        (DATA BETWEEN i.feed_start_date
+                            AND i.feed_end_date
+                            OR (DATA >= i.feed_start_date
+                            AND i.feed_end_date IS NULL))
+                        WHERE
+                            i.feed_start_date != s.feed_start_date
+                        """,
+            "order_columns": ["data"],
+        },
         "check_gps_capture": {
             "query": """WITH
             t AS (
